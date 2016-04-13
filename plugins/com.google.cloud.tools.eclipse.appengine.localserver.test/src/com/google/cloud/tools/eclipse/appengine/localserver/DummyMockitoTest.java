f706a0bff424d9a32c1d799372e93621da4e412f
Andras Kerekes <andras.kerekes.work@gmail.com>
2016-04-13 13:45:00 -0400

    bundle JUnit into the test plugin, use import package for org.junit instead of require bundle

--- plugins/com.google.cloud.tools.eclipse.appengine.localserver.test/src/com/google/cloud/tools/eclipse/appengine/localserver/DummyMockitoTest.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.localserver.test/src/com/google/cloud/tools/eclipse/appengine/localserver/DummyMockitoTest.java
@@ -1,17 +1,23 @@
 package com.google.cloud.tools.eclipse.appengine.localserver;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import org.mockito.Mockito;
-
+@RunWith(MockitoJUnitRunner.class)
 public class DummyMockitoTest {
 
+	@Mock
 	private List<Integer> list;
 	
 	@Test
 	public void testMockito() {
-		
+		assertThat(list, notNullValue());
 	}
 }
