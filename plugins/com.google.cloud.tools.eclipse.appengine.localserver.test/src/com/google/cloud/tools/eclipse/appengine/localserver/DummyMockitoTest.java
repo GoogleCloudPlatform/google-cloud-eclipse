042536fdcf5532f7503961bd087d608d04018dfb
Andras Kerekes <andras.kerekes.work@gmail.com>
2016-04-13 11:30:11 -0400

    Fixes #62 - Add Mockito bundled as plugin to use in tests

---
+++ plugins/com.google.cloud.tools.eclipse.appengine.localserver.test/src/com/google/cloud/tools/eclipse/appengine/localserver/DummyMockitoTest.java
@@ -0,0 +1,17 @@
+package com.google.cloud.tools.eclipse.appengine.localserver;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+public class DummyMockitoTest {
+
+	private List<Integer> list;
+	
+	@Test
+	public void testMockito() {
+		
+	}
+}
