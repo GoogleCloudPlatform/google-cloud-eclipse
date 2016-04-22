1db8a3b9014a60b1cf2f09172810db9601ed6133
Elliotte Rusty Harold <elharo@google.com>
2016-04-22 17:38:34 -0400

    more wizardry

---
+++ plugins/com.google.cloud.tools.eclipse.appengine.newproject.test/src/com/google/cloud/tools/eclipse/appengine/newproject/StandardProjectWizardTest.java
@@ -0,0 +1,15 @@
+package com.google.cloud.tools.eclipse.appengine.newproject;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StandardProjectWizardTest {
+
+  private StandardProjectWizard wizard = new StandardProjectWizard();
+
+  @Test
+  public void testCanFinish() {
+    Assert.assertTrue(wizard.canFinish());
+  }
+
+}
