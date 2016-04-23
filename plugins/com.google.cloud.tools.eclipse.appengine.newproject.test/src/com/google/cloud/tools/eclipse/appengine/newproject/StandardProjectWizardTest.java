95f9cff6b0442a7225476b02b007b0ff14f09ac3
Elliotte Rusty Harold <elharo@google.com>
2016-04-23 06:26:24 -0400

    fix NullPointer

--- plugins/com.google.cloud.tools.eclipse.appengine.newproject.test/src/com/google/cloud/tools/eclipse/appengine/newproject/StandardProjectWizardTest.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.newproject.test/src/com/google/cloud/tools/eclipse/appengine/newproject/StandardProjectWizardTest.java
@@ -1,12 +1,19 @@
 package com.google.cloud.tools.eclipse.appengine.newproject;
 
+import org.eclipse.jface.resource.JFaceResources;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class StandardProjectWizardTest {
 
   private StandardProjectWizard wizard = new StandardProjectWizard();
 
+  @BeforeClass // seems to be needed on Travis to avoid race condition
+  public void loadImageRegistry() {
+    JFaceResources.getImageRegistry();
+  }
+  
   @Test
   public void testCanFinish() {
     Assert.assertTrue(wizard.canFinish());
