fb44e8d1ef552a29afaabf7d84c8320d16744bb2
Elliotte Rusty Harold <elharo@google.com>
2016-06-15 10:16:59 -0400

    fake starting a server

--- plugins/com.google.cloud.tools.eclipse.appengine.localserver/src/com/google/cloud/tools/eclipse/appengine/localserver/server/LocalAppEngineServerBehaviour.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.localserver/src/com/google/cloud/tools/eclipse/appengine/localserver/server/LocalAppEngineServerBehaviour.java
@@ -1,5 +1,6 @@
 package com.google.cloud.tools.eclipse.appengine.localserver.server;
 
+import org.eclipse.wst.server.core.IServer;
 import org.eclipse.wst.server.core.model.ServerBehaviourDelegate;
 
 public class LocalAppEngineServerBehaviour extends ServerBehaviourDelegate {
@@ -7,6 +8,10 @@ public class LocalAppEngineServerBehaviour extends ServerBehaviourDelegate {
   @Override
   public void stop(boolean force) {
     // TODO Auto-generated method stub
+  }
+
+  public void setStarted() {
+    setServerState(IServer.STATE_STARTED);    
   }
 
 }
--- plugins/com.google.cloud.tools.eclipse.appengine.localserver/src/com/google/cloud/tools/eclipse/appengine/localserver/server/LocalAppEngineServerLaunchConfigurationDelegate.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.localserver/src/com/google/cloud/tools/eclipse/appengine/localserver/server/LocalAppEngineServerLaunchConfigurationDelegate.java
@@ -1,10 +1,15 @@
 package com.google.cloud.tools.eclipse.appengine.localserver.server;
 
+import java.util.List;
+
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.jdt.launching.AbstractJavaLaunchConfigurationDelegate;
+import org.eclipse.wst.server.core.IServer;
+import org.eclipse.wst.server.core.ServerUtil;
 
 public class LocalAppEngineServerLaunchConfigurationDelegate
     extends AbstractJavaLaunchConfigurationDelegate {
@@ -12,8 +17,18 @@ public class LocalAppEngineServerLaunchConfigurationDelegate
   @Override
   public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch,
       IProgressMonitor monitor) throws CoreException {
-    // TODO Auto-generated method stub
+    
+    final IServer server = ServerUtil.getServer(configuration);
+    if (server == null) {
+      return;
+    }
 
+    LocalAppEngineServerBehaviour serverBehaviour =
+        (LocalAppEngineServerBehaviour) server.loadAdapter(LocalAppEngineServerBehaviour.class, null);
+
+    serverBehaviour.setStarted();
+    
+    System.out.println("Server started. (Not really)");
   }
 
 }
