c6e4db412431b3890ba1fa7c9f8e7fc3ae5980f2
Elliotte Rusty Harold <elharo@google.com>
2016-06-15 11:23:59 -0400

    reformat

--- plugins/com.google.cloud.tools.eclipse.appengine.localserver/src/com/google/cloud/tools/eclipse/appengine/localserver/server/TargetPlatform.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.localserver/src/com/google/cloud/tools/eclipse/appengine/localserver/server/TargetPlatform.java
@@ -25,16 +25,17 @@ public class TargetPlatform {
   }
 
   static MessageConsole findConsole(String name) {
-      ConsolePlugin plugin = ConsolePlugin.getDefault();
-      IConsoleManager conMan = plugin.getConsoleManager();
-      IConsole[] existing = conMan.getConsoles();
-      for (int i = 0; i < existing.length; i++)
-         if (name.equals(existing[i].getName()))
-            return (MessageConsole) existing[i];
-      //no console found, so create a new one
-      MessageConsole myConsole = new MessageConsole(name, null);
-      conMan.addConsoles(new IConsole[]{myConsole});
-      return myConsole;
+    ConsolePlugin plugin = ConsolePlugin.getDefault();
+    IConsoleManager manager = plugin.getConsoleManager();
+    IConsole[] existing = manager.getConsoles();
+    for (int i = 0; i < existing.length; i++)
+       if (name.equals(existing[i].getName())) {
+          return (MessageConsole) existing[i];
+       }
+    // console not found, so create a new one
+    MessageConsole console = new MessageConsole(name, null);
+    manager.addConsoles(new IConsole[]{console});
+    return console;
    }
 
 }
