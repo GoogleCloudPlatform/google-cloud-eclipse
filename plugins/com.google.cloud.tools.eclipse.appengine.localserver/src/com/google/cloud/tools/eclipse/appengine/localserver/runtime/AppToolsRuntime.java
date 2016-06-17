d3188f5a6464540a433910a11775df964561b59d
Elliotte Rusty Harold <elharo@google.com>
2016-06-17 10:33:30 -0400

    rationalize IDs

--- plugins/com.google.cloud.tools.eclipse.appengine.localserver/src/com/google/cloud/tools/eclipse/appengine/localserver/runtime/AppToolsRuntime.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.localserver/src/com/google/cloud/tools/eclipse/appengine/localserver/runtime/AppToolsRuntime.java
@@ -2,6 +2,6 @@ package com.google.cloud.tools.eclipse.appengine.localserver.runtime;
 
 import org.eclipse.wst.server.core.model.RuntimeDelegate;
 
-public class AppToolsRuntime  extends RuntimeDelegate {
+public class AppToolsRuntime extends RuntimeDelegate {
 
 }
