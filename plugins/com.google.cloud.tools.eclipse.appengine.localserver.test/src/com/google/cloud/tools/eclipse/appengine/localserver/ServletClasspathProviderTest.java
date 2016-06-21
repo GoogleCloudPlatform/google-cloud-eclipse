738d66b092d42a68caddf9214b1c623f206d2e1f
Elliotte Rusty Harold <elharo@google.com>
2016-06-21 09:59:57 -0400

    unit test for ServletClasspathProvider

---
+++ plugins/com.google.cloud.tools.eclipse.appengine.localserver.test/src/com/google/cloud/tools/eclipse/appengine/localserver/ServletClasspathProviderTest.java
@@ -0,0 +1,20 @@
+package com.google.cloud.tools.eclipse.appengine.localserver;
+
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.junit.Assert;
+import org.junit.Test;
+
+// Must Run as JUnit Plug-in Test in Eclipse.
+public class ServletClasspathProviderTest {
+
+  private ServletClasspathProvider provider = new ServletClasspathProvider();
+  
+  @Test
+  public void testResolveClasspathContainer() {
+     IClasspathEntry[] result = provider.resolveClasspathContainer(null, null);
+     Assert.assertEquals(2, result.length);
+     Assert.assertTrue(result[0].getPath().toString().endsWith("servlet-api.jar"));
+     Assert.assertTrue(result[0].getPath().toString().endsWith("jsp-api.jar"));
+  }
+
+}
