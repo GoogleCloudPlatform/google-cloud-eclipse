c4551263add613e9cf2eb7fa700ce13a8942f469
Andras Kerekes <andras.kerekes.work@gmail.com>
2016-07-01 16:25:51 -0400

    test for AppEngineStandardDeployCommandHandler

--- plugins/com.google.cloud.tools.eclipse.appengine.deploy.test/src/com/google/cloud/tools/eclipse/appengine/deploy/AppEngineStandardDeployCommandHandlerTest.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.deploy.test/src/com/google/cloud/tools/eclipse/appengine/deploy/AppEngineStandardDeployCommandHandlerTest.java
@@ -2,9 +2,12 @@ package com.google.cloud.tools.eclipse.appengine.deploy;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
+
 
 public class AppEngineStandardDeployCommandHandlerTest {
 
+  @Test
   public void testIsEnabled() {
     assertTrue(new AppEngineStandardDeployCommandHandler().isEnabled());
   }
