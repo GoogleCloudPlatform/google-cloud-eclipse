60949249862eea653ae6b5572ae427635f6ab12e
Chanseok Oh <chanseok@google.com>
2016-07-18 17:54:46 -0400

    Fill in client id and secret at compile time

---
+++ plugins/com.google.cloud.tools.eclipse.appengine.login/java-templates/com/google/cloud/tools/eclipse/appengine/login/Constants.java
@@ -0,0 +1,7 @@
+package com.google.cloud.tools.eclipse.appengine.login;
+
+public class Constants {
+
+  public static final String OAUTH_CLIENT_ID = "@oauth.client.id@";
+  public static final String OAUTH_CLIENT_SECRET = "@oauth.client.secret@";
+}
