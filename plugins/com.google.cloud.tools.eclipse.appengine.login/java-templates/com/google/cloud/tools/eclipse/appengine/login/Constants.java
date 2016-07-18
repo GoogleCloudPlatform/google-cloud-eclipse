83837e39518f9f0fcc5948bce9eceed9df8ee58c
Chanseok Oh <chanseok@google.com>
2016-07-18 18:06:29 -0400

    Add comment / fix minor build.properties error

--- plugins/com.google.cloud.tools.eclipse.appengine.login/java-templates/com/google/cloud/tools/eclipse/appengine/login/Constants.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.login/java-templates/com/google/cloud/tools/eclipse/appengine/login/Constants.java
@@ -1,5 +1,8 @@
 package com.google.cloud.tools.eclipse.appengine.login;
 
+/**
+ * Placeholder constants initialized at compile-time.
+ */
 public class Constants {
 
   public static final String OAUTH_CLIENT_ID = "@oauth.client.id@";
