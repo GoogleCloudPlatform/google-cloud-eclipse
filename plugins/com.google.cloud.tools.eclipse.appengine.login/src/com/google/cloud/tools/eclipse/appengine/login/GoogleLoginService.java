d605d1febde5e7794469ef3338f3cd5d5e3794f5
Chanseok Oh <chanseok@google.com>
2016-07-21 18:02:23 -0400

    Integrate the new login browser

--- plugins/com.google.cloud.tools.eclipse.appengine.login/src/com/google/cloud/tools/eclipse/appengine/login/GoogleLoginService.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.login/src/com/google/cloud/tools/eclipse/appengine/login/GoogleLoginService.java
@@ -16,20 +16,15 @@
 package com.google.cloud.tools.eclipse.appengine.login;
 
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleOAuthConstants;
 import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.googleapis.util.Utils;
+import com.google.cloud.tools.eclipse.appengine.login.ui.GoogleLoginBrowser;
 
 import org.eclipse.e4.ui.model.application.MApplication;
-import org.eclipse.jface.dialogs.InputDialog;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.window.IShellProvider;
-import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -70,33 +65,17 @@ public class GoogleLoginService {
   }
 
   private Credential logIn(IShellProvider shellProvider) throws IOException {
-    GoogleAuthorizationCodeRequestUrl requestUrl =
-        new GoogleAuthorizationCodeRequestUrl(Constants.getOAuthClientId(),
-                                              GoogleOAuthConstants.OOB_REDIRECT_URI,
-                                              OAUTH_SCOPES);
-
-    try {
-      IWorkbenchBrowserSupport browserSupport = PlatformUI.getWorkbench().getBrowserSupport();
-      browserSupport.getExternalBrowser().openURL(requestUrl.toURL());
-    } catch (PartInitException pie) {
-      MessageDialog.openError(shellProvider.getShell(),
-          Messages.ERROR_BROWSER_LAUNCHING, pie.getMessage());
+    GoogleLoginBrowser loginBrowser = new GoogleLoginBrowser(
+        shellProvider.getShell(), Constants.getOAuthClientId(), OAUTH_SCOPES);
+    if (loginBrowser.open() != GoogleLoginBrowser.OK) {
       return null;
     }
 
-    InputDialog dialog = new InputDialog(shellProvider.getShell(),
-        Messages.ENTER_VERIFICATION_CODE_DIALOG_TITLE,
-        Messages.ENTER_VERIFICATION_CODE_DIALOG_MESSAGE, null, null);
-    if (dialog.open() != InputDialog.OK) {
-      return null;
-    }
-
-    String verificationCode = dialog.getValue();
     GoogleAuthorizationCodeTokenRequest authRequest = new GoogleAuthorizationCodeTokenRequest(
         Utils.getDefaultTransport(), Utils.getDefaultJsonFactory(),
         Constants.getOAuthClientId(), Constants.getOAuthClientSecret(),
-        verificationCode,
-        GoogleOAuthConstants.OOB_REDIRECT_URI);
+        loginBrowser.getAuthorizationCode(),
+        GoogleLoginBrowser.REDIRECT_URI);
 
     return createCredential(authRequest.execute());
   }
--- plugins/com.google.cloud.tools.eclipse.appengine.login/src/com/google/cloud/tools/eclipse/appengine/login/Messages.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.login/src/com/google/cloud/tools/eclipse/appengine/login/Messages.java
@@ -4,9 +4,7 @@ import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
   private static final String BUNDLE_NAME = "com.google.cloud.tools.eclipse.appengine.login.messages"; //$NON-NLS-1$
-  public static String ENTER_VERIFICATION_CODE_DIALOG_MESSAGE;
-  public static String ENTER_VERIFICATION_CODE_DIALOG_TITLE;
-  public static String ERROR_BROWSER_LAUNCHING;
+  public static String LOGIN_BROWSER_TITLE;
   static {
     // initialize resource bundle
     NLS.initializeMessages(BUNDLE_NAME, Messages.class);
