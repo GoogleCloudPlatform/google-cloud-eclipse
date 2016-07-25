cc6f74be251633742a85d94d3195c4d642900877
Elliotte Rusty Harold <elharo@google.com>
2016-07-25 06:31:14 -0400

    start workbench if necessary

--- plugins/com.google.cloud.tools.eclipse.appengine.localserver.test/src/com/google/cloud/tools/eclipse/appengine/localserver/ui/AppToolsTabGroupTest.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.localserver.test/src/com/google/cloud/tools/eclipse/appengine/localserver/ui/AppToolsTabGroupTest.java
@@ -1,10 +1,26 @@
 package com.google.cloud.tools.eclipse.appengine.localserver.ui;
 
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class AppToolsTabGroupTest {
 
+  @Before 
+  public void createWorkbench() {
+    if(!PlatformUI.isWorkbenchRunning()) {
+      WorkbenchAdvisor workbenchAdvisor = new WorkbenchAdvisor() {
+        @Override
+        public String getInitialWindowPerspectiveId() {
+          return null;
+        }
+      };
+      PlatformUI.createAndRunWorkbench(PlatformUI.createDisplay(), workbenchAdvisor); 
+    }
+  }
+  
   @Test
   public void testCreateTabs() {
     AppToolsTabGroup group = new AppToolsTabGroup();
