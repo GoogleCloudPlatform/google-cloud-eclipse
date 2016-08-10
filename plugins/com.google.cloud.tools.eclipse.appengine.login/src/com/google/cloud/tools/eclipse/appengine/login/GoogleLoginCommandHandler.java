48094032cf6d4832371bd9cab9087fbc22a3c0ea
Chanseok Oh <chanseok@google.com>
2016-08-10 12:26:52 -0400

    Add comments

--- plugins/com.google.cloud.tools.eclipse.appengine.login/src/com/google/cloud/tools/eclipse/appengine/login/GoogleLoginCommandHandler.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.login/src/com/google/cloud/tools/eclipse/appengine/login/GoogleLoginCommandHandler.java
@@ -19,13 +19,13 @@ public class GoogleLoginCommandHandler extends AbstractHandler implements IEleme
   public Object execute(ExecutionEvent event) throws ExecutionException {
     IGoogleLoginService loginService = ServiceUtils.getService(event, IGoogleLoginService.class);
 
-    Credential credential = loginService.getCachedActiveCredential();
+    Credential credential = loginService.getCachedActiveCredential();  // See if already logged in.
     if (credential == null) {
-      loginService.getActiveCredential();
+      loginService.getActiveCredential();  // Log in.
     } else {
       if (MessageDialog.openConfirm(HandlerUtil.getActiveShell(event),
           Messages.LOGOUT_CONFIRM_DIALOG_TITILE, Messages.LOGOUT_CONFIRM_DIALOG_MESSAGE)) {
-        loginService.clearCredential();
+        loginService.clearCredential();  // Log out on confirmation.
       }
     }
 
