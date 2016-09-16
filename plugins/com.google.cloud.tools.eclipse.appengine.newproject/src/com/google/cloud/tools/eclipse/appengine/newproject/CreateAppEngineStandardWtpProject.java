c4ef7f288915f574aea7d8dd57bf8394454b30e3
Andras Kerekes <andras.kerekes.work@gmail.com>
2016-09-16 10:52:52 -0400

    cleanup

--- plugins/com.google.cloud.tools.eclipse.appengine.newproject/src/com/google/cloud/tools/eclipse/appengine/newproject/CreateAppEngineStandardWtpProject.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.newproject/src/com/google/cloud/tools/eclipse/appengine/newproject/CreateAppEngineStandardWtpProject.java
@@ -1,5 +1,9 @@
 package com.google.cloud.tools.eclipse.appengine.newproject;
 
+import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
+import java.util.Arrays;
+
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
@@ -8,7 +12,6 @@ import org.eclipse.core.resources.ProjectScope;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.jdt.core.IAccessRule;
@@ -16,8 +19,7 @@ import org.eclipse.jdt.core.IClasspathAttribute;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.internal.junit.buildpath.BuildPathSupport;
-import org.eclipse.jdt.internal.junit.buildpath.JUnitContainerInitializer;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.junit.JUnitCore;
 import org.eclipse.jst.j2ee.classpathdep.UpdateClasspathAttributeUtil;
 import org.eclipse.ui.actions.WorkspaceModifyOperation;
@@ -26,9 +28,6 @@ import org.eclipse.wst.common.project.facet.core.IFacetedProject;
 import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
 
 import com.google.cloud.tools.eclipse.appengine.facets.AppEngineStandardFacet;
-import java.lang.reflect.InvocationTargetException;
-import java.net.URI;
-import java.util.Arrays;
 
 /**
 * Utility to make a new Eclipse project with the App Engine Standard facets in the workspace.  
@@ -73,16 +72,21 @@ class CreateAppEngineStandardWtpProject extends WorkspaceModifyOperation {
     
     setProjectIdPreference(newProject);
 
-    // add JUnit to project
+    addJunit4ToClasspath(monitor, newProject);
+  }
+
+  private void addJunit4ToClasspath(IProgressMonitor monitor, final IProject newProject) throws CoreException,
+                                                                                         JavaModelException {
     IJavaProject javaProject = JavaCore.create(newProject);
-    
-    IClasspathEntry junitVariable = JavaCore.newContainerEntry(JUnitCore.JUNIT4_CONTAINER_PATH, new IAccessRule[0], new IClasspathAttribute[]{ UpdateClasspathAttributeUtil.createNonDependencyAttribute() }, false);
-    
+    IClasspathAttribute nonDependencyAttribute = UpdateClasspathAttributeUtil.createNonDependencyAttribute();
+    IClasspathEntry junit4Container = JavaCore.newContainerEntry(JUnitCore.JUNIT4_CONTAINER_PATH,
+                                                                 new IAccessRule[0],
+                                                                 new IClasspathAttribute[]{ nonDependencyAttribute },
+                                                                 false);
     IClasspathEntry[] rawClasspath = javaProject.getRawClasspath();
     IClasspathEntry[] newRawClasspath = Arrays.copyOf(rawClasspath, rawClasspath.length + 1);
-    newRawClasspath[newRawClasspath.length - 1] = junitVariable;
+    newRawClasspath[newRawClasspath.length - 1] = junit4Container;
     javaProject.setRawClasspath(newRawClasspath, monitor);
-    new JUnitContainerInitializer().initialize(JUnitCore.JUNIT4_CONTAINER_PATH, javaProject);
   }
 
   void setProjectIdPreference(IProject project) {
