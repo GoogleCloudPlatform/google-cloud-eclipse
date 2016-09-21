d2659c531c099ee5b72b5fd9f71959fda0f9f43f
Chanseok Oh <chanseok@google.com>
2016-09-21 11:26:13 -0400

    Use new LocalServerReceiver

--- plugins/com.google.cloud.tools.eclipse.appengine.login/src/com/google/cloud/tools/eclipse/appengine/login/ui/LoginServiceUi.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.login/src/com/google/cloud/tools/eclipse/appengine/login/ui/LoginServiceUi.java
@@ -17,7 +17,6 @@ package com.google.cloud.tools.eclipse.appengine.login.ui;
 
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl;
-import com.google.api.client.repackaged.com.google.common.annotations.VisibleForTesting;
 import com.google.cloud.tools.eclipse.appengine.login.GoogleLoginService;
 import com.google.cloud.tools.eclipse.appengine.login.Messages;
 import com.google.cloud.tools.eclipse.usagetracker.AnalyticsEvents;
@@ -26,9 +25,6 @@ import com.google.cloud.tools.ide.login.UiFacade;
 import com.google.cloud.tools.ide.login.VerificationCodeHolder;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
@@ -41,17 +37,12 @@ import org.eclipse.ui.services.IServiceLocator;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.concurrent.Semaphore;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class LoginServiceUi implements UiFacade {
 
   private static final Logger logger = Logger.getLogger(LoginServiceUi.class.getName());
-
-  private static final String ERROR_MARKER_USER_CANCELED_LOGIN = "canceled-by-user"; //$NON-NLS-1$
 
   private IServiceLocator serviceLocator;
   private IShellProvider shellProvider;
@@ -115,20 +106,26 @@ public class LoginServiceUi implements UiFacade {
       return null;
 
     } catch (IOException ioe) {
-      // Don't show an error dialog if a user pressed the cancel button.
-      if (!ioe.getMessage().contains(ERROR_MARKER_USER_CANCELED_LOGIN)) {
-        showErrorDialogHelper(Messages.LOGIN_ERROR_DIALOG_TITLE,
-            Messages.LOGIN_ERROR_LOCAL_SERVER_RUN + ioe.getLocalizedMessage());
-      }
+      showErrorDialogHelper(Messages.LOGIN_ERROR_DIALOG_TITLE,
+          Messages.LOGIN_ERROR_LOCAL_SERVER_RUN + ioe.getLocalizedMessage());
       return null;
+    }
+    finally {
+      stopLocalServerReceiver(codeReceiver);
+    }
+  }
+
+  private void stopLocalServerReceiver(LocalServerReceiver codeReceiver) {
+    try {
+      codeReceiver.stop();
+    } catch (IOException ex) {
+      logger.log(Level.WARNING, "Failed to stop the local web server for login.", ex); //$NON-NLS-1$
     }
   }
 
   private String showProgressDialogAndWaitForCode(final String message,
       final LocalServerReceiver codeReceiver, final String redirectUrl) throws IOException {
     try {
-      final Semaphore wait = new Semaphore(0 /* initially zero permit */);
-
       final ProgressMonitorDialog dialog = new ProgressMonitorDialog(shellProvider.getShell()) {
         @Override
         protected void configureShell(Shell shell) {
@@ -137,8 +134,7 @@ public class LoginServiceUi implements UiFacade {
         }
         @Override
         protected void cancelPressed() {
-          wait.release();  // Allow termination of the attached task.
-          stopCodeWaitingJob(redirectUrl);
+          stopLocalServerReceiver(codeReceiver);
 
           AnalyticsPingManager.getInstance().sendPing(
               AnalyticsEvents.LOGIN_CANCELED, null, null, getParentShell());
@@ -146,8 +142,6 @@ public class LoginServiceUi implements UiFacade {
       };
 
       final String[] codeHolder = new String[1];
-      final IOException[] exceptionHolder = new IOException[1];
-
       dialog.run(true /* fork */, true /* cancelable */, new IRunnableWithProgress() {
         @Override
         public void run(IProgressMonitor monitor)
@@ -157,98 +151,20 @@ public class LoginServiceUi implements UiFacade {
 
           monitor.beginTask(message != null ? message : Messages.LOGIN_PROGRESS_DIALOG_MESSAGE,
               IProgressMonitor.UNKNOWN);
-          // Fork another sub-job to circumvent the limitation of LocalServerReceiver.
-          // (See the comments of scheduleCodeWaitingJob().)
-          scheduleCodeWaitingJob(
-              new LocalServerReceiverWrapper(codeReceiver), wait, codeHolder, exceptionHolder);
-          wait.acquireUninterruptibly();  // Block until signaled.
+          try {
+            codeHolder[0] = codeReceiver.waitForCode();
+          } catch (IOException ioe) {
+            throw new InvocationTargetException(ioe);
+          }
         }
       });
-
-      if (exceptionHolder[0] != null) {
-        throw exceptionHolder[0];
-      }
       return codeHolder[0];
 
-    } catch (InvocationTargetException | InterruptedException ex) {
-      // Never thrown from the attached task.
+    } catch (InvocationTargetException ex) {
+      throw (IOException) ex.getTargetException();
+    } catch (InterruptedException ex) {  // Never thrown from the attached task.
       return null;
     }
-  }
-
-  /**
-   * Schedule and run a job that calls {@link LocalServerReceiver#waitForCode}. The reason for
-   * creating another job inside the job of {@link showProgressDialogAndWaitForCode} is that we
-   * cannot have a 100%-guarantee that {@link LocalServerReceiver#waitForCode} will eventually
-   * return. (If {@link stopCodeWaitingJob} fails to stop {@link LocalServerReceiver#waitForCode}
-   * gracefully, users will be stuck and the IDE has to be killed forcibly.)
-   *
-   * However, under normal circumstances, {@link stopCodeWaitingJob} will succeed to terminate
-   * this sub-job.
-   *
-   * @return scheduled job
-   */
-  @VisibleForTesting
-  Job scheduleCodeWaitingJob(
-      final LocalServerReceiverWrapper codeReceiver, final Semaphore wait,
-      final String[] codeHolder, final IOException[] exceptionHolder) {
-    Job codeWaitingJob = new Job("Waiting for Authorization Code") { //$NON-NLS-1$
-      @Override
-      protected IStatus run(IProgressMonitor monitor) {
-        try {
-          codeHolder[0] = codeReceiver.waitForCode();
-        } catch (IOException ioe) {
-          exceptionHolder[0] = ioe;
-        }
-        finally {
-          wait.release();  // Terminate the task attached to the ProgressMonitorDialog.
-          try {
-            codeReceiver.stop();
-          } catch (IOException ioe) {
-            logger.log(Level.WARNING,
-                "Failed to stop the local web server for login.", ioe); //$NON-NLS-1$
-          }
-        }
-        return Status.OK_STATUS;
-      }
-    };
-    codeWaitingJob.setSystem(true);  // Hide the job from UI.
-    codeWaitingJob.schedule();
-    return codeWaitingJob;
-  }
-
-  /**
-   * Stops the background task of {@link showProgressDialogAndWaitForCode} by sending a login
-   * error (as an HTTP request) to the local server. {@link LocalServerReceiver#waitForCode} will
-   * subsequently throw an {@link IOException}.
-   */
-  @VisibleForTesting
-  void stopCodeWaitingJob(final String redirectUrl) {
-    // Wrap in a Job for the case where making HTTP connections takes a long time.
-    new Job("Terminating Authorization Code Receiver") { //$NON-NLS-1$
-      @Override
-      protected IStatus run(IProgressMonitor monitor) {
-        HttpURLConnection connection = null;
-
-        try {
-          URL url = new URL(redirectUrl
-              + "?error=" + ERROR_MARKER_USER_CANCELED_LOGIN); //$NON-NLS-1$
-          connection = (HttpURLConnection) url.openConnection();
-          int responseCode = connection.getResponseCode();
-          if (responseCode != HttpURLConnection.HTTP_OK) {
-            logger.log(Level.WARNING,
-                "Error terminating code waiting job. Response: " + responseCode); //$NON-NLS-1$
-          }
-        } catch (IOException ioe) {
-          logger.log(Level.WARNING, "Error terminating code waiting job", ioe); //$NON-NLS-1$
-        } finally {
-          if (connection != null) {
-            connection.disconnect();
-          }
-        }
-        return Status.OK_STATUS;
-      }
-    }.schedule();
   }
 
   @Override
@@ -256,25 +172,4 @@ public class LoginServiceUi implements UiFacade {
       String title, GoogleAuthorizationCodeRequestUrl authCodeRequestUrl) {
     throw new RuntimeException("Not to be called."); //$NON-NLS-1$
   }
-
-  /**
-   * A wrapper class that delegates methods to {@link LocalServerReceiver}. Defined only to allow
-   * unit testing, since Mockito cannot mock {@link LocalServerReceiver}, which is {@code final}.
-   */
-  public static class LocalServerReceiverWrapper {
-
-    private LocalServerReceiver receiver;
-
-    public LocalServerReceiverWrapper(LocalServerReceiver receiver) {
-      this.receiver = receiver;
-    }
-
-    public String waitForCode() throws IOException {
-      return receiver.waitForCode();
-    }
-
-    public void stop() throws IOException {
-      receiver.stop();
-    }
-  };
 }
