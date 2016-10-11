9510d2b62c9c2f8aedbe38cf6f2a4754f04f5e74
Chanseok Oh <chanseok@google.com>
2016-10-11 13:17:46 -0400

    Validate/save account email selection for deploy

--- plugins/com.google.cloud.tools.eclipse.appengine.login/src/com/google/cloud/tools/eclipse/appengine/login/ui/AccountSelector.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.login/src/com/google/cloud/tools/eclipse/appengine/login/ui/AccountSelector.java
@@ -26,6 +26,7 @@ import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 
@@ -67,6 +68,17 @@ public class AccountSelector extends Composite {
     return selectedCredential;
   }
 
+  public String getSelectedEmail() {
+    return combo.getText();
+  }
+
+  public void selectAccount(String email) {
+    int index = combo.indexOf(email);
+    if (index != -1) {
+      combo.select(index);
+    }
+  }
+
   @VisibleForTesting
   class LogInOnSelect extends SelectionAdapter {
     @Override
@@ -76,7 +88,7 @@ public class AccountSelector extends Composite {
         if (account != null) {
           addAndSelectAccount(account);
         } else {
-          combo.deselect(combo.getSelectionIndex());
+          combo.deselectAll();
         }
       }
 
@@ -100,5 +112,13 @@ public class AccountSelector extends Composite {
       combo.setData(account.getEmail(), account.getOAuth2Credential());
       combo.select(0);
     }
+  }
+
+  public void addSelectionListener(SelectionListener listener) {
+    combo.addSelectionListener(listener);
+  }
+
+  public void removeSelectionListener(SelectionListener listener) {
+    combo.removeSelectionListener(listener);
   }
 }
