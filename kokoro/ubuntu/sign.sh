5d8e91b9e451c117ccd0fac23865f95b27c453c8
Elliotte Rusty Harold <elharo@google.com>
2016-11-17 05:40:23 -0500

    simplify script

--- kokoro/ubuntu/sign.sh
+++ kokoro/ubuntu/sign.sh
@@ -7,37 +7,38 @@ set -x
 
 echo ${KOKORO_GFILE_DIR}
 cd $KOKORO_GFILE_DIR
+
 mkdir -p signed && chmod 777 signed
 mkdir -p signed/plugins && chmod 777 signed/plugins
 mkdir -p signed/features && chmod 777 signed/features
  
-cp $KOKORO_GFILE_DIR/artifacts.jar $KOKORO_GFILE_DIR/signed/artifacts.jar
-cp $KOKORO_GFILE_DIR/content.jar $KOKORO_GFILE_DIR/signed/content.jar
-cp $KOKORO_GFILE_DIR/category.xml $KOKORO_GFILE_DIR/signed/category.xml
+cp artifacts.jar signed/artifacts.jar
+cp content.jar signed/content.jar
+cp category.xml signed/category.xml
 
-FILES=$KOKORO_GFILE_DIR/plugins/*.jar
+FILES=plugins/*.jar
 for f in $FILES
 do
   echo "Processing $f file..."
-  filename=$(echo $f | sed 's/\/\(.*\)\/\(.*\)\.jar/\2.jar/g')
+  filename=$(basename "$f" .jar)
   echo "Signing $filename"
   if /escalated_sign/escalated_sign.py -j /escalated_sign_jobs -t signjar \
-    $KOKORO_GFILE_DIR/plugins/$filename \
-    $KOKORO_GFILE_DIR/signed/plugins/$filename
+    plugins/$filename \
+    signed/plugins/$filename
   then echo "Signed $filename"
   else 
-    cp $KOKORO_GFILE_DIR/plugins/$filename $KOKORO_GFILE_DIR/signed/plugins/$filename
+    cp plugins/$filename signed/plugins/$filename
   fi
 done
 
-FEATURES=$KOKORO_GFILE_DIR/features/*.jar
+FEATURES=features/*.jar
 for f in $FEATURES
 do
   echo "Processing $f file..."
   filename=$(echo $f | sed 's/\/\(.*\)\/\(.*\)\.jar/\2.jar/g')
   echo "Signing $filename"
   /escalated_sign/escalated_sign.py -j /escalated_sign_jobs -t signjar \
-    $KOKORO_GFILE_DIR/features/$filename \
-    $KOKORO_GFILE_DIR/signed/features/$filename
+    features/$filename \
+    signed/features/$filename
   echo "Signed $filename"
 done
