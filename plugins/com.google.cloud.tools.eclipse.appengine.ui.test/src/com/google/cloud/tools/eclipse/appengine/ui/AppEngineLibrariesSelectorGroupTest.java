9c99ee69b3d3a44f9faf406ab96278c60ee19fcd
Elliotte Rusty Harold <elharo@google.com>
2017-01-08 06:57:24 -0500

    caps

--- plugins/com.google.cloud.tools.eclipse.appengine.ui.test/src/com/google/cloud/tools/eclipse/appengine/ui/AppEngineLibrariesSelectorGroupTest.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.ui.test/src/com/google/cloud/tools/eclipse/appengine/ui/AppEngineLibrariesSelectorGroupTest.java
@@ -65,7 +65,7 @@ public class AppEngineLibrariesSelectorGroupTest {
   }
   
   @Test
-  public void testTooltips() {
+  public void testToolTips() {
     assertTrue(appengineButton.getToolTipText().length() > 0);
     assertTrue(endpointsButton.getToolTipText().length() > 0);
     assertTrue(objectifyButton.getToolTipText().length() > 0);
