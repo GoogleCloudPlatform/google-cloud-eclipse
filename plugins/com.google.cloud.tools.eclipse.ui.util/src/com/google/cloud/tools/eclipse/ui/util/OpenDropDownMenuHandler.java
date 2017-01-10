c7157898debe90d78465e1ee0171d6ff706a8fa9
Brian de Alwis <bsd@mt.ca>
2017-01-10 14:14:24 -0500

    Address review issues   - fix indentation   - Strip out `menuId` support from  OpenDropDownMenuHandler and make it     specific to tool item contributions

--- plugins/com.google.cloud.tools.eclipse.ui.util/src/com/google/cloud/tools/eclipse/ui/util/OpenDropDownMenuHandler.java
+++ plugins/com.google.cloud.tools.eclipse.ui.util/src/com/google/cloud/tools/eclipse/ui/util/OpenDropDownMenuHandler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.google.cloud.tools.eclipse.ui.util;
 
@@ -15,29 +30,51 @@ import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.ToolItem;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.menus.IMenuService;
 
 /**
- * A command handler for commands representing drop-down Menu contribution item. This handler just
- * opens drop-down menu, since Eclipse behavior for such items is to open drop-down menu only by
- * clicking on small arrow. Drop-down Menu contribution items without any handler shown as disabled.
+ * A handler for submenu-style tool bar command contributions that are intended to be a placeholder
+ * to open a drop-down menu. This handler is required as Eclipse otherwise only opens the drop-down
+ * menu when clicking on the small drop-down arrow. The popup menu is populated using normal menu
+ * contributions found via the Eclipse {@link IMenuService}, and the menu ID is taken from the
+ * command contribution's {@code id}.
+ * <p>
+ * <b>Example</b>
+ * 
+ * <pre>
+ * &lt;extension point=&quot;org.eclipse.ui.menus&quot;&gt;
+ *    &lt;menuContribution locationURI=&quot;toolbar:org.eclipse.ui.main.toolbar?after=additions&quot;&gt;
+ *       &lt;toolbar id=&quot;com.google.cloud.tools.eclipse.appengine.toolbar&quot;&gt;
+ *          &lt;command
+ *                style=&quot;pulldown&quot;
+ *                commandId=&quot;com.google.cloud.tools.eclipse.ui.util.showPopup&quot;
+ *                id=&quot;com.google.cloud.tools.eclipse.appengine.actions&quot;
+ *                icon=&quot;xxx.png&quot; /&gt;
+ *       &lt;/toolbar&gt;
+ *    &lt;/menuContribution&gt;
+ *    &lt;menuContribution locationURI=&quot;menu:com.google.cloud.tools.eclipse.appengine.actions&quot;&gt;
+ *       &lt;command commandId=&quot;...&quot;/&gt;
+ *    &lt;/menuContribution&gt;
+ * &lt;/extension&gt;
+ * </pre>
  */
 public final class OpenDropDownMenuHandler extends AbstractHandler {
 
   @Override
   public Object execute(ExecutionEvent event) throws ExecutionException {
-    ToolItem toolItem = checkToolItem(event);
+    ToolItem toolItem = findToolItem(event);
     String menuId = getMenuId(event, toolItem);
-    IMenuService menuService = getService(event, IMenuService.class);
+    IMenuService menuService = ServiceUtils.getService(event, IMenuService.class);
     openDropDownMenu(menuId, toolItem, menuService);
     return null;
   }
 
   /**
    * Ensure we're being executed as a command tool item with style {@code DROP_DOWN}.
+   * 
+   * @return the {@link ToolItem} or
    */
-  private ToolItem checkToolItem(ExecutionEvent event) throws ExecutionException {
+  private static ToolItem findToolItem(ExecutionEvent event) throws ExecutionException {
     if (event.getTrigger() instanceof Event) {
       Event swtEvent = (Event) event.getTrigger();
       if (swtEvent.widget instanceof ToolItem) {
@@ -48,18 +85,15 @@ public final class OpenDropDownMenuHandler extends AbstractHandler {
         }
       }
     }
-    throw new ExecutionException("Invalid toolitem");
+    throw new ExecutionException("Invalid tool item");
   }
 
   /**
-   * Retrieve the menu id to show either from the event's {@code menuId} parameter or from the
-   * contribution item's ID (following the documented approach for DROP_DOWN items).
+   * Retrieve the menu id to show from the contribution item's ID (following the documented approach
+   * for DROP_DOWN items).
    */
-  private String getMenuId(ExecutionEvent event, ToolItem toolItem) throws ExecutionException {
-    String menuId = event.getParameter("menuId");
-    if (!Strings.isNullOrEmpty(menuId)) {
-      return menuId;
-    }
+  private static String getMenuId(ExecutionEvent event, ToolItem toolItem)
+      throws ExecutionException {
     if (toolItem.getData() instanceof ContributionItem) {
       ContributionItem data = (ContributionItem) toolItem.getData();
       if (!Strings.isNullOrEmpty(data.getId())) {
@@ -69,14 +103,10 @@ public final class OpenDropDownMenuHandler extends AbstractHandler {
     throw new ExecutionException("Unable to determine menu ID");
   }
 
-  private <T> T getService(ExecutionEvent event, Class<T> clazz) {
-    return PlatformUI.getWorkbench().getService(clazz);
-  }
-
   /**
    * Opens drop-down menu.
    */
-  private void openDropDownMenu(final String menuId, final ToolItem toolItem,
+  private static void openDropDownMenu(final String menuId, final ToolItem toolItem,
       final IMenuService menuService) {
     final MenuManager menuManager = new MenuManager();
     Menu menu = menuManager.createContextMenu(toolItem.getParent());
