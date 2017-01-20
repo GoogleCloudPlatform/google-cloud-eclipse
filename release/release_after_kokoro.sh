b4c422c22b72dc4be231319baabe0a272351c4de
Chanseok Oh <chanseok@google.com>
2017-01-19 23:26:44 -0500

    Simplify code

--- release/release_after_kokoro.sh
+++ release/release_after_kokoro.sh
@@ -3,22 +3,26 @@
 # This script starts with Step 14 in the release process (i.e., after Kokoro
 # has built and signed the binaries.)
 #
-# It is safe to re-run this script again when something goes wrong, provided
+# It is safe to rerun this script again when something goes wrong, provided
 # that you didn't complete the final step, which means you are done. (The final
-# step grants public access to files uploaded to cloud-tools-for-eclipse.)
+# step grants public access to the files uploaded to cloud-tools-for-eclipse.)
 #
 # Note: this script clears and creates "$HOME/CT4E_release_work" (after
-# confirmation) for temporary work directory, which can be inspected later
+# confirmation) for a temporary work directory, which can be inspected later
 # and/or deleted safely anytime.
 
-if [ -z "$ECLIPSE_HOME" ]; then
-    echo 'The release steps require running an Eclipse binary.'
-    echo 'Set $ECLIPSE_HOME before running this script.'
+die() {
+    for line in "$@"; do
+        echo $line
+    done
     exit 1
-elif [ ! -x "$ECLIPSE_HOME/eclipse" ]; then
-    echo "'$ECLIPSE_HOME/eclipse' is not an executable. Halting."
-    exit 1
-fi
+}
+
+[ -z "$ECLIPSE_HOME" ] && \
+    die 'The release steps require running an Eclipse binary.' \
+        'Set $ECLIPSE_HOME before running this script.'
+[ ! -x "$ECLIPSE_HOME/eclipse" ] && \
+    die "'$ECLIPSE_HOME/eclipse' is not an executable. Halting."
 
 WORK_DIR=$HOME/CT4E_release_work
 SIGNED_DIR=$WORK_DIR/signed
@@ -65,9 +69,8 @@ mkdir $SIGNED_DIR && \
 set +x
 
 if [ 0 -eq $( ls -1 $SIGNED_DIR | wc -l ) ]; then
-    echo "'$SIGNED_DIR' is empty. Wrong GCS bucket URL provided?"
-    echo "Fix the problem and rerun the script. Halting."
-    exit 1
+    die "'$SIGNED_DIR' is empty. Wrong GCS bucket URL provided?" \
+        "Fix the problem and rerun the script. Halting."
 fi
 
 echo "#"
@@ -77,8 +80,7 @@ for jar in $SIGNED_DIR/plugins/com.google.cloud.tools.eclipse.*.jar \
         $SIGNED_DIR/features/com.google.cloud.tools.eclipse.*.jar; do
     echo -n "$( basename $jar ): "
     if ! jarsigner -strict -verify $jar; then
-        echo "unsigned artifact. Halting."
-        exit 1
+        die "unsigned artifact. Halting."
     fi
 done
 
@@ -100,10 +102,8 @@ ANALYTICS_CONSTANT=$( javap -classpath \
 echo -e "$LOGIN_CONSTANTS\n$ANALYTICS_CONSTANT"
 
 # Verify if the constants have been injected.
-if echo -e "$LOGIN_CONSTANTS\n$ANALYTICS_CONSTANT" | \
-        grep -q '@oauth.client\|@ga.tracking.id@'; then
-    echo "Some constant(s) have not been injected. Halting."
-    exit 1
+if [[ "${LOGIN_CONSTANTS}${ANALYTICS_CONSTANT}" =~ "@" ]]; then
+    die "Some constant(s) have not been injected. Halting."
 fi
 
 # Verify if the injected constants have the right lengths.
@@ -113,16 +113,12 @@ OAUTH_CLIENT_SECRET=$( echo "$LOGIN_CONSTANTS" | grep "OAUTH_CLIENT_SECRET" | \
     sed -e 's/.* = "\(.*\)"; */\1/' )
 ANALYTICS_TRACKING_ID=$( echo "$ANALYTICS_CONSTANT" | \
     sed -e 's/.* = "\(.*\)"; */\1/' )
-if [ ${#OAUTH_CLIENT_ID} -ne 72 ]; then
-    echo "The length of injected OAUTH_CLIENT_ID is not 72. Halting."
-    exit 1
-elif [ ${#OAUTH_CLIENT_SECRET} -ne 24 ]; then
-    echo "The length of injected OAUTH_CLIENT_SECRET is not 24. Halting."
-    exit 1
-elif [ ${#ANALYTICS_TRACKING_ID} -ne 13 ]; then
-    echo "The length of injected ANALYTICS_TRACKING_ID is not 13. Halting."
-    exit 1
-fi
+[ ${#OAUTH_CLIENT_ID} -ne 72 ] && \
+    die "OAUTH_CLIENT_ID is not 72. Halting."
+[ ${#OAUTH_CLIENT_SECRET} -ne 24 ] && \
+    die "The length of injected OAUTH_CLIENT_SECRET is not 24. Halting."
+[ ${#ANALYTICS_TRACKING_ID} -ne 13 ] && \
+    die "The length of injected ANALYTICS_TRACKING_ID is not 13. Halting."
 
 echo
 echo -n "Looks good, but check the output above once more. "
@@ -152,8 +148,7 @@ $ECLIPSE_HOME/eclipse -nosplash -consolelog \
 set +x
 
 if [ ! -e "$LOCAL_REPO/artifacts.xml" -o ! -e "$LOCAL_REPO/content.xml" ]; then
-    echo "The files have not been generated. Halting."
-    exit 1
+    die "The files have not been generated. Halting."
 fi
 
 ###############################################################################
@@ -169,8 +164,7 @@ gsutil cp gs://gcloud-for-eclipse-testing/category.xml $WORK_DIR
 set +x
 
 if [ ! -e "$WORK_DIR/category.xml" ]; then
-    echo "The file was not copied. Halting."
-    exit 1
+    die "The file was not copied. Halting."
 fi
 
 ###############################################################################
