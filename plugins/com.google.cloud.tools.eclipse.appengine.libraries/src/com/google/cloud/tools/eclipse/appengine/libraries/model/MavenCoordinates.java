4316191d94708aa5596edcc287ea80ee239605e4
Elliotte Rusty Harold <elharo@google.com>
2017-02-28 09:27:58 -0500

    reduce public visibility

--- plugins/com.google.cloud.tools.eclipse.appengine.libraries/src/com/google/cloud/tools/eclipse/appengine/libraries/model/MavenCoordinates.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.libraries/src/com/google/cloud/tools/eclipse/appengine/libraries/model/MavenCoordinates.java
@@ -73,7 +73,7 @@ public class MavenCoordinates {
    * @param version the Maven version of the artifact, defaults to special value
    *        {@link MavenCoordinates#LATEST_VERSION}, cannot be <code>null</code> or empty string.
    */
-  public void setVersion(String version) {
+  void setVersion(String version) {
     Preconditions.checkNotNull(version, "version is null");
     Preconditions.checkArgument(!version.isEmpty(), "version is empty");
     this.version = version;
@@ -90,7 +90,7 @@ public class MavenCoordinates {
    * @param type the Maven packaging type, defaults to <code>jar</code>, cannot be <code>null</code>
    *        or empty string.
    */
-  public void setType(String type) {
+  void setType(String type) {
     Preconditions.checkNotNull(type, "type is null");
     Preconditions.checkArgument(!type.isEmpty(), "type is empty");
     this.type = type;
@@ -106,7 +106,7 @@ public class MavenCoordinates {
   /**
    * @param classifier the Maven classifier, defaults to null.
    */
-  public void setClassifier(String classifier) {
+  void setClassifier(String classifier) {
     this.classifier = classifier;
   }
 
@@ -122,7 +122,7 @@ public class MavenCoordinates {
    * @param repository the URI or the identifier of the repository used to download the artifact
    *        from, cannot be <code>null</code> or empty string.
    */
-  public void setRepository(String repository) {
+  void setRepository(String repository) {
     Preconditions.checkNotNull(repository, "repository null");
     Preconditions.checkArgument(!repository.isEmpty(), "repository is empty");
     this.repository = repository;
