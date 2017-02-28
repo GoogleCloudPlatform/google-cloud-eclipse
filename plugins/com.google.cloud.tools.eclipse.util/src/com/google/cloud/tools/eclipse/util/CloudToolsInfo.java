16ac1af5a96640c9f4bc01932a8f6f7e67593088
Brian de Alwis <bsd@mt.ca>
2017-02-27 22:33:09 -0500

    Revise implementation and fix up tests.

--- plugins/com.google.cloud.tools.eclipse.util/src/com/google/cloud/tools/eclipse/util/CloudToolsInfo.java
+++ plugins/com.google.cloud.tools.eclipse.util/src/com/google/cloud/tools/eclipse/util/CloudToolsInfo.java
@@ -16,6 +16,7 @@
 
 package com.google.cloud.tools.eclipse.util;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.eclipse.core.runtime.IBundleGroup;
 import org.eclipse.core.runtime.IBundleGroupProvider;
 import org.eclipse.core.runtime.Platform;
@@ -29,7 +30,8 @@ public class CloudToolsInfo {
   /**
    * Our main feature identifier, used for branding.
    */
-  private static final String CLOUD_TOOLS_FOR_ECLIPSE_FEATURE_ID =
+  @VisibleForTesting
+  static final String CLOUD_TOOLS_FOR_ECLIPSE_FEATURE_ID =
       "com.google.cloud.tools.eclipse.suite.e45.feature";
 
   // Don't change the value; this name is used as an originating "application" of usage metrics.
@@ -37,17 +39,22 @@ public class CloudToolsInfo {
 
   public static String USER_AGENT = METRICS_NAME + "/" + getToolsVersion();
 
-  /** Return the version of associated feature. May be slow. */
+  /** Return the version of associated Cloud Tools for Eclipse feature, or 0.0.0 if unknown. */
   public static String getToolsVersion() {
-    for(IBundleGroupProvider provider : Platform.getBundleGroupProviders()) {
-      for(IBundleGroup feature : provider.getBundleGroups()) {
-        if(CLOUD_TOOLS_FOR_ECLIPSE_FEATURE_ID.equals(feature.getIdentifier())) {
+    return getToolsVersion(Platform.getBundleGroupProviders());
+  }
+
+  @VisibleForTesting
+  static String getToolsVersion(IBundleGroupProvider[] bundleGroupProviders) {
+    for (IBundleGroupProvider provider : bundleGroupProviders) {
+      for (IBundleGroup feature : provider.getBundleGroups()) {
+        if (CLOUD_TOOLS_FOR_ECLIPSE_FEATURE_ID.equals(feature.getIdentifier())) {
           return feature.getVersion();
         }
       }
     }
     // May not have been installed with via a feature. Although we could report the bundle version,
     // that may result in a confusing versions.
-    return "UNKNOWN";
+    return "0.0.0";
   }
 }
