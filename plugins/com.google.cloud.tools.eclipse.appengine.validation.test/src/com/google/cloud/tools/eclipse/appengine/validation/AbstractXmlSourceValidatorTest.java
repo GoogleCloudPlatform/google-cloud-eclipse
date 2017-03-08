3687ee40cf1cfe67599933d2e439c67234528eb2
Abbie Farr <afarr@google.com>
2017-03-08 16:15:08 -0500

    check for app engine standard facet, add tests

--- plugins/com.google.cloud.tools.eclipse.appengine.validation.test/src/com/google/cloud/tools/eclipse/appengine/validation/AbstractXmlSourceValidatorTest.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.validation.test/src/com/google/cloud/tools/eclipse/appengine/validation/AbstractXmlSourceValidatorTest.java
@@ -17,15 +17,26 @@
 package com.google.cloud.tools.eclipse.appengine.validation;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.wst.common.project.facet.core.IFacetedProject;
+import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
+import org.eclipse.wst.sse.ui.internal.reconcile.validator.IncrementalHelper;
 import org.eclipse.wst.sse.ui.internal.reconcile.validator.IncrementalReporter;
+import org.eclipse.wst.validation.internal.core.ValidationException;
 import org.eclipse.wst.validation.internal.provisional.core.IMessage;
+import org.eclipse.wst.validation.internal.provisional.core.IValidationContext;
 import org.junit.Rule;
 import org.junit.Test;
+
+import com.google.cloud.tools.eclipse.appengine.facets.AppEngineStandardFacet;
 import com.google.cloud.tools.eclipse.test.util.project.TestProjectCreator;
 
 public class AbstractXmlSourceValidatorTest {
@@ -37,6 +48,50 @@ public class AbstractXmlSourceValidatorTest {
       + "</appengine-web-app>";
   
   @Rule public TestProjectCreator projectCreator = new TestProjectCreator();
+  
+  @Test
+  public void testValidate_appEngineProject() throws CoreException, ValidationException {
+    IProject project = projectCreator.getProject();
+    IFile file = project.getFile("testdata.xml");
+    file.create(ValidationTestUtils.stringToInputStream(
+        APPLICATION_XML), 0, null);
+    
+    IDocument document = ValidationTestUtils.getDocument(file);
+    
+    // Installs the App Engine Standard facet
+    IFacetedProject facetedProject = ProjectFacetsManager.create(project);
+    AppEngineStandardFacet.installAppEngineFacet(facetedProject, true, null);
+    
+    // Adds the URI of the file to be validated to the IncrementalHelper.
+    IncrementalHelper helper = new IncrementalHelper(document, project);
+    IPath path = file.getFullPath();
+    helper.setURI(path.toString());
+    
+    AbstractXmlSourceValidator validator = new AppEngineWebXmlSourceValidator();
+    validator.connect(document);
+    validator.validate(helper, new IncrementalReporter(null));
+    assertTrue(validator.hasValidated());
+  }
+  
+  @Test
+  public void testValidate_notAppEngineProject() throws CoreException, ValidationException {
+    IProject project = projectCreator.getProject();
+    IFile file = project.getFile("testdata.xml");
+    file.create(ValidationTestUtils.stringToInputStream(
+        APPLICATION_XML), 0, null);
+    
+    IDocument document = ValidationTestUtils.getDocument(file);
+    
+    // Adds the URI of the file to be validated to the IncrementalHelper.
+    IncrementalHelper helper = new IncrementalHelper(document, project);
+    IPath path = file.getFullPath();
+    helper.setURI(path.toString());
+    
+    AbstractXmlSourceValidator validator = new AppEngineWebXmlSourceValidator();
+    validator.connect(document);
+    validator.validate(helper, new IncrementalReporter(null));
+    assertFalse(validator.hasValidated());
+  }
   
   @Test
   public void getDocumentEncodingTest() throws CoreException {
@@ -57,6 +112,42 @@ public class AbstractXmlSourceValidatorTest {
     BannedElement element = new BannedElement("message");
     validator.createMessage(reporter, element, 0, "", IMessage.NORMAL_SEVERITY);
     assertEquals(1, reporter.getMessages().size());
+  }
+  
+  @Test
+  public void testGetFile() throws CoreException {
+    IProject project = projectCreator.getProject();
+    IFile file = project.getFile("testdata.xml");
+    file.create(ValidationTestUtils.stringToInputStream(
+        APPLICATION_XML), 0, null);
+    
+    assertTrue(file.exists());
+    
+    AbstractXmlSourceValidator validator = new WebXmlSourceValidator();
+    IPath path = file.getFullPath();
+    IFile testFile = validator.getFile(path.toString());
+    
+    assertNotNull(testFile);
+    assertEquals(file, testFile);
+  }
+  
+  @Test
+  public void testGetProject() throws CoreException {
+    IProject project = projectCreator.getProject();
+    IFile file = project.getFile("testdata.xml");
+    file.create(ValidationTestUtils.stringToInputStream(
+        APPLICATION_XML), 0, null);
+    
+    IDocument document = ValidationTestUtils.getDocument(file);
+    
+    IncrementalHelper helper = new IncrementalHelper(document, project);
+    IPath path = file.getFullPath();
+    helper.setURI(path.toString());
+
+    AbstractXmlSourceValidator validator = new WebXmlSourceValidator();
+    IProject testProject = validator.getProject((IValidationContext) helper);
+    assertNotNull(testProject);
+    assertEquals(project, testProject);
   }
   
 }
--- plugins/com.google.cloud.tools.eclipse.appengine.validation/src/com/google/cloud/tools/eclipse/appengine/validation/AbstractXmlSourceValidator.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.validation/src/com/google/cloud/tools/eclipse/appengine/validation/AbstractXmlSourceValidator.java
@@ -22,9 +22,17 @@ import java.util.logging.Logger;
 
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
+import org.eclipse.wst.common.project.facet.core.IFacetedProject;
+import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
 import org.eclipse.wst.sse.core.internal.encoding.EncodingMemento;
 import org.eclipse.wst.sse.core.internal.text.BasicStructuredDocument;
 import org.eclipse.wst.sse.ui.internal.reconcile.validator.ISourceValidator;
@@ -35,6 +43,9 @@ import org.eclipse.wst.validation.internal.provisional.core.IReporter;
 import org.eclipse.wst.validation.internal.provisional.core.IValidationContext;
 import org.eclipse.wst.validation.internal.provisional.core.IValidator;
 
+import com.google.cloud.tools.eclipse.appengine.facets.AppEngineStandardFacet;
+import com.google.common.annotations.VisibleForTesting;
+
 /**
  * Abstract source view validator.
  */
@@ -44,15 +55,21 @@ public abstract class AbstractXmlSourceValidator implements ISourceValidator, IV
       AbstractXmlSourceValidator.class.getName());
   
   private IDocument document;
+  private boolean hasValidated;
   
   /**
-   * Validates a given {@link IDocument}.
+   * Validates a given {@link IDocument} if the project has the App Engine Standard facet.
    */
   public void validate(IValidationContext helper, IReporter reporter) throws ValidationException {
+    IProject project = getProject(helper);
     try {
-      String encoding = getDocumentEncoding(document);
-      byte[] bytes = document.get().getBytes(encoding);
-      this.validate(reporter, bytes);
+      IFacetedProject facetedProject = ProjectFacetsManager.create(project);
+      if (AppEngineStandardFacet.hasAppEngineFacet(facetedProject)) {
+        hasValidated = true;
+        String encoding = getDocumentEncoding(document);
+        byte[] bytes = document.get().getBytes(encoding);
+        this.validate(reporter, bytes);
+      }
     } catch (IOException | CoreException | ParserConfigurationException ex) {
       logger.log(Level.SEVERE, ex.getMessage());
     }
@@ -79,6 +96,38 @@ public abstract class AbstractXmlSourceValidator implements ISourceValidator, IV
     reporter.addMessage(this, message);
   }
   
+  /**
+   * Returns the underlying IProject from a given IValidationContext or
+   * null if the IValidationContext does not return any files that need
+   * to be validated.
+   */
+  IProject getProject(IValidationContext helper) {
+    String[] fileUri = helper.getURIs();
+    if (fileUri.length > 0) {
+      IFile file = getFile(fileUri[0]);
+      if (file != null) {
+        return file.getProject();
+      }
+    }
+    return null;
+  }
+  
+  /**
+   * Returns the IFile for a given URI or null if the file does
+   * not exist in the workspace.
+   */
+  IFile getFile(String fileUri) {
+    IPath path = new Path(fileUri);
+    if (path.segmentCount() > 1) {
+      IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+      IFile file = root.getFile(path);
+      if (file != null && file.exists()) {
+        return file;
+      }
+    }
+    return null;
+  } 
+  
   static String getDocumentEncoding(IDocument document) {
     EncodingMemento encodingMemento = ((BasicStructuredDocument) document).getEncodingMemento();
     return encodingMemento.getDetectedCharsetName();
@@ -100,6 +149,11 @@ public abstract class AbstractXmlSourceValidator implements ISourceValidator, IV
 
   @Override
   public void validate(IRegion dirtyRegion, IValidationContext helper, IReporter reporter) {
+  }
+  
+  @VisibleForTesting
+  boolean hasValidated() {
+    return hasValidated;
   }
   
 }
