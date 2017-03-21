24ddc3d834914908dd375801c86e01fda8c86c42
nbashirbello <nbashirbello@google.com>
2017-03-21 13:12:03 -0400

    Added FacetUtil and FacetUtilTest

---
+++ plugins/com.google.cloud.tools.eclipse.appengine.facets.test/src/com/google/cloud/tools/eclipse/appengine/facets/FacetUtilTest.java
@@ -0,0 +1,201 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.eclipse.appengine.facets;
+
+import static org.mockito.Mockito.when;
+
+import com.google.cloud.tools.eclipse.test.util.project.TestProjectCreator;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jst.common.project.facet.core.JavaFacet;
+import org.eclipse.jst.j2ee.web.project.facet.WebFacetUtils;
+import org.eclipse.wst.common.project.facet.core.IFacetedProject;
+import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
+import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Tests for {@code FacetUtil}
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class FacetUtilTest {
+  @Mock private IFacetedProject mockFacetedProject;
+  @Rule public TestProjectCreator projectCreator = new TestProjectCreator();
+  private Set<IFacetedProject.Action> facetInstallSet = new HashSet<>();
+
+  @Test
+  public void TestAddJavaFacetToBatch_facetDoesNotExitsInProject() throws CoreException
+  {
+    when(mockFacetedProject.hasProjectFacet(JavaFacet.VERSION_1_7)).thenReturn(false);
+    when(mockFacetedProject.getProject()).thenReturn(projectCreator.getProject());
+    
+    Assert.assertTrue(FacetUtil.addJavaFacetToBatch(
+        JavaFacet.VERSION_1_7, mockFacetedProject, facetInstallSet));
+  }
+
+  @Test
+  public void TestAddJavaFacetToBatch_facetExitsInProject() throws CoreException
+  {
+    when(mockFacetedProject.hasProjectFacet(JavaFacet.VERSION_1_7)).thenReturn(true);
+    
+    Assert.assertFalse(FacetUtil.addJavaFacetToBatch(
+        JavaFacet.VERSION_1_7, mockFacetedProject, facetInstallSet));
+  }
+
+  @Test(expected = CoreException.class)
+  public void TestAddJavaFacetToBatch_nonJavaFacet() throws CoreException
+  {
+    FacetUtil.addJavaFacetToBatch(WebFacetUtils.WEB_25, mockFacetedProject, facetInstallSet);
+  }
+
+  @Test
+  public void TestAddWebFacetToBatch_facetDoesNotExitsInProject() throws CoreException
+  {
+    when(mockFacetedProject.hasProjectFacet(WebFacetUtils.WEB_25)).thenReturn(false);
+    when(mockFacetedProject.getProject()).thenReturn(projectCreator.getProject());
+    
+    Assert.assertTrue(FacetUtil.addWebFacetToBatch(
+        WebFacetUtils.WEB_25, mockFacetedProject, facetInstallSet));
+  }
+
+  @Test
+  public void TestAddWebFacetToBatch_facetExitsInProject() throws CoreException
+  {
+    when(mockFacetedProject.hasProjectFacet(WebFacetUtils.WEB_25)).thenReturn(true);
+    
+    Assert.assertFalse(FacetUtil.addWebFacetToBatch(
+        WebFacetUtils.WEB_25, mockFacetedProject, facetInstallSet));
+  }
+
+  @Test(expected = CoreException.class)
+  public void TestAddWebFacetToBatch_nonWebFacet() throws CoreException
+  {
+    FacetUtil.addWebFacetToBatch(JavaFacet.VERSION_1_7, mockFacetedProject, facetInstallSet);
+  }
+
+  @Test
+  public void TestAddFacetSetToProject_emptyFacetSet() throws CoreException {
+    IFacetedProject facetedProject = ProjectFacetsManager.create(projectCreator.getProject());
+    FacetUtil.addFacetSetToProject(facetedProject, facetInstallSet, null);
+    Set<IProjectFacetVersion> listOfFacets = facetedProject.getProjectFacets();
+    Assert.assertEquals(0, listOfFacets.size());
+  }
+
+  @Test
+  public void TestAddFacetSetToProject_nonEmptyfacetSet() throws CoreException {
+    IFacetedProject facetedProject = ProjectFacetsManager.create(projectCreator.getProject());
+    FacetUtil.addJavaFacetToBatch(JavaFacet.VERSION_1_7, facetedProject, facetInstallSet);
+    FacetUtil.addFacetSetToProject(facetedProject, facetInstallSet, null);
+    Set<IProjectFacetVersion> listOfFacets = facetedProject.getProjectFacets();
+    Assert.assertEquals(1, listOfFacets.size());
+    Assert.assertEquals(JavaFacet.VERSION_1_7, listOfFacets.iterator().next());
+  }
+
+  @Test
+  public void testFindAllWebInfFolders_noWebInfFolders() {
+    List<IFolder> webInfFolders =
+        FacetUtil.findAllWebInfFolders(projectCreator.getProject());
+    Assert.assertTrue(webInfFolders.isEmpty());
+  }
+
+  @Test
+  public void testFindAllWebInfFolders() throws CoreException {
+    IProject project = projectCreator.getProject();
+    createPath(project, new Path("src/my-webapp/WEB-INF"));
+
+    List<IFolder> webInfFolders =
+        FacetUtil.findAllWebInfFolders(project);
+    Assert.assertEquals(1, webInfFolders.size());
+    Assert.assertEquals(project.getFolder("src/my-webapp/WEB-INF"), webInfFolders.get(0));
+  }
+
+  @Test
+  public void testFindAllWebInfFolders_multipleFolders() throws CoreException {
+    IProject project = projectCreator.getProject();
+    createPath(project, new Path("webapps/first-webapp/WEB-INF"));
+    createPath(project, new Path("webapps/second-webapp/WEB-INF"));
+    createPath(project, new Path("third-webapp/WEB-INF"));
+    createPath(project, new Path("WEB-INF"));
+
+    List<IFolder> webInfFolders = FacetUtil.findAllWebInfFolders(project);
+    Assert.assertEquals(4, webInfFolders.size());
+    Assert.assertTrue(webInfFolders.contains(project.getFolder("webapps/first-webapp/WEB-INF")));
+    Assert.assertTrue(webInfFolders.contains(project.getFolder("webapps/second-webapp/WEB-INF")));
+    Assert.assertTrue(webInfFolders.contains(project.getFolder("third-webapp/WEB-INF")));
+    Assert.assertTrue(webInfFolders.contains(project.getFolder("WEB-INF")));
+  }
+
+  @Test
+  public void testFindMainWebAppDirectory_noWebInfFolders() {
+    IPath mainWebApp = FacetUtil.findMainWebAppDirectory(projectCreator.getProject());
+    Assert.assertNull(mainWebApp);
+  }
+
+  @Test
+  public void testFindMainWebAppDirectory() throws CoreException {
+    createPath(projectCreator.getProject(), new Path("webapps/first-webapp/WEB-INF"));
+    IPath mainWebApp = FacetUtil.findMainWebAppDirectory(projectCreator.getProject());
+    Assert.assertEquals(new Path("webapps/first-webapp"), mainWebApp);
+  }
+
+  @Test
+  public void testFindMainWebAppDirectory_returnsFolderWithWebXml() throws CoreException {
+    IProject project = projectCreator.getProject();
+    createPath(project, new Path("webapps/first-webapp/WEB-INF"));
+    createEmptyFile(project, new Path("webapps/second-webapp/WEB-INF/web.xml"));
+    createPath(project, new Path("third-webapp/WEB-INF"));
+    createPath(project, new Path("WEB-INF"));
+
+    IPath mainWebApp = FacetUtil.findMainWebAppDirectory(project);
+    Assert.assertEquals(new Path("webapps/second-webapp"), mainWebApp);
+  }
+
+  private static void createEmptyFile(IProject project, IPath relativePath) throws CoreException {
+    createPath(project, relativePath.removeLastSegments(1));
+
+    InputStream emptyStream = new ByteArrayInputStream(new byte[0]);
+    project.getFile(relativePath).create(emptyStream, false /* force */, null /* monitor */);
+    Assert.assertTrue(project.getFile(relativePath).exists());
+  }
+
+  private static void createPath(IContainer parent, IPath relativePath) throws CoreException {
+    if (!relativePath.isEmpty()) {
+      String firstSegment = relativePath.segment(0);
+      IFolder child = parent.getFolder(new Path(firstSegment));
+      if (!child.exists()) {
+        child.create(false /* force */, true /* local */, null /* monitor */);
+      }
+      Assert.assertTrue(child.exists());
+
+      createPath(child, relativePath.removeFirstSegments(1));
+    }
+  }
+}
---
+++ plugins/com.google.cloud.tools.eclipse.appengine.facets/src/com/google/cloud/tools/eclipse/appengine/facets/FacetUtil.java
@@ -0,0 +1,211 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.eclipse.appengine.facets;
+
+import com.google.cloud.tools.eclipse.util.status.StatusUtil;
+import com.google.common.annotations.VisibleForTesting;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceVisitor;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jst.common.project.facet.core.JavaFacet;
+import org.eclipse.jst.common.project.facet.core.JavaFacetInstallConfig;
+import org.eclipse.jst.j2ee.project.facet.IJ2EEFacetInstallDataModelProperties;
+import org.eclipse.jst.j2ee.project.facet.IJ2EEModuleFacetInstallDataModelProperties;
+import org.eclipse.jst.j2ee.web.project.facet.IWebFacetInstallDataModelProperties;
+import org.eclipse.jst.j2ee.web.project.facet.WebFacetInstallDataModelProvider;
+import org.eclipse.jst.j2ee.web.project.facet.WebFacetUtils;
+import org.eclipse.wst.common.componentcore.internal.builder.DependencyGraphImpl;
+import org.eclipse.wst.common.componentcore.internal.builder.IDependencyGraph;
+import org.eclipse.wst.common.frameworks.datamodel.DataModelFactory;
+import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
+import org.eclipse.wst.common.project.facet.core.IFacetedProject;
+import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
+
+/**
+ * Utility class for processing facets.
+ */
+@SuppressWarnings("restriction") // For IDependencyGraph
+public class FacetUtil {
+  public static final Logger logger = Logger.getLogger(FacetUtil.class.getName());
+  
+
+  /**
+   * Adds an install action for the {@code javaFacet} to the {@code facetInstallSet} if the
+   * {@code javaFacet} does not already exist in {@code facetedProject}.
+   *
+   * @return true is the facet action was added to {@code facetInstallSet} and false if {@code javaFacet}
+   *   already exists in {@code facetedProject}
+   * @throws CoreException if {@code javaFacet} is not a Java facet
+   */
+ public static boolean addJavaFacetToBatch(IProjectFacetVersion javaFacet, IFacetedProject facetedProject,
+     Set<IFacetedProject.Action> facetInstallSet) throws CoreException {
+   if (!JavaFacet.FACET.getId().equals(javaFacet.getProjectFacet().getId())) {
+     throw new CoreException(StatusUtil.error(FacetUtil.class, javaFacet.toString() + " is not a Java facet"));
+   }
+   
+   if (facetedProject.hasProjectFacet(javaFacet)) {
+     return false;
+   }
+
+   // TODO use "src/main/java" for only maven projects
+   JavaFacetInstallConfig javaConfig = new JavaFacetInstallConfig();
+   List<IPath> sourcePaths = new ArrayList<>();
+
+   IProject project = facetedProject.getProject();
+   if (project.getFolder("src/main/java").exists()) {
+     sourcePaths.add(new Path("src/main/java"));
+   }
+
+   if (project.getFolder("src/test/java").exists()) {
+     sourcePaths.add(new Path("src/test/java"));
+   }
+
+   javaConfig.setSourceFolders(sourcePaths);
+   facetInstallSet.add(new IFacetedProject.Action(
+       IFacetedProject.Action.Type.INSTALL, javaFacet, javaConfig));
+   return true;
+ }
+
+ /**
+  * Adds an install action for the {@code webFacet} to the {@code facetInstallSet} if the
+  * {@code webFacet} does not already exist in {@code facetedProject}.
+  * 
+  * @return true is the facet action was added to {@code facetInstallSet} and false if {@code webFacet}
+  *   already exists in {@code facetedProject}
+  * @throws CoreException if {@code webFacet} is not a Web facet
+  */
+ public static boolean addWebFacetToBatch(IProjectFacetVersion webFacet, IFacetedProject facetedProject,
+     Set<IFacetedProject.Action> facetInstallSet) throws CoreException {
+   if (!WebFacetUtils.WEB_FACET.getId().equals(webFacet.getProjectFacet().getId())) {
+     throw new CoreException(StatusUtil.error(FacetUtil.class, webFacet.toString() + " is not a Web facet"));
+   }
+
+   if (facetedProject.hasProjectFacet(webFacet)) {
+     return false;
+   }
+
+   String webAppDirectory = "src/main/webapp";
+   IPath webAppDirectoryFound = findMainWebAppDirectory(facetedProject.getProject());
+   if (webAppDirectoryFound != null) {
+     webAppDirectory = webAppDirectoryFound.toOSString();
+   }
+
+   IDataModel webModel = DataModelFactory.createDataModel(new WebFacetInstallDataModelProvider());
+   webModel.setBooleanProperty(IJ2EEModuleFacetInstallDataModelProperties.ADD_TO_EAR, false);
+   webModel.setBooleanProperty(IJ2EEFacetInstallDataModelProperties.GENERATE_DD, true);
+   webModel.setBooleanProperty(IWebFacetInstallDataModelProperties.INSTALL_WEB_LIBRARY, false);
+   webModel.setStringProperty(IWebFacetInstallDataModelProperties.CONFIG_FOLDER, webAppDirectory);
+   facetInstallSet.add(new IFacetedProject.Action(
+       IFacetedProject.Action.Type.INSTALL, webFacet, webModel));
+   return true;
+ }
+
+ /**
+  * Modifies the set of project facets in {@code facetedProject} by performing the series of
+  * actions in {@code facetActionSet}.
+  *
+  * @throws CoreException
+  */
+ public static void addFacetSetToProject(IFacetedProject facetedProject,
+     Set<IFacetedProject.Action> facetActionSet, IProgressMonitor monitor) throws CoreException {
+   SubMonitor subMonitor = SubMonitor.convert(monitor, 100);
+
+   // Workaround deadlock bug described in Eclipse bug (https://bugs.eclipse.org/511793).
+   // There are graph update jobs triggered by the completion of the CreateProjectOperation
+   // above (from resource notifications) and from other resource changes from modifying the
+   // project facets. So we force the dependency graph to defer updates
+   try {
+     IDependencyGraph.INSTANCE.preUpdate();
+     try {
+       Job.getJobManager().join(DependencyGraphImpl.GRAPH_UPDATE_JOB_FAMILY,
+           subMonitor.newChild(10));
+     } catch (OperationCanceledException | InterruptedException ex) {
+       logger.log(Level.WARNING, "Exception waiting for WTP Graph Update job", ex);
+     }
+
+     facetedProject.modify(facetActionSet, subMonitor.newChild(90));
+   } finally {
+     IDependencyGraph.INSTANCE.postUpdate();
+   }
+ }
+
+ /**
+  * Attempts to find a main web application directory, by the following logic:
+  *
+  * 1. If there is no {@code WEB-INF} folder in the {@code project}, returns {@code null}.
+  * 2. Otherwise, if there is at least one {@code WEB-INF} folder that contains {@code web.xml},
+  *     returns the parent directory of one of such {@code WEB-INF} folders.
+  * 3. Otherwise, returns the parent directory of an arbitrary {@code WEB-INF}.
+  *
+  * @return path of the main web application directory, relative to {@code project}, if found;
+  *     otherwise, {@code null}
+  */
+ @VisibleForTesting
+ static IPath findMainWebAppDirectory(IProject project) {
+   List<IFolder> webInfFolders = findAllWebInfFolders(project);
+   if (webInfFolders.isEmpty()) {
+     return null;
+   }
+
+   for (IFolder webInf : webInfFolders) {
+     if (webInf.getFile("web.xml").exists()) {
+       return webInf.getParent().getProjectRelativePath();
+     }
+   }
+   return webInfFolders.get(0).getParent().getProjectRelativePath();
+ }
+
+ /**
+  * Returns a list of WEB-INF folders in {@code container}.
+  */
+ @VisibleForTesting
+ static List<IFolder> findAllWebInfFolders(IContainer container) {
+   final List<IFolder> webInfFolders = new ArrayList<>();
+
+   try {
+     IResourceVisitor webInfCollector = new IResourceVisitor() {
+       @Override
+       public boolean visit(IResource resource) throws CoreException {
+         if (resource.getType() == IResource.FOLDER && "WEB-INF".equals(resource.getName())) {
+           webInfFolders.add((IFolder) resource);
+           return false;  // No need to visit sub-directories.
+         }
+         return true;
+       }
+     };
+     container.accept(webInfCollector);
+   } catch (CoreException ex) {
+     // Our attempt to find folders failed, but don't error out.
+   }
+   return webInfFolders;
+ }
+
+}
