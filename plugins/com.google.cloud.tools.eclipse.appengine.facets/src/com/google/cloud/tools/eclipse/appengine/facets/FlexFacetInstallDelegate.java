5f6b5aba81d3c6e7037157e0d9f8b4af217516ac
nbashirbello <nbashirbello@google.com>
2017-03-22 12:46:19 -0400

    Implemente flex facet install to add the config files if they don't exist

--- plugins/com.google.cloud.tools.eclipse.appengine.facets/src/com/google/cloud/tools/eclipse/appengine/facets/FlexFacetInstallDelegate.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.facets/src/com/google/cloud/tools/eclipse/appengine/facets/FlexFacetInstallDelegate.java
@@ -16,6 +16,47 @@
 
 package com.google.cloud.tools.eclipse.appengine.facets;
 
-public class FlexFacetInstallDelegate extends AppEngineFacetInstallDelegate {
+import com.google.cloud.tools.eclipse.appengine.deploy.flex.FlexDeployPreferences;
+import com.google.cloud.tools.eclipse.util.io.ResourceUtils;
+import com.google.cloud.tools.eclipse.util.templates.appengine.AppEngineTemplateUtility;
+import java.io.ByteArrayInputStream;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
 
+public class FlexFacetInstallDelegate extends AppEngineFacetInstallDelegate {
+  @Override
+  public void execute(IProject project,
+                      IProjectFacetVersion version,
+                      Object config,
+                      IProgressMonitor monitor) throws CoreException {
+    super.execute(project, version, config, monitor);
+    createConfigFiles(project, monitor);
+  }
+
+  // TODO: https://github.com/GoogleCloudPlatform/google-cloud-eclipse/issues/1640
+  private void createConfigFiles(IProject project, IProgressMonitor monitor) throws CoreException {
+    SubMonitor progress = SubMonitor.convert(monitor, 10);
+
+    FlexDeployPreferences flexDeployPreferences = new FlexDeployPreferences(project);
+    String appYamlParentPath = flexDeployPreferences.getAppEngineDirectory();
+    IFolder appYamlParentFolder = project.getFolder(appYamlParentPath);
+    IFile appYaml = appYamlParentFolder.getFile("app.yaml");
+    if (appYaml.exists()) {
+      return;
+    }
+
+    ResourceUtils.createFolders(appYamlParentFolder, progress.newChild(2));
+    appYaml.create(new ByteArrayInputStream(new byte[0]), true, progress.newChild(2));
+    String appYamlLocation = appYaml.getLocation().toString();
+    AppEngineTemplateUtility.copyFileContent(appYamlLocation, AppEngineTemplateUtility.APP_YAML);
+    appYaml.refreshLocal(IResource.DEPTH_ZERO, monitor);
+    
+    // TODO: Set default editor for app.yaml, if not it opens app.yaml as text file outside eclipse
+  }
 }
