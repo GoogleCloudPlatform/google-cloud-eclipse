8444a8a69a746775c316d794b7ee5e7177a449b6
Chanseok Oh <chanseok@google.com>
2017-03-23 11:31:52 -0400

    Preformat GitHub issue body

--- plugins/com.google.cloud.tools.eclipse.bugreport.test/src/com/google/cloud/tools/eclipse/bugreport/ui/BugReportCommandHandlerTest.java
+++ plugins/com.google.cloud.tools.eclipse.bugreport.test/src/com/google/cloud/tools/eclipse/bugreport/ui/BugReportCommandHandlerTest.java
@@ -16,14 +16,37 @@
 
 package com.google.cloud.tools.eclipse.bugreport.ui;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.junit.Test;
 
 public class BugReportCommandHandlerTest {
 
   @Test
-  public void testBugReportUrl_noMalformedUrlException() throws MalformedURLException {
-    new URL(BugReportCommandHandler.BUG_REPORT_URL);
+  public void formatReportUrl() throws MalformedURLException {
+    URL url = new URL(BugReportCommandHandler.formatReportUrl());
+
+    assertEquals("https", url.getProtocol());
+    assertEquals("github.com", url.getHost());
+    assertEquals("/GoogleCloudPlatform/google-cloud-eclipse/issues/new", url.getPath());
+
+    Pattern pattern = Pattern.compile("body="
+        + "-\\+Cloud\\+Tools\\+for\\+Eclipse\\+Version%3A\\+(.*)%0A"
+        + "-\\+OS%3A\\+(.*)%0A"
+        + "-\\+Java\\+Version%3A\\+(.*)%0A%0A");
+    Matcher matcher = pattern.matcher(url.getQuery());
+    assertTrue(matcher.matches());
+    String toolVersion = matcher.group(1);
+    String os = matcher.group(2);
+    String javaVersion = matcher.group(3);
+
+    assertTrue(Pattern.compile("^\\d+\\.\\d+\\.\\d+").matcher(toolVersion).find());
+    assertTrue(javaVersion.startsWith("1.7.") || javaVersion.startsWith("1.8."));
+    assertTrue(os.startsWith("Linux") || os.startsWith("Mac") || os.startsWith("Windows"));
   }
 }
--- plugins/com.google.cloud.tools.eclipse.bugreport/src/com/google/cloud/tools/eclipse/bugreport/ui/BugReportCommandHandler.java
+++ plugins/com.google.cloud.tools.eclipse.bugreport/src/com/google/cloud/tools/eclipse/bugreport/ui/BugReportCommandHandler.java
@@ -16,31 +16,37 @@
 
 package com.google.cloud.tools.eclipse.bugreport.ui;
 
+import com.google.cloud.tools.eclipse.util.CloudToolsInfo;
 import com.google.common.annotations.VisibleForTesting;
-import java.net.MalformedURLException;
-import java.net.URL;
+import com.google.common.escape.Escaper;
+import com.google.common.net.UrlEscapers;
+import java.text.MessageFormat;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
+import org.eclipse.swt.program.Program;
 
 public class BugReportCommandHandler extends AbstractHandler {
 
-  @VisibleForTesting
-  static final String BUG_REPORT_URL =
-      "https://github.com/GoogleCloudPlatform/google-cloud-eclipse/issues";
+  private static final String BUG_REPORT_URL =
+      "https://github.com/GoogleCloudPlatform/google-cloud-eclipse/issues/new";
+
+  private static final String BODY_TEMPLATE =
+      "- Cloud Tools for Eclipse Version: {0}\n- OS: {1} {2}\n- Java Version: {3}\n\n";
 
   @Override
   public Object execute(ExecutionEvent event) throws ExecutionException {
-    try {
-      IWorkbenchBrowserSupport browserSupport = PlatformUI.getWorkbench().getBrowserSupport();
-      browserSupport.getExternalBrowser().openURL(new URL(BUG_REPORT_URL));
-    } catch (MalformedURLException | PartInitException ex) {
-      throw new ExecutionException("Failed to open external browser", ex);
-    }
+    Program.launch(formatReportUrl());
     return null;
   }
 
+  @VisibleForTesting
+  static String formatReportUrl() {
+    String body = MessageFormat.format(BODY_TEMPLATE, CloudToolsInfo.getToolsVersion(),
+        System.getProperty("os.name"), System.getProperty("os.version"),
+        System.getProperty("java.version"));
+
+    Escaper escaper = UrlEscapers.urlFormParameterEscaper();
+    return BUG_REPORT_URL + "?body=" + escaper.escape(body);
+  }
 }
