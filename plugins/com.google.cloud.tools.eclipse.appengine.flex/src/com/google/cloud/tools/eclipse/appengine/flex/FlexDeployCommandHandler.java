f800f02af68b3f61197a33612d9d4758ee3e3ac8
nbashirbello <nbashirbello@google.com>
2017-03-23 16:04:03 -0400

    Added FlexDeployCommandHandler

---
+++ plugins/com.google.cloud.tools.eclipse.appengine.flex/src/com/google/cloud/tools/eclipse/appengine/flex/FlexDeployCommandHandler.java
@@ -0,0 +1,14 @@
+package com.google.cloud.tools.eclipse.appengine.flex;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class FlexDeployCommandHandler extends AbstractHandler {
+
+  @Override
+  public Object execute(ExecutionEvent event) throws ExecutionException {
+    return null;
+  }
+
+}
