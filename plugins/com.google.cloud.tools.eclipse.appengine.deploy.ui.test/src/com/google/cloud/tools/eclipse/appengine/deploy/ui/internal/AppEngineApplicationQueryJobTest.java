d9b8a72995cdc3b08cb33c626e850df78af4e17d
Chanseok Oh <chanseok@google.com>
2017-03-27 15:47:16 -0400

    Add tests for AppEngineApplicationQueryJob

--- plugins/com.google.cloud.tools.eclipse.appengine.deploy.ui.test/src/com/google/cloud/tools/eclipse/appengine/deploy/ui/internal/AppEngineApplicationQueryJobTest.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.deploy.ui.test/src/com/google/cloud/tools/eclipse/appengine/deploy/ui/internal/AppEngineApplicationQueryJobTest.java
@@ -19,6 +19,9 @@ package com.google.cloud.tools.eclipse.appengine.deploy.ui.internal;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -48,7 +51,7 @@ public class AppEngineApplicationQueryJobTest {
   private static final String EXPECTED_MESSAGE_WHEN_EXCEPTION =
       ProjectSelectorSelectionChangedListenerTest.EXPECTED_MESSAGE_WHEN_EXCEPTION;
 
-  private GcpProject project = new GcpProject("name", "myProjectId");
+  private GcpProject project = new GcpProject("name", "projectId");
   @Mock private Credential credential;
   @Mock private ProjectRepository projectRepository;
   @Mock private ProjectSelector projectSelector;
@@ -61,8 +64,7 @@ public class AppEngineApplicationQueryJobTest {
     assertNotNull(Display.getCurrent());
 
     queryJob = new AppEngineApplicationQueryJob(project, credential, projectRepository,
-        projectSelector, EXPECTED_LINK,
-        isLatestQueryJob, Display.getCurrent());
+        projectSelector, EXPECTED_LINK, isLatestQueryJob, Display.getCurrent());
 
     when(projectSelector.isDisposed()).thenReturn(false);
     when(isLatestQueryJob.apply(queryJob)).thenReturn(true);
@@ -76,42 +78,76 @@ public class AppEngineApplicationQueryJobTest {
   @Test
   public void testRun_projectHasNoApplication()
       throws ProjectRepositoryException, InterruptedException {
-    when(projectRepository.getAppEngineApplication(credential, "myProjectId"))
+    when(projectRepository.getAppEngineApplication(credential, "projectId"))
         .thenReturn(AppEngine.NO_APPENGINE_APPLICATION);
     assertNull(project.getAppEngine());
 
     queryJob.schedule();
     queryJob.join();
 
-    verify(projectRepository).getAppEngineApplication(credential, "myProjectId");
+    verify(projectRepository).getAppEngineApplication(credential, "projectId");
     verify(isLatestQueryJob).apply(queryJob);
     verify(projectSelector).isDisposed();
     verify(projectSelector).setStatusLink(EXPECTED_MESSAGE_WHEN_NO_APPLICATION, EXPECTED_LINK);
 
     assertEquals(AppEngine.NO_APPENGINE_APPLICATION, project.getAppEngine());
   }
-/*
+
   @Test
-  public void testRun_abandonIfDisposed() throws InterruptedException, ProjectRepositoryException {
-    when(projectSelector.isDisposed()).thenReturn(true);
+  public void testRun_projectHasApplication()
+      throws ProjectRepositoryException, InterruptedException {
+    AppEngine appEngine = AppEngine.withId("unique-id");
+    when(projectRepository.getAppEngineApplication(credential, "projectId")).thenReturn(appEngine);
 
     queryJob.schedule();
     queryJob.join();
 
-    verify(projectRepository).getProjects(credential);
-    verify(projectSelector, never()).setProjects(projects);
+    verify(isLatestQueryJob, never()).apply(queryJob);
+    verify(projectSelector, never()).isDisposed();
+    verify(projectSelector, never()).setStatusLink(anyString(), anyString());
+
+    assertTrue(appEngine == project.getAppEngine());
+  }
+
+  @Test
+  public void testRun_queryError() throws ProjectRepositoryException, InterruptedException {
+    when(projectRepository.getAppEngineApplication(credential, "projectId"))
+        .thenThrow(new ProjectRepositoryException("testException"));
+
+    queryJob.schedule();
+    queryJob.join();
+
+    verify(isLatestQueryJob).apply(queryJob);
+    verify(projectSelector).isDisposed();
+    verify(projectSelector).setStatusLink(EXPECTED_MESSAGE_WHEN_EXCEPTION, null);
+
+    assertNull(project.getAppEngine());
+  }
+
+  @Test
+  public void testRun_abandonIfDisposed() throws InterruptedException, ProjectRepositoryException {
+    when(projectSelector.isDisposed()).thenReturn(true);
+    when(projectRepository.getAppEngineApplication(credential, "projectId"))
+        .thenReturn(AppEngine.NO_APPENGINE_APPLICATION);
+
+    queryJob.schedule();
+    queryJob.join();
+
+    verify(projectSelector).isDisposed();
+    verify(projectSelector, never()).setStatusLink(anyString(), anyString());
   }
 
   @Test
   public void testRun_abandonIfNotLatestJob()
       throws InterruptedException, ProjectRepositoryException {
     when(isLatestQueryJob.apply(queryJob)).thenReturn(false);
+    when(projectRepository.getAppEngineApplication(credential, "projectId"))
+        .thenReturn(AppEngine.NO_APPENGINE_APPLICATION);
 
     queryJob.schedule();
     queryJob.join();
 
-    verify(projectRepository).getProjects(credential);
-    verify(projectSelector, never()).setProjects(projects);
+    verify(isLatestQueryJob).apply(queryJob);
+    verify(projectSelector, never()).setStatusLink(anyString(), anyString());
   }
-*/
 }
