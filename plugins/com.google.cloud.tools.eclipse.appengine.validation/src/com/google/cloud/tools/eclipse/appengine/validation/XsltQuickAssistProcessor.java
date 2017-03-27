3df66f71417055c031d8c1c551d14adbca7c2099
Abbie Farr <afarr@google.com>
2017-03-27 11:33:37 -0400

    work in progress:

--- plugins/com.google.cloud.tools.eclipse.appengine.validation/src/com/google/cloud/tools/eclipse/appengine/validation/XsltQuickAssistProcessor.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.validation/src/com/google/cloud/tools/eclipse/appengine/validation/XsltQuickAssistProcessor.java
@@ -21,12 +21,16 @@ import java.util.Iterator;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExecutableExtension;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
 import org.eclipse.jface.text.quickassist.IQuickAssistInvocationContext;
 import org.eclipse.jface.text.quickassist.IQuickAssistProcessor;
 import org.eclipse.jface.text.source.Annotation;
 import org.eclipse.jface.text.source.IAnnotationModel;
 import org.eclipse.jface.text.source.ISourceViewer;
+import org.osgi.framework.Bundle;
+
+import com.google.cloud.tools.eclipse.util.status.StatusUtil;
 
 /**
  * Provides quick assists for source editor.
@@ -35,20 +39,41 @@ public class XsltQuickAssistProcessor implements IQuickAssistProcessor, IExecuta
 
   private String annotationText;
   private ICompletionProposal fix;
-  
-  @Override
-  public void setInitializationData(IConfigurationElement config, String propertyName, Object data)
-      throws CoreException {
-    String extensionData = (String) data;
-    String[] splitData = extensionData.split(":");
-    if (splitData.length == 3) {  // Number of identifiers in XsltQuickAssistProcessor
-      //TODO
-    }
-  }
+  private Class<?> clazz;
   
   XsltQuickAssistProcessor(String annotationType, XsltSourceQuickFix fix) {
     this.annotationText = annotationType;
     this.fix = fix;
+  }
+  
+  /**
+   * 0-argument constructor required by the Eclipse Extension Registry. Not intended for normal use.
+   * 
+   * @see XsltQuickAssistProcessor(String, XsltSourceQuickFix)
+   * @noreference use {@link XsltQuickAssistProcessor(String, XsltSourceQuickFix)} instead
+   */
+  public XsltQuickAssistProcessor() {}
+  
+  @Override
+  public void setInitializationData(IConfigurationElement config, String propertyName, Object data)
+      throws CoreException {
+    if (data == null || !(data instanceof String)) {
+      throw new CoreException(StatusUtil.error(getClass(), "Data must be a class name"));
+    }
+    String className = (String) data;
+    String bundleSymbolicName = config.getNamespaceIdentifier();
+    Bundle bundle = Platform.getBundle(bundleSymbolicName);
+    if (bundle == null) {
+      throw new CoreException(StatusUtil.error(this, "Missing bundle " + bundleSymbolicName));
+    }
+    try {
+      clazz = bundle.loadClass(className);
+    } catch (ClassNotFoundException ex) {
+       throw new CoreException(StatusUtil.error(this,
+           "Could not load class " + className
+           + " from bundle " + bundle.getSymbolicName(),
+           ex));
+    }
   }
   
   @Override
