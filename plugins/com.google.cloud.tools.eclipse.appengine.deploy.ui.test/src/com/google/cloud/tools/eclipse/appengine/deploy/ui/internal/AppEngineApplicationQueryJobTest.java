e605fcd1c461abe715332a7f1f3a6eb572bc7dc8
Chanseok Oh <chanseok@google.com>
2017-03-27 17:37:14 -0400

    Add a test

--- plugins/com.google.cloud.tools.eclipse.appengine.deploy.ui.test/src/com/google/cloud/tools/eclipse/appengine/deploy/ui/internal/AppEngineApplicationQueryJobTest.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.deploy.ui.test/src/com/google/cloud/tools/eclipse/appengine/deploy/ui/internal/AppEngineApplicationQueryJobTest.java
@@ -21,6 +21,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -149,5 +150,38 @@ public class AppEngineApplicationQueryJobTest {
 
     verify(isLatestQueryJob).apply(queryJob);
     verify(projectSelector, never()).setStatusLink(anyString(), anyString());
+  }
+
+  @Test
+  public void testRun_abandonStaleJob() throws InterruptedException, ProjectRepositoryException {
+    when(projectRepository.getAppEngineApplication(credential, "projectId"))
+        .thenReturn(AppEngine.NO_APPENGINE_APPLICATION);
+
+    // Prepare another concurrent query job.
+    Credential staleCredential = mock(Credential.class);
+
+    GcpProject staleProject = new GcpProject("name", "staleProjectId");
+    ProjectRepository projectRepository2 = mock(ProjectRepository.class);
+    when(projectRepository2.getAppEngineApplication(staleCredential, "staleProjectId"))
+        .thenThrow(new ProjectRepositoryException("testException"));
+
+    Predicate<Job> notLatest = mock(Predicate.class);
+    Job staleJob = new AppEngineApplicationQueryJob(staleProject, staleCredential,
+        projectRepository2, projectSelector, EXPECTED_LINK, notLatest, Display.getCurrent());
+
+    // This second job is stale, i.e., it was fired, but user has selected another credential.
+    when(notLatest.apply(staleJob)).thenReturn(false);
+
+    queryJob.schedule();
+    queryJob.join();
+    // Make the stale job complete even after "queryJob" finishes.
+    staleJob.schedule();
+    staleJob.join();
+
+    verify(projectRepository).getAppEngineApplication(credential, "projectId");
+    verify(projectRepository2).getAppEngineApplication(staleCredential, "staleProjectId");
+
+    verify(projectSelector).setStatusLink(EXPECTED_MESSAGE_WHEN_NO_APPLICATION, EXPECTED_LINK);
+    verify(projectSelector, never()).setStatusLink(EXPECTED_MESSAGE_WHEN_EXCEPTION, null);
   }
 }
