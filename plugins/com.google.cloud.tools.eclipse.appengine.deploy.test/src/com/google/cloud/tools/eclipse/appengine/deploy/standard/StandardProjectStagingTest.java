1990598507401e5aad1a59c6fef4ae9193390aa0
Chanseok Oh <chanseok@google.com>
2017-03-29 16:42:53 -0400

    Add/fix tests

--- plugins/com.google.cloud.tools.eclipse.appengine.deploy.test/src/com/google/cloud/tools/eclipse/appengine/deploy/standard/StandardProjectStagingTest.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.deploy.test/src/com/google/cloud/tools/eclipse/appengine/deploy/standard/StandardProjectStagingTest.java
@@ -16,29 +16,32 @@
 
 package com.google.cloud.tools.eclipse.appengine.deploy.standard;
 
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.google.cloud.tools.appengine.cloudsdk.CloudSdk;
 import com.google.cloud.tools.eclipse.appengine.facets.AppEngineStandardFacet;
 import com.google.cloud.tools.eclipse.test.util.project.TestProjectCreator;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jst.common.project.facet.core.JavaFacet;
 import org.eclipse.jst.j2ee.web.project.facet.WebFacetUtils;
 import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
 import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
@@ -57,41 +60,53 @@ public class StandardProjectStagingTest {
       ProjectFacetsManager.getProjectFacet(AppEngineStandardFacet.ID).getVersion("1");
 
   @Rule
+  public TemporaryFolder tempFolder = new TemporaryFolder();
+
+  @Rule
   public TestProjectCreator projectCreator = new TestProjectCreator().withFacetVersions(
       JavaFacet.VERSION_1_7, WebFacetUtils.WEB_25, APP_ENGINE_STANDARD_FACET_1);
 
-  @Mock private IPath warDirectory;
-  @Mock private IPath stagingDirectory;
-  @Mock private CloudSdk cloudSdk;
   @Mock private IProgressMonitor monitor;
 
+  private final CloudSdk cloudSdk = new CloudSdk.Builder().async(false).build();
+
+  private IPath stagingDirectory;
   private IProject project;
 
   @Before
-  public void setUp() {
+  public void setUp() throws IOException {
     project = projectCreator.getProject();
+    stagingDirectory = new Path(tempFolder.getRoot().toString());
   }
 
   @Test(expected = OperationCanceledException.class)
   public void testStage_cancelled() {
     when(monitor.isCanceled()).thenReturn(true);
-    new StandardProjectStaging().stage(warDirectory, stagingDirectory, cloudSdk, monitor);
+    new StandardProjectStaging().stage(mock(IPath.class), stagingDirectory, cloudSdk, monitor);
   }
 
   @Test
-  public void test() throws CoreException {
-    IProject project = projectCreator.getProject();
+  public void testConfigFilesConversion() throws CoreException {
     createConfigFile("cron.xml", cronXml);
     createConfigFile("datastore-indexes.xml", datastoreIndexesXml);
     createConfigFile("dispatch.xml", dispatchXml);
     createConfigFile("dos.xml", dosXml);
     createConfigFile("queue.xml", queueXml);
-    Assert.fail();
+
+    IPath explodedWarDirectory = project.getFolder("WebContent").getRawLocation();
+    new StandardProjectStaging().stage(explodedWarDirectory, stagingDirectory, cloudSdk, monitor);
+
+    IPath stagingGenerated = stagingDirectory.append("WEB-INF/appengine-generated");
+    assertTrue(stagingGenerated.append("cron.yaml").toFile().exists());
+    assertTrue(stagingGenerated.append("index.yaml").toFile().exists());
+    assertTrue(stagingGenerated.append("dispatch.yaml").toFile().exists());
+    assertTrue(stagingGenerated.append("dos.yaml").toFile().exists());
+    assertTrue(stagingGenerated.append("queue.yaml").toFile().exists());
   }
 
   private void createConfigFile(String filename, String content) throws CoreException {
     InputStream in = new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8));
-    IFile file = project.getFile("src/main/webapp/WEB-INF/" + filename);
-    file.setContents(in, IResource.FORCE, null);
+    IFile file = project.getFile("WebContent/WEB-INF/" + filename);
+    file.create(in, true, null);
   }
 }
