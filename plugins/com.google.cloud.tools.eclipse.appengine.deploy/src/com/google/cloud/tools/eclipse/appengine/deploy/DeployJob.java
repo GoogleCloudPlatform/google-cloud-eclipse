c7f9e80c8f842bf78311347535d7a9cb1c64d841
Chanseok Oh <chanseok@google.com>
2017-04-16 19:46:00 -0400

    Comment typo

--- plugins/com.google.cloud.tools.eclipse.appengine.deploy/src/com/google/cloud/tools/eclipse/appengine/deploy/DeployJob.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.deploy/src/com/google/cloud/tools/eclipse/appengine/deploy/DeployJob.java
@@ -91,7 +91,7 @@ public class DeployJob extends WorkspaceJob {
    *     log messages)
    * @param deployConfiguration configuration passed to {@link CloudSdk} that describes what and
    *     how to deploy
-   * @param includeOptionalConfigurationFiles if true, deploys optional XML configurations files
+   * @param includeOptionalConfigurationFiles if true, deploys optional XML configuration files
    *     (e.g., {@code queue.yaml}) together
    */
   public DeployJob(IProject project, Credential credential, IPath workDirectory,
