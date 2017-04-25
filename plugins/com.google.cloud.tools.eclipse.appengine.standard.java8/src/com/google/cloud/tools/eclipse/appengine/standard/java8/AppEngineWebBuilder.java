09b8143c54491e6735c7ecb4ad2c32de5ad13c96
Brian de Alwis <bsd@mt.ca>
2017-04-24 23:54:33 -0400

    Add builder to update facets in response to appengine-web.xml <runtime> changes

---
+++ plugins/com.google.cloud.tools.eclipse.appengine.standard.java8/src/com/google/cloud/tools/eclipse/appengine/standard/java8/AppEngineWebBuilder.java
@@ -0,0 +1,85 @@
+
+package com.google.cloud.tools.eclipse.appengine.standard.java8;
+
+import com.google.cloud.tools.appengine.AppEngineDescriptor;
+import com.google.cloud.tools.eclipse.appengine.facets.AppEngineStandardFacet;
+import com.google.cloud.tools.eclipse.appengine.facets.WebProjectUtil;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jst.common.project.facet.core.JavaFacet;
+import org.eclipse.jst.j2ee.web.project.facet.WebFacetUtils;
+import org.eclipse.wst.common.project.facet.core.IFacetedProject;
+import org.eclipse.wst.common.project.facet.core.IFacetedProject.Action;
+import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
+
+public class AppEngineWebBuilder extends IncrementalProjectBuilder {
+  private static final Logger logger = Logger.getLogger(AppEngineWebBuilder.class.getName());
+  static final String BUILDER_ID =
+      "com.google.cloud.tools.eclipse.appengine.standard.java8.appengineWeb";
+
+  @Override
+  protected IProject[] build(int kind, Map<String, String> args, IProgressMonitor monitor)
+      throws CoreException {
+    IFacetedProject project = ProjectFacetsManager.create(getProject());
+    if (project == null || !AppEngineStandardFacet.hasFacet(project)) {
+      logger.fine(getProject() + ": no build required: no App Engine Standard facet");
+      return null;
+    }
+    IFile appEngineWebDescriptor =
+        WebProjectUtil.findInWebInf(project.getProject(), new Path("appengine-web.xml"));
+    if (appEngineWebDescriptor == null || !appEngineWebDescriptor.exists()) {
+      logger.warning(getProject() + ": no build required: missing appengine-web.xml");
+      return null;
+    }
+    if (kind == FULL_BUILD) {
+      checkRuntimeElement(project, appEngineWebDescriptor, monitor);
+    } else {
+      IResourceDelta delta = getDelta(getProject());
+      if (delta == null
+          || delta.findMember(appEngineWebDescriptor.getProjectRelativePath()) != null) {
+        checkRuntimeElement(project, appEngineWebDescriptor, monitor);
+      } else {
+        logger.finer(getProject() + ": no build required: appengine-web.xml not changed");
+      }
+    }
+    return null;
+  }
+
+  private void checkRuntimeElement(IFacetedProject project, IFile appEngineWebDescriptor,
+      IProgressMonitor monitor) {
+    try (InputStream input = appEngineWebDescriptor.getContents()) {
+      boolean isJava8 = AppEngineDescriptor.parse(input).isJava8();
+      if (project.hasProjectFacet(JavaFacet.VERSION_1_8) != isJava8) {
+        Set<Action> updates = new HashSet<Action>();
+        // Can upgrade jst.web to 3.1, but cannot downgrade from 3.1
+        if (isJava8) {
+          updates.add(new Action(Action.Type.VERSION_CHANGE, JavaFacet.VERSION_1_8, null));
+          updates.add(new Action(Action.Type.VERSION_CHANGE, WebFacetUtils.WEB_31, null));
+        } else {
+          updates.add(new Action(Action.Type.VERSION_CHANGE, JavaFacet.VERSION_1_7, null));
+        }
+        logger.fine(getProject() + ": changing facets: " + updates);
+        project.modify(updates, monitor);
+      }
+    } catch (CoreException | IOException ex) {
+      logger.log(Level.SEVERE, getProject() + ": error updating facets", ex);
+    }
+  }
+
+  protected void clean(IProgressMonitor monitor) throws CoreException {
+    // nothing to do
+  }
+
+}
---
+++ plugins/com.google.cloud.tools.eclipse.appengine.standard.java8/src/com/google/cloud/tools/eclipse/appengine/standard/java8/FacetChangeListener.java
@@ -0,0 +1,115 @@
+
+package com.google.cloud.tools.eclipse.appengine.standard.java8;
+
+import com.google.cloud.tools.eclipse.appengine.facets.AppEngineStandardFacet;
+import com.google.cloud.tools.eclipse.appengine.facets.WebProjectUtil;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.eclipse.core.resources.ICommand;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jst.common.project.facet.core.JavaFacet;
+import org.eclipse.wst.common.project.facet.core.IFacetedProject;
+import org.eclipse.wst.common.project.facet.core.events.IFacetedProjectEvent;
+import org.eclipse.wst.common.project.facet.core.events.IFacetedProjectEvent.Type;
+import org.eclipse.wst.common.project.facet.core.events.IFacetedProjectListener;
+import org.eclipse.wst.common.project.facet.core.events.IProjectFacetActionEvent;
+
+/**
+ * Handle project facet changes such as adding or removing the App Engine Standard facet, or Java
+ * facet, and installing the {@code appengine-web.xml} builder.
+ */
+public class FacetChangeListener implements IFacetedProjectListener {
+  private static final Logger logger = Logger.getLogger(FacetChangeListener.class.getName());
+
+  @Override
+  public void handleEvent(IFacetedProjectEvent event) {
+    IFacetedProject project = event.getProject();
+    if (!AppEngineStandardFacet.hasFacet(project)) {
+      removeAppEngineWebBuilder(project.getProject());
+      return;
+    }
+    addAppEngineWebBuilder(project.getProject());
+    if (event.getType() == Type.POST_INSTALL || event.getType() == Type.POST_VERSION_CHANGE) {
+      IProjectFacetActionEvent action = (IProjectFacetActionEvent) event;
+      if (JavaFacet.FACET.equals(action.getProjectFacet())) {
+        IFile descriptor = findDescriptor(project);
+        if (descriptor == null) {
+          logger.warning(project + ": cannot find appengine-web.xml");
+          return;
+        }
+        if (project.hasProjectFacet(JavaFacet.VERSION_1_8)) {
+          AppEngineDescriptorTransform.addJava8Runtime(descriptor);
+        } else {
+          AppEngineDescriptorTransform.removeJava8Runtime(descriptor);
+        }
+      }
+    }
+  }
+
+  /**
+   * @param project
+   */
+  private void addAppEngineWebBuilder(IProject project) {
+    try {
+      IProjectDescription desc = project.getDescription();
+      ICommand[] commands = desc.getBuildSpec();
+      for (int i = 0; i < commands.length; i++) {
+        if (AppEngineWebBuilder.BUILDER_ID.equals(commands[i].getBuilderName())) {
+          return;
+        }
+      }
+      ICommand[] nc = new ICommand[commands.length + 1];
+      // Add it after other builders.
+      System.arraycopy(commands, 0, nc, 0, commands.length);
+      // add builder to project
+      ICommand command = desc.newCommand();
+      command.setBuilderName(AppEngineWebBuilder.BUILDER_ID);
+      nc[commands.length] = command;
+      desc.setBuildSpec(nc);
+      project.setDescription(desc, null);
+      logger.finer(project + ": added AppEngineWebBuilder");
+    } catch (CoreException ex) {
+      logger.log(Level.SEVERE, "Unable to add builder for " + project, ex);
+    }
+  }
+
+  /**
+   * @param project
+   */
+  private void removeAppEngineWebBuilder(IProject project) {
+    try {
+      IProjectDescription desc = project.getDescription();
+      ICommand[] commands = desc.getBuildSpec();
+      for (int i = 0; i < commands.length; i++) {
+        if (AppEngineWebBuilder.BUILDER_ID.equals(commands[i].getBuilderName())) {
+          ICommand[] nc = new ICommand[commands.length - 1];
+          System.arraycopy(commands, 0, nc, 0, i);
+          System.arraycopy(commands, i + 1, nc, i, commands.length - i - 1);
+          desc.setBuildSpec(nc);
+          project.setDescription(desc, null);
+          logger.finer(project + ": removed AppEngineWebBuilder");
+          return;
+        }
+      }
+    } catch (CoreException ex) {
+      logger.log(Level.SEVERE, "Unable to remove builder for " + project, ex);
+    }
+
+  }
+
+  /**
+   * Find the <code>appengine-web.xml</code> file.
+   * 
+   * @return the file or {@code null} if not found
+   */
+  private IFile findDescriptor(IFacetedProject project) {
+    IFile descriptor =
+        WebProjectUtil.findInWebInf(project.getProject(), new Path("appengine-web.xml"));
+    return descriptor;
+  }
+
+}
