147d8e191cc8847365a81d79b3d3755e7c86b8aa
Andras Kerekes <akerekes@users.noreply.github.com>
2017-04-25 18:20:24 -0400

    Update continuous.bat

--- kokoro/windows/continuous.bat
+++ kokoro/windows/continuous.bat
@@ -10,7 +10,7 @@ set GOOGLE_CLOUD_SDK_HOME=t:\google\google-cloud-sdk
 wget http://www-us.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.zip
 unzip apache-maven-3.3.9-bin.zip
 
-apache-maven-3.3.9/bin/mvn clean install -B -U
+apache-maven-3.3.9/bin/mvn verify -Ptravis
 REM curl -s https://codecov.io/bash | bash
 
 exit /b %ERRORLEVEL%
