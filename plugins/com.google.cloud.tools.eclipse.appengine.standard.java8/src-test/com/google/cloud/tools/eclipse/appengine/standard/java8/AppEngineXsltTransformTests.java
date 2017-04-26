aa942cd83b95561c82e67847c1eb790e975a72f4
Brian de Alwis <bsd@mt.ca>
2017-04-26 10:38:31 -0400

    Fix AppEngineXsltTransformTests

--- plugins/com.google.cloud.tools.eclipse.appengine.standard.java8/src-test/com/google/cloud/tools/eclipse/appengine/standard/java8/AppEngineXsltTransformTests.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.standard.java8/src-test/com/google/cloud/tools/eclipse/appengine/standard/java8/AppEngineXsltTransformTests.java
@@ -17,19 +17,25 @@
 package com.google.cloud.tools.eclipse.appengine.standard.java8;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import com.google.cloud.tools.eclipse.util.Xslt;
-import com.google.common.io.CharStreams;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  * Test the appengine-web.xml XSLT transforms
@@ -37,100 +43,123 @@ import org.junit.Test;
 public class AppEngineXsltTransformTests {
 
   @Test
-  public void testAddBare() throws IOException, TransformerException {
-    apply("/xslt/addJava8Runtime.xsl",
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\"/>",
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\">"
-            + "<runtime>java8</runtime></appengine-web-app>");
+  public void testAddBare() {
+    Document transformed = transform("/xslt/addJava8Runtime.xsl",
+        "<appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\"/>");
+    assertEquals("java8", getRuntimeElement(transformed));
   }
 
   @Test
   public void testAddExistingRuntime() throws IOException, TransformerException {
-    apply("/xslt/addJava8Runtime.xsl",
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\"><runtime>XXX</runtime></appengine-web-app>",
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\">"
-            + "<runtime>java8</runtime></appengine-web-app>");
+    Document transformed = transform("/xslt/addJava8Runtime.xsl",
+        "<appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\"><runtime>XXX</runtime></appengine-web-app>");
+    assertEquals("java8", getRuntimeElement(transformed));
   }
 
   @Test
   public void testAddPreservesOrder() throws IOException, TransformerException {
-    apply("/xslt/addJava8Runtime.xsl",
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\">"
+    Document transformed = transform("/xslt/addJava8Runtime.xsl",
+        "<appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\">"
             + "<threadsafe>true</threadsafe><runtime>XXX</runtime>"
-            + "<sessions-enabled>true</sessions-enabled></appengine-web-app>",
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\">"
-            + "<threadsafe>true</threadsafe><runtime>java8</runtime>"
             + "<sessions-enabled>true</sessions-enabled></appengine-web-app>");
+    Element documentElement = transformed.getDocumentElement();
+    NodeList childElements = documentElement.getChildNodes();
+    assertEquals("appengine-web-app", documentElement.getTagName());
+    assertEquals(3, childElements.getLength());
+    assertEquals("threadsafe", ((Element) childElements.item(0)).getTagName());
+    assertEquals("true", childElements.item(0).getTextContent());
+    assertEquals("runtime", ((Element) childElements.item(1)).getTagName());
+    assertEquals("java8", childElements.item(1).getTextContent());
+    assertEquals("sessions-enabled", ((Element) childElements.item(2)).getTagName());
+    assertEquals("true", childElements.item(2).getTextContent());
   }
 
   @Test
   public void testAddExistingJava8Runtime() throws IOException, TransformerException {
-    apply("/xslt/addJava8Runtime.xsl",
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\">"
-            + "<runtime>java8</runtime></appengine-web-app>",
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\">"
+    Document transformed = transform("/xslt/addJava8Runtime.xsl",
+        "<appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\">"
             + "<runtime>java8</runtime></appengine-web-app>");
+    assertEquals("java8", getRuntimeElement(transformed));
   }
 
   @Test
-  public void testdAdBareNoDefaultNS() throws IOException, TransformerException {
-    apply("/xslt/addJava8Runtime.xsl",
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><ae:appengine-web-app xmlns:ae=\"http://appengine.google.com/ns/1.0\"/>",
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><ae:appengine-web-app xmlns:ae=\"http://appengine.google.com/ns/1.0\">"
-            + "<runtime xmlns=\"http://appengine.google.com/ns/1.0\">java8</runtime>"
-            + "</ae:appengine-web-app>");
+  public void testdAddBareNoDefaultNS() throws IOException, TransformerException {
+    Document transformed = transform("/xslt/addJava8Runtime.xsl",
+        "<ae:appengine-web-app xmlns:ae=\"http://appengine.google.com/ns/1.0\"/>");
+    assertEquals("java8", getRuntimeElement(transformed));
   }
 
   @Test
   public void testRemoveBare() throws IOException, TransformerException {
-    apply("/xslt/removeJava8Runtime.xsl",
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\"/>",
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\"/>");
+    Document transformed = transform("/xslt/removeJava8Runtime.xsl",
+        "<appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\"/>");
+    assertNull(getRuntimeElement(transformed));
   }
 
   @Test
   public void testRemovePreservesNonJava8Runtime() throws IOException, TransformerException {
-    apply("/xslt/removeJava8Runtime.xsl",
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\">"
-            + "<runtime>XXX</runtime></appengine-web-app>",
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\">"
+    Document transformed = transform("/xslt/removeJava8Runtime.xsl",
+        "<appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\">"
             + "<runtime>XXX</runtime></appengine-web-app>");
+    assertEquals("XXX", getRuntimeElement(transformed));
   }
 
   @Test
   public void testRemovePreservesOrder() throws IOException, TransformerException {
-    apply("/xslt/removeJava8Runtime.xsl",
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\">"
+    Document transformed = transform("/xslt/removeJava8Runtime.xsl",
+        "<appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\">"
             + "<threadsafe>true</threadsafe>" + "<runtime>java8</runtime>"
-            + "<sessions-enabled>true</sessions-enabled>" + "</appengine-web-app>",
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\">"
-            + "<threadsafe>true</threadsafe>" + "<sessions-enabled>true</sessions-enabled>"
-            + "</appengine-web-app>");
+            + "<sessions-enabled>true</sessions-enabled>" + "</appengine-web-app>");
+    Element documentElement = transformed.getDocumentElement();
+    NodeList childElements = documentElement.getChildNodes();
+    assertEquals("appengine-web-app", documentElement.getTagName());
+    assertEquals(2, childElements.getLength());
+    assertEquals("threadsafe", ((Element) childElements.item(0)).getTagName());
+    assertEquals("true", childElements.item(0).getTextContent());
+    assertEquals("sessions-enabled", ((Element) childElements.item(1)).getTagName());
+    assertEquals("true", childElements.item(1).getTextContent());
   }
 
   @Test
   public void testRemoveExistingJava8Runtime() throws IOException, TransformerException {
-    apply("/xslt/removeJava8Runtime.xsl",
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\">"
-            + "<runtime>java8</runtime></appengine-web-app>",
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\"/>");
+    Document transformed = transform("/xslt/removeJava8Runtime.xsl",
+        "<appengine-web-app xmlns=\"http://appengine.google.com/ns/1.0\">"
+            + "<runtime>java8</runtime></appengine-web-app>");
+    assertNull(getRuntimeElement(transformed));
   }
 
-  private void apply(String templateFile, String inputString, String outputString)
-      throws IOException, TransformerException {
-    URL xslPath = AppEngineXsltTransformTests.class.getResource(templateFile);
-    if (xslPath == null) {
-      File xslFile = new File("." + templateFile);
-      assertTrue(xslFile.exists());
-      xslPath = xslFile.toURI().toURL();
+  private Document transform(String templateFile, String inputValue) {
+    try {
+      URL xslPath = AppEngineXsltTransformTests.class.getResource(templateFile);
+      if (xslPath == null) {
+        File xslFile = new File("." + templateFile);
+        assertTrue(xslFile.exists());
+        xslPath = xslFile.toURI().toURL();
+      }
+      InputStream documentStream =
+          new ByteArrayInputStream(inputValue.getBytes(StandardCharsets.UTF_8));
+      InputStream transformed = Xslt.applyXslt(documentStream, xslPath.openStream());
+      DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+      builderFactory.setNamespaceAware(true);
+      DocumentBuilder builder = builderFactory.newDocumentBuilder();
+      return builder.parse(transformed);
+    } catch (IOException | TransformerException | ParserConfigurationException | SAXException ex) {
+      // these aren't interesting for the test
+      throw new RuntimeException(ex);
     }
-    InputStream documentStream =
-        new ByteArrayInputStream(inputString.getBytes(StandardCharsets.UTF_8));
-    InputStream transformed = Xslt.applyXslt(documentStream, xslPath.openStream());
-    String result =
-        CharStreams.toString(new InputStreamReader(transformed, StandardCharsets.UTF_8));
-    result = result.trim();
-    assertEquals(outputString, result);
+
   }
 
+  /** Return the {@code <runtime>} value, or {@code null} if no element found. */
+  private String getRuntimeElement(Document transformed) {
+    Element documentElement = transformed.getDocumentElement();
+    NodeList runtimeElements =
+        documentElement.getElementsByTagNameNS("http://appengine.google.com/ns/1.0", "runtime");
+    if (runtimeElements.getLength() == 0) {
+      return null;
+    }
+    assertTrue(runtimeElements.getLength() == 1);
+    assertTrue("runtime".equals(runtimeElements.item(0).getLocalName()));
+    return runtimeElements.item(0).getTextContent();
+  }
 }
