81a9c4cb6d5e57facc866a1a4175807272459d3e
Chanseok Oh <chanseok@google.com>
2017-05-17 17:48:42 -0400

    WIP

--- plugins/com.google.cloud.tools.eclipse.appengine.newproject/src/com/google/cloud/tools/eclipse/appengine/newproject/flex/AppEngineFlexWizardPage.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.newproject/src/com/google/cloud/tools/eclipse/appengine/newproject/flex/AppEngineFlexWizardPage.java
@@ -19,11 +19,17 @@ package com.google.cloud.tools.eclipse.appengine.newproject.flex;
 import com.google.cloud.tools.eclipse.appengine.newproject.AppEngineProjectConfig.BuildTool;
 import com.google.cloud.tools.eclipse.appengine.newproject.AppEngineWizardPage;
 import com.google.cloud.tools.eclipse.appengine.newproject.Messages;
+import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.PlatformUI;
 
 public class AppEngineFlexWizardPage extends AppEngineWizardPage {
@@ -32,6 +38,13 @@ public class AppEngineFlexWizardPage extends AppEngineWizardPage {
   // "createControl()", and "getBuildTool()" down to super class.
   // https://github.com/GoogleCloudPlatform/google-cloud-eclipse/issues/1326
   private Button asMavenProject;
+  private Label groupIdLabel;
+  private Text groupIdText;
+  private Label artifactIdLabel;
+  private Text artifactIdText;
+  private Label versionLabel;
+  private Text versionText;
+  private Group mavenSection;
 
   public AppEngineFlexWizardPage() {
     super(false);
@@ -56,10 +69,52 @@ public class AppEngineFlexWizardPage extends AppEngineWizardPage {
 
     Composite container = (Composite) getControl();
     Composite composite = new Composite(container, SWT.NONE);
-    GridLayoutFactory.swtDefaults().numColumns(2).generateLayout(composite);
 
-    asMavenProject = new Button(composite, SWT.LEAD | SWT.CHECK);
+    asMavenProject = new Button(composite, SWT.CHECK);
     asMavenProject.setText(Messages.getString("create.maven.project")); //$NON-NLS-1$
+    asMavenProject.addSelectionListener(new SelectionAdapter() {
+      @Override
+      public void widgetSelected(SelectionEvent event) {
+        updateMavenSectionEnabled();
+      }
+    });
+
+    mavenSection = new Group(composite, SWT.NONE);
+    groupIdLabel = new Label(mavenSection, SWT.NONE);
+    groupIdLabel.setText("Group ID:");
+    groupIdText = new Text(mavenSection, SWT.BORDER);
+    groupIdText.setText("com.example");
+    artifactIdLabel = new Label(mavenSection, SWT.NONE);
+    artifactIdLabel.setText("Artifact ID:");
+    artifactIdText = new Text(mavenSection, SWT.BORDER);
+    artifactIdText.setText("awesome-plugin");
+    versionLabel = new Label(mavenSection, SWT.NONE);
+    versionLabel.setText("Version:");
+    versionText = new Text(mavenSection, SWT.BORDER);
+    versionText.setText("0.1.0-SNAPSHOT");
+
+    updateMavenSectionEnabled();  // Disable initially.
+
+    GridLayoutFactory.swtDefaults().generateLayout(composite);
+    GridDataFactory.swtDefaults().align(SWT.FILL, SWT.BEGINNING).applyTo(composite);
+    GridLayoutFactory.swtDefaults().numColumns(2).generateLayout(mavenSection);
+    GridDataFactory horizontalFill =
+        GridDataFactory.swtDefaults().align(SWT.FILL, SWT.BEGINNING).grab(true, false);
+    horizontalFill.applyTo(mavenSection);
+    horizontalFill.applyTo(groupIdText);
+    horizontalFill.applyTo(artifactIdText);
+    horizontalFill.applyTo(versionText);
+  }
+
+  private void updateMavenSectionEnabled() {
+    boolean checked = asMavenProject.getSelection();
+    mavenSection.setEnabled(checked);
+    groupIdLabel.setEnabled(checked);
+    groupIdText.setEnabled(checked);
+    artifactIdLabel.setEnabled(checked);
+    artifactIdText.setEnabled(checked);
+    versionLabel.setEnabled(checked);
+    versionText.setEnabled(checked);
   }
 
   @Override
