5df025fe4fa6c76003ecac60dfe8d0fede36c633
Chanseok Oh <chanseok@google.com>
2017-07-14 15:36:19 -0400

    Parameterize tests

--- plugins/com.google.cloud.tools.eclipse.dataflow.ui.test/src/com/google/cloud/tools/eclipse/dataflow/ui/launcher/PipelineArgumentsTabTest.java
+++ plugins/com.google.cloud.tools.eclipse.dataflow.ui.test/src/com/google/cloud/tools/eclipse/dataflow/ui/launcher/PipelineArgumentsTabTest.java
@@ -24,6 +24,7 @@ import com.google.cloud.tools.eclipse.dataflow.core.project.MajorVersion;
 import com.google.cloud.tools.eclipse.test.util.ui.CompositeUtil;
 import com.google.cloud.tools.eclipse.test.util.ui.ShellTestResource;
 import com.google.common.base.Predicate;
+import com.google.common.collect.Lists;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -32,67 +33,87 @@ import org.eclipse.swt.widgets.Shell;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  PipelineArgumentsTabTest.TabTest.class,
+  PipelineArgumentsTabTest.RunButtonCheckedTest.class
+})
 
 public class PipelineArgumentsTabTest {
 
-  @Rule public ShellTestResource shellResource = new ShellTestResource();
+  public static class TabTest {
 
-  private PipelineArgumentsTab tab = new PipelineArgumentsTab();
+    private PipelineArgumentsTab tab = new PipelineArgumentsTab();
 
-  @Test
-  public void testGetName() {
-    Assert.assertEquals("Pipeline Arguments", tab.getName());
+    @Test
+    public void testGetName() {
+      Assert.assertEquals("Pipeline Arguments", tab.getName());
+    }
   }
 
-  @Test
-  public void testRunnerButtonChecked_majorVersionOne() {
-    assertRunnerButtonChecked(MajorVersion.ONE, "DirectPipelineRunner");
-  }
+  @RunWith(Parameterized.class)
+  public static class RunButtonCheckedTest {
 
-  @Test
-  public void testRunnerButtonChecked_majorVersionTwo() {
-    assertRunnerButtonChecked(MajorVersion.TWO, "DirectRunner");
-  }
+    private static class Parameter {
+      private final MajorVersion majorVersion;
+      private final String expectedButtonText;
 
-  @Test
-  public void testRunnerButtonChecked_majorVersionQualifiedTwo() {
-    assertRunnerButtonChecked(MajorVersion.QUALIFIED_TWO, "DirectRunner");
-  }
-
-  @Test
-  public void testRunnerButtonChecked_majorVersionThreePlus() {
-    assertRunnerButtonChecked(MajorVersion.THREE_PLUS, "DirectRunner");
-  }
-
-  @Test
-  public void testRunnerButtonChecked_majorVersionAll() {
-    assertRunnerButtonChecked(MajorVersion.ALL, "DirectPipelineRunner");
-  }
-
-  private void assertRunnerButtonChecked(MajorVersion majorVersion, String expectedButtonText) {
-    Shell shell = shellResource.getShell();
-    tab.createControl(shell);
-
-    tab.updateRunnerButtons(majorVersion);
-    Button runnerButton = getCheckedRunnerButton(shell);
-    assertNotNull(runnerButton);
-    assertEquals(expectedButtonText, runnerButton.getText());
-    assertTrue(runnerButton.getSelection());
-
-    // Should not throw IllegalStateException:
-    // https://github.com/GoogleCloudPlatform/google-cloud-eclipse/issues/2136
-    tab.getSelectedRunner();
-  }
-
-  private static Button getCheckedRunnerButton(Composite composite) {
-    Group runnerGroup = CompositeUtil.findControl(composite, Group.class);
-    assertEquals("Runner:", runnerGroup.getText());
-
-    return (Button) CompositeUtil.findControl(runnerGroup, new Predicate<Control>() {
-      @Override
-      public boolean apply(Control control) {
-        return control instanceof Button && ((Button) control).getSelection();
+      private Parameter(MajorVersion majorVersion, String expectedButtonText) {
+        this.majorVersion = majorVersion;
+        this.expectedButtonText = expectedButtonText;
       }
-    });
+    }
+
+    @Parameters
+    public static Iterable<? extends Object> parameters() {
+      return Lists.newArrayList(
+          new Parameter(MajorVersion.ONE, "DirectPipelineRunner"),
+          new Parameter(MajorVersion.TWO, "DirectRunner"),
+          new Parameter(MajorVersion.QUALIFIED_TWO, "DirectRunner"),
+          new Parameter(MajorVersion.THREE_PLUS, "DirectRunner"),
+          new Parameter(MajorVersion.ALL, "DirectPipelineRunner"));
+    }
+
+    @Rule public ShellTestResource shellResource = new ShellTestResource();
+
+    private PipelineArgumentsTab tab = new PipelineArgumentsTab();
+
+    private final Parameter testParameter;
+    public RunButtonCheckedTest(Parameter parameter) {
+      testParameter = parameter;
+    }
+
+    @Test
+    public void assertRunnerButtonChecked() {
+      Shell shell = shellResource.getShell();
+      tab.createControl(shell);
+
+      tab.updateRunnerButtons(testParameter.majorVersion);
+      Button runnerButton = getCheckedRunnerButton(shell);
+      assertNotNull(runnerButton);
+      assertEquals(testParameter.expectedButtonText, runnerButton.getText());
+      assertTrue(runnerButton.getSelection());
+
+      // Should not throw IllegalStateException:
+      // https://github.com/GoogleCloudPlatform/google-cloud-eclipse/issues/2136
+      tab.getSelectedRunner();
+    }
+
+    private static Button getCheckedRunnerButton(Composite composite) {
+      Group runnerGroup = CompositeUtil.findControl(composite, Group.class);
+      assertEquals("Runner:", runnerGroup.getText());
+
+      return (Button) CompositeUtil.findControl(runnerGroup, new Predicate<Control>() {
+        @Override
+        public boolean apply(Control control) {
+          return control instanceof Button && ((Button) control).getSelection();
+        }
+      });
+    }
   }
 }
