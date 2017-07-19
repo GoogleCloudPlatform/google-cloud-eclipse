578733b3ac294afb2341e62f3dbf8174786096b5
Elliotte Rusty Harold <elharo@google.com>
2017-07-19 11:02:51 -0400

    remove annotation

--- plugins/com.google.cloud.tools.eclipse.appengine.libraries.test/src/com/google/cloud/tools/eclipse/appengine/libraries/model/LibraryFactoryTest.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.libraries.test/src/com/google/cloud/tools/eclipse/appengine/libraries/model/LibraryFactoryTest.java
@@ -21,9 +21,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
 
-@RunWith(MockitoJUnitRunner.class)
 public class LibraryFactoryTest {
   
   private LibraryFactory factory = new LibraryFactory();
