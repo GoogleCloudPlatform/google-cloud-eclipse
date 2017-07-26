57cf3a38a447abb5b9ad2d2e4760a111fa601db2
Chanseok Oh <chanseok@google.com>
2017-07-26 16:36:10 -0400

    Refactor DeployJob

---
+++ plugins/com.google.cloud.tools.eclipse.appengine.deploy/src/com/google/cloud/tools/eclipse/appengine/deploy/CloudSdkProcessFacade.java
@@ -0,0 +1,150 @@
+package com.google.cloud.tools.eclipse.appengine.deploy;
+
+import com.google.cloud.tools.appengine.cloudsdk.CloudSdk;
+import com.google.cloud.tools.appengine.cloudsdk.process.ProcessExitListener;
+import com.google.cloud.tools.appengine.cloudsdk.process.ProcessOutputLineListener;
+import com.google.cloud.tools.appengine.cloudsdk.process.ProcessStartListener;
+import com.google.cloud.tools.appengine.cloudsdk.process.StringBuilderProcessOutputLineListener;
+import com.google.cloud.tools.eclipse.sdk.CollectingLineListener;
+import com.google.cloud.tools.eclipse.util.CloudToolsInfo;
+import com.google.cloud.tools.eclipse.util.status.StatusUtil;
+import com.google.common.base.Joiner;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
+import java.nio.file.Path;
+import java.util.List;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+
+public class CloudSdkProcessFacade {
+
+  private static final String ERROR_MESSAGE_PREFIX = "ERROR:";
+  private static final Predicate<String> IS_ERROR_LINE = new Predicate<String>() {
+    @Override
+    public boolean apply(String line) {
+      return line != null && line.startsWith(ERROR_MESSAGE_PREFIX);
+    }
+  };
+
+  private CloudSdk cloudSdk;
+
+  public Process process;
+  public boolean canceled;
+  private IStatus exitStatus = Status.OK_STATUS;
+  private ProcessOutputLineListener stdOutCaptor;
+
+  private CloudSdkProcessFacade() {  // empty private constructor
+  }
+
+  /**
+   * Creates a wrapper/facade of {@link CloudSdk} to be used for App Engine deploy.
+   */
+  public static CloudSdkProcessFacade forDeploy(Path credentialFile,
+      ProcessOutputLineListener normalOutputListener) {
+    Preconditions.checkNotNull(credentialFile);
+
+    // Normal operation output goes to stderr.
+    ProcessOutputLineListener stdErrListener = normalOutputListener;
+    // Structured deploy result (in JSON format) goes to stdout, so prepare to capture that.
+    ProcessOutputLineListener stdOutCaptor = new StringBuilderProcessOutputLineListener();
+
+    CloudSdkProcessFacade facade = new CloudSdkProcessFacade();
+    CloudSdk.Builder cloudSdkBuilder = facade.getBaseCloudSdkBuilder(stdErrListener);
+    cloudSdkBuilder.appCommandCredentialFile(credentialFile.toFile());
+    cloudSdkBuilder.addStdOutLineListener(stdOutCaptor);
+    facade.cloudSdk = cloudSdkBuilder.build();
+    facade.stdOutCaptor = stdOutCaptor;
+
+    return facade;
+  }
+
+  /**
+   * Creates a wrapper/facade of {@link CloudSdk} to be used for App Engine standard staging.
+   *
+   * @param javaHome JDK/JRE to run gcloud, which is also used to compile JSPs during staging
+   */
+  public static CloudSdkProcessFacade forStandardStaging(Path javaHome,
+      ProcessOutputLineListener stdOutListener, ProcessOutputLineListener stdErrListener) {
+    Preconditions.checkNotNull(javaHome);
+
+    CloudSdkProcessFacade cloudSdkRunner = new CloudSdkProcessFacade();
+    CloudSdk.Builder cloudSdkBuilder =
+        cloudSdkRunner.getBaseCloudSdkBuilder(stdErrListener);
+    cloudSdkBuilder.javaHome(javaHome);
+    cloudSdkBuilder.addStdOutLineListener(stdOutListener);
+    cloudSdkRunner.cloudSdk = cloudSdkBuilder.build();
+
+    return cloudSdkRunner;
+  }
+
+  private CloudSdk.Builder getBaseCloudSdkBuilder(ProcessOutputLineListener stdErrListener) {
+    CollectingLineListener errorMessageCollector = new CollectingLineListener(IS_ERROR_LINE);
+
+    return new CloudSdk.Builder()
+        .addStdErrLineListener(stdErrListener)
+        .addStdErrLineListener(errorMessageCollector)
+        .startListener(new StoreProcessObjectListener())
+        .exitListener(new ProcessExitRecorder(errorMessageCollector))
+        .appCommandMetricsEnvironment(CloudToolsInfo.METRICS_NAME)
+        .appCommandMetricsEnvironmentVersion(CloudToolsInfo.getToolsVersion())
+        .appCommandOutputFormat("json");
+  }
+
+  public CloudSdk getCloudSdk() {
+    return cloudSdk;
+  }
+
+  public void cancel() {
+    canceled = true;  // not to miss destruction due to race condition
+    if (process != null) {
+      process.destroy();
+    }
+  }
+
+  public IStatus getExitStatus() {
+    return exitStatus;
+  }
+
+  public String getStdOutAsString() {
+    Preconditions.checkNotNull(stdOutCaptor);
+    return stdOutCaptor.toString();
+  }
+
+  private class StoreProcessObjectListener implements ProcessStartListener {
+    @Override
+    public void onStart(Process proces) {
+      process = proces;
+      if (canceled) {
+        process.destroy();
+      }
+    }
+  }
+
+  private class ProcessExitRecorder implements ProcessExitListener {
+
+    private final CollectingLineListener errorMessageCollector;
+
+    private ProcessExitRecorder(CollectingLineListener errorMessageCollector) {
+      this.errorMessageCollector = errorMessageCollector;
+    }
+
+    @Override
+    public void onExit(int exitCode) {
+      if (exitCode != 0) {
+        exitStatus = StatusUtil.error(this, getErrorMessage(exitCode));
+      } else {
+        exitStatus = Status.OK_STATUS;
+      }
+    }
+
+    private String getErrorMessage(int exitCode) {
+      if (errorMessageCollector != null) {
+        List<String> lines = errorMessageCollector.getCollectedMessages();
+        if (!lines.isEmpty()) {
+          return Joiner.on('\n').join(lines);
+        }
+      }
+      return Messages.getString("cloudsdk.process.failed", exitCode);
+    }
+  }
+}
--- plugins/com.google.cloud.tools.eclipse.appengine.deploy/src/com/google/cloud/tools/eclipse/appengine/deploy/DeployJob.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.deploy/src/com/google/cloud/tools/eclipse/appengine/deploy/DeployJob.java
@@ -19,22 +19,14 @@ package com.google.cloud.tools.eclipse.appengine.deploy;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.cloud.tools.appengine.api.deploy.DefaultDeployConfiguration;
 import com.google.cloud.tools.appengine.cloudsdk.CloudSdk;
-import com.google.cloud.tools.appengine.cloudsdk.process.ProcessExitListener;
 import com.google.cloud.tools.appengine.cloudsdk.process.ProcessOutputLineListener;
-import com.google.cloud.tools.appengine.cloudsdk.process.ProcessStartListener;
-import com.google.cloud.tools.appengine.cloudsdk.process.StringBuilderProcessOutputLineListener;
 import com.google.cloud.tools.eclipse.login.CredentialHelper;
-import com.google.cloud.tools.eclipse.sdk.CollectingLineListener;
 import com.google.cloud.tools.eclipse.ui.util.WorkbenchUtil;
-import com.google.cloud.tools.eclipse.util.CloudToolsInfo;
 import com.google.cloud.tools.eclipse.util.status.StatusUtil;
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Joiner;
-import com.google.common.base.Predicate;
 import com.google.gson.JsonParseException;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.List;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
@@ -62,23 +54,20 @@ public class DeployJob extends WorkspaceJob {
   private static final String STAGING_DIRECTORY_NAME = "staging";
   private static final String SAFE_STAGING_WORK_DIRECTORY_NAME = "staging-work";
   private static final String CREDENTIAL_FILENAME = "gcloud-credentials.json";
-  private static final String ERROR_MESSAGE_PREFIX = "ERROR:";
   private static final String DEFAULT_SERVICE = "default";
-
-  //temporary way of error handling, after #439 is fixed, it'll be cleaner
-  private IStatus cloudSdkProcessStatus = Status.OK_STATUS;
-  private Process process;
 
   private final IProject project;
   private final Credential credential;
   private final IPath workDirectory;
   private final ProcessOutputLineListener stagingStdoutLineListener;
-  private final ProcessOutputLineListener deployStdoutLineListener;
   private final ProcessOutputLineListener stderrLineListener;
   private final DefaultDeployConfiguration deployConfiguration;
   private final boolean includeOptionalConfigurationFiles;
-  private final CollectingLineListener errorCollectingLineListener;
   private final StagingDelegate stager;
+
+  private CloudSdkProcessFacade deployCloudSdkFacade;
+  private CloudSdkProcessFacade stagingCloudSdkFacade;
+  private boolean canceled;
 
   /**
    * @param workDirectory temporary work directory the job can safely use (e.g., for creating and
@@ -109,15 +98,6 @@ public class DeployJob extends WorkspaceJob {
     this.deployConfiguration = deployConfiguration;
     this.includeOptionalConfigurationFiles = includeOptionalConfigurationFiles;
     this.stager = stager;
-    deployStdoutLineListener = new StringBuilderProcessOutputLineListener();
-    errorCollectingLineListener =
-        new CollectingLineListener(new Predicate<String>() {
-                                     @Override
-                                     public boolean apply(String line) {
-                                       return line != null
-                                           && line.startsWith(ERROR_MESSAGE_PREFIX);
-                                     }
-                                   });
   }
 
   @Override
@@ -133,28 +113,45 @@ public class DeployJob extends WorkspaceJob {
         return saveStatus;
       }
 
-      IStatus stagingStatus = stageProject(credentialFile, stagingDirectory, progress.newChild(30));
-      if (stagingStatus != Status.OK_STATUS) {
-        return stagingStatus;
+      synchronized (this) {
+        if (canceled) {
+          return Status.CANCEL_STATUS;
+        }
+        deployCloudSdkFacade = CloudSdkProcessFacade.forDeploy(credentialFile, stderrLineListener);
+        stagingCloudSdkFacade = CloudSdkProcessFacade.forStandardStaging(null /*javaHome*/,
+            stagingStdoutLineListener, stderrLineListener);
       }
 
-      IStatus deployStatus = deployProject(credentialFile, stagingDirectory, progress.newChild(70));
+      IStatus stagingStatus = stageProject(stagingDirectory, progress.newChild(30));
+      if (stagingStatus != Status.OK_STATUS) {
+        return stagingStatus;
+      } else if (monitor.isCanceled()) {
+        return Status.CANCEL_STATUS;
+      }
+
+      IStatus deployStatus = deployProject(stagingDirectory, progress.newChild(70));
       if (deployStatus != Status.OK_STATUS) {
         return deployStatus;
+      } else if (monitor.isCanceled()) {
+        return Status.CANCEL_STATUS;
       }
 
       return openAppInBrowser();
     } finally {
-      // todo should this be progress.done() instead?
-      monitor.done();
+      progress.done();
     }
   }
 
   @Override
   protected void canceling() {
-    cloudSdkProcessStatus = Status.CANCEL_STATUS;
-    if (process != null) {
-      process.destroy();
+    synchronized (this) {
+      canceled = true;
+      if (deployCloudSdkFacade != null) {
+        deployCloudSdkFacade.cancel();
+      }
+      if (stagingCloudSdkFacade != null) {
+        stagingCloudSdkFacade.cancel();
+      }
     }
     super.canceling();
   }
@@ -168,19 +165,16 @@ public class DeployJob extends WorkspaceJob {
     }
   }
 
-  private IStatus stageProject(Path credentialFile,
-      IPath stagingDirectory, IProgressMonitor monitor) {
+  private IStatus stageProject(IPath stagingDirectory, IProgressMonitor monitor) {
     SubMonitor progress = SubMonitor.convert(monitor, 100);
-    RecordProcessError stagingExitListener = new RecordProcessError();
-    CloudSdk cloudSdk = getCloudSdk(credentialFile, stagingStdoutLineListener, stagingExitListener);
 
     try {
       getJobManager().beginRule(project, progress.newChild(1));
       IPath safeWorkDirectory = workDirectory.append(SAFE_STAGING_WORK_DIRECTORY_NAME);
-      IStatus status = stager.stage(
-          project, stagingDirectory, safeWorkDirectory, cloudSdk, progress.newChild(99));
-      if (stagingExitListener.getExitStatus() != Status.OK_STATUS) {
-        return stagingExitListener.getExitStatus();
+      IStatus status = stager.stage(project, stagingDirectory, safeWorkDirectory,
+          stagingCloudSdkFacade.getCloudSdk(), progress.newChild(99));
+      if (stagingCloudSdkFacade.getExitStatus() != Status.OK_STATUS) {
+        return stagingCloudSdkFacade.getExitStatus();
       }
       return status;
     } catch (IllegalArgumentException ex) {
@@ -190,40 +184,20 @@ public class DeployJob extends WorkspaceJob {
     }
   }
 
-  private IStatus deployProject(Path credentialFile, IPath stagingDirectory,
-      IProgressMonitor monitor) {
-    RecordProcessError deployExitListener = new RecordProcessError();
-    CloudSdk cloudSdk = getCloudSdk(credentialFile, deployStdoutLineListener, deployExitListener);
-
+  private IStatus deployProject(IPath stagingDirectory, IProgressMonitor monitor) {
     IPath optionalConfigurationFilesDirectory = null;
     if (includeOptionalConfigurationFiles) {
       optionalConfigurationFilesDirectory = stager.getOptionalConfigurationFilesDirectory();
     }
 
-    new AppEngineProjectDeployer().deploy(stagingDirectory, cloudSdk, deployConfiguration,
-        optionalConfigurationFilesDirectory, monitor);
-    return deployExitListener.getExitStatus();
-  }
-
-  private CloudSdk getCloudSdk(Path credentialFile,
-      ProcessOutputLineListener stdoutLineListener, ProcessExitListener processExitListener) {
-    CloudSdk cloudSdk = new CloudSdk.Builder()
-        .addStdOutLineListener(stdoutLineListener)
-        .addStdErrLineListener(stderrLineListener)
-        .addStdErrLineListener(errorCollectingLineListener)
-        .appCommandCredentialFile(credentialFile.toFile())
-        .startListener(new StoreProcessObjectListener())
-        .exitListener(processExitListener)
-        .appCommandMetricsEnvironment(CloudToolsInfo.METRICS_NAME)
-        .appCommandMetricsEnvironmentVersion(CloudToolsInfo.getToolsVersion())
-        .appCommandOutputFormat("json")
-        .build();
-    return cloudSdk;
+    new AppEngineProjectDeployer().deploy(stagingDirectory, deployCloudSdkFacade.getCloudSdk(),
+        deployConfiguration, optionalConfigurationFilesDirectory, monitor);
+    return deployCloudSdkFacade.getExitStatus();
   }
 
   private IStatus openAppInBrowser() {
     try {
-      String rawDeployOutput = deployStdoutLineListener.toString();
+      String rawDeployOutput = deployCloudSdkFacade.getStdOutAsString();
       AppEngineDeployOutput structuredOutput = AppEngineDeployOutput.parse(rawDeployOutput);
 
       boolean promoted = deployConfiguration.getPromote();
@@ -234,22 +208,6 @@ public class DeployJob extends WorkspaceJob {
       return Status.OK_STATUS;
     } catch (IndexOutOfBoundsException | JsonParseException ex)  {
       return StatusUtil.error(this, Messages.getString("browser.launch.failed"), ex);
-    }
-  }
-
-  /**
-   * @return the error message obtained from {@code errorCollectingLineListener()} or
-   * {@code defaultMessage}.
-   */
-  private String getErrorMessageOrDefault(String defaultMessage) {
-    // TODO: Check the assumption that if there are error messages during staging collected via
-    // the errorCollectingLineListener, the staging process will have a non-zero exitcode,
-    // making it ok to use the same errorCollectingLineListener for the deploy process
-    List<String> messages = errorCollectingLineListener.getCollectedMessages();
-    if (!messages.isEmpty()) {
-      return Joiner.on('\n').join(messages);
-    } else {
-      return defaultMessage;
     }
   }
 
@@ -275,39 +233,6 @@ public class DeployJob extends WorkspaceJob {
       return "https://" + version + "-dot-" + projectId + domain;
     } else {
       return "https://" + version + "-dot-" + service +  "-dot-"+  projectId + domain;
-    }
-  }
-
-  private final class StoreProcessObjectListener implements ProcessStartListener {
-    @Override
-    public void onStart(Process proces) {
-      process = proces;
-    }
-  }
-
-  private class RecordProcessError implements ProcessExitListener {
-    // Defaults to OK in case CloudSdk was not used at all (e.g., in flex staging)
-    private IStatus status = Status.OK_STATUS;
-
-    @Override
-    public void onExit(int exitCode) {
-      if (cloudSdkProcessStatus == Status.CANCEL_STATUS) {
-        status = Status.CANCEL_STATUS;
-      } else if (exitCode != 0) {
-        // temporary way of error handling, after #439 is fixed, it'll be cleaner
-        String errorMessage =
-            getErrorMessageOrDefault(Messages.getString("cloudsdk.process.failed", exitCode));
-        status = StatusUtil.error(this, errorMessage);
-      } else {
-        status = Status.OK_STATUS;
-      }
-    }
-
-    /**
-     * @return the status on exit of the process or null if the process has not exited.
-     */
-    public IStatus getExitStatus() {
-      return status;
     }
   }
 
