a9039f340fbd048eace35ab2e07dec76e648e2ab
Elliotte Harold <elharo@google.com>
2017-08-10 06:30:59 -0400

    update test

--- plugins/com.google.cloud.tools.eclipse.dataflow.ui.test/src/com/google/cloud/tools/eclipse/dataflow/ui/MessagesTest.java
+++ plugins/com.google.cloud.tools.eclipse.dataflow.ui.test/src/com/google/cloud/tools/eclipse/dataflow/ui/MessagesTest.java
@@ -41,7 +41,7 @@ public class MessagesTest {
   
   @Test
   public void testLocationToolTip() {
-    Assert.assertEquals("The location where the project will be created. Must be an existing local directory.",
+    Assert.assertEquals("The directory where the project will be created. Must be an existing local directory.",
         Messages.getString("LOCATION_TOOLTIP"));
   }
 
