d39917a3dfd66e5b7f4b3e0170e738026608784e
Elliotte Rusty Harold <elharo@google.com>
2017-08-17 14:30:45 -0400

    fix test

--- plugins/com.google.cloud.tools.eclipse.util.test/src/com/google/cloud/tools/eclipse/util/MessagesTest.java
+++ plugins/com.google.cloud.tools.eclipse.util.test/src/com/google/cloud/tools/eclipse/util/MessagesTest.java
@@ -30,7 +30,7 @@ public class MessagesTest {
   @Test
   public void testPackageContainsWhiteSpace() {
     Assert.assertEquals(
-        "com google foo contains white space.", 
+        "com google foo contains whitespace.", 
         Messages.getString("package.contains.whitespace", "com google foo"));
   }
   
