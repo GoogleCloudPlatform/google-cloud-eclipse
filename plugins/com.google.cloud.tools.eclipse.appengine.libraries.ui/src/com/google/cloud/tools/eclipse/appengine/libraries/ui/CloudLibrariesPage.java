4799d2e4885af8cf875501c9551a2cce891f17a4
Elliotte Harold <elharo@google.com>
2017-09-06 06:33:38 -0400

    allow library dependencies from other groups

--- plugins/com.google.cloud.tools.eclipse.appengine.libraries.ui/src/com/google/cloud/tools/eclipse/appengine/libraries/ui/CloudLibrariesPage.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.libraries.ui/src/com/google/cloud/tools/eclipse/appengine/libraries/ui/CloudLibrariesPage.java
@@ -108,6 +108,7 @@ public abstract class CloudLibrariesPage extends WizardPage implements IClasspat
         return new IClasspathEntry[0];
       } else {
         for (Library library : libraries) {
+          // todo how is library null here?
           if (!library.isResolved()) {
             library.resolveDependencies();
           }
