dddba6ff7d558e22863f9cad7e5ebaf232ec5429
Brian de Alwis <bsd@mt.ca>
2017-09-13 10:44:53 -0400

    Separate FuturisticJob into PluggableJob and add tests

--- plugins/com.google.cloud.tools.eclipse.util/src/com/google/cloud/tools/eclipse/util/jobs/FuturisticJob.java
+++ plugins/com.google.cloud.tools.eclipse.util/src/com/google/cloud/tools/eclipse/util/jobs/FuturisticJob.java
@@ -16,12 +16,8 @@
 
 package com.google.cloud.tools.eclipse.util.jobs;
 
-import com.google.common.base.Preconditions;
-import com.google.common.base.Predicate;
-import com.google.common.base.Predicates;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
-import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
@@ -31,32 +27,20 @@ import org.eclipse.core.runtime.jobs.Job;
 
 /**
  * A {@link Job} for executing some background computation using the Jobs framework that produces a
- * result. The computation result is made available via a {@link #getFuture() future}. The job
- * checks for whether the {@link #cancel() job} or {@link Future#cancel(boolean) future} have been
- * cancelled. The Job also checks if it has become {@link #isStale() stale}, meaning that the
- * original configuration parameters for the computation have become out of date. The job checks for
- * cancellation or staleness before setting the future result.
+ * result. The <em>computation result</em> is made available via a {@link #getFuture() future}. The
+ * job checks for whether the {@link #cancel() job} or {@link Future#cancel(boolean) future} have
+ * been cancelled. The job also checks whether it has become {@link #isStale() stale}, meaning that
+ * the original configuration parameters for the computation have become out of date. The job checks
+ * for cancellation or staleness before setting the future result.
  * <p>
- * By default, exceptions are treated as an expected outcome and reported on the future, and the
- * {@link #getResult() job result} will be {@link IStatus#OK OK}. Subclasses can change this
- * exception handling by overriding {@link #handleException(SettableFuture, Exception)}. Note that a
- * {@link #getResult() job result} ofanything other than {@link IStatus#OK} may result in some kind
- * of UI shown to the user.
+ * By default, exceptions are treated as an expected outcome and reported as a <em>computation
+ * result</em> on the future, but the {@link #getResult() job result} will be {@link IStatus#OK OK}.
+ * Subclasses may change this exception handling by overriding
+ * {@link #handleException(SettableFuture, Exception)}. Note that a {@link #getResult() job result}
+ * of anything other than {@link IStatus#OK} may result in some kind of UI shown to the user.
  */
-public class FuturisticJob<T> extends Job {
+public abstract class FuturisticJob<T> extends Job {
   private final SettableFuture<T> future = SettableFuture.create();
-
-  /**
-   * A pluggable check for whether this instance is stale; extenders can override
-   * {@link #isStale()}.
-   */
-  private final Predicate<? super FuturisticJob<T>> stalenessCheck;
-
-  /**
-   * Pluggable runnable for computing the result of the job; extenders can instead override
-   * {@link #compute()}.
-   */
-  private final Callable<? extends T> computeTask;
 
   /**
    * Create a default instance; intended to be used by subclasses that must override
@@ -66,48 +50,17 @@ public class FuturisticJob<T> extends Job {
    */
   protected FuturisticJob(String name) {
     super(name);
-    // subclasses must override compute() and optionally isStale()
-    computeTask = null;
-    stalenessCheck = Predicates.alwaysFalse();
   }
 
   /**
-   * Create a new instance with pluggable runnable. This instance is never stale.
-   * 
-   * @param name the job name, surfaced in UI; never {@code null}
-   * @param computeTask the actual runnable to compute a result; never {@code null}
-   */
-  public FuturisticJob(String name, Callable<? extends T> computeTask) {
-    this(name, computeTask, Predicates.alwaysFalse());
-  }
-
-  /**
-   * Create a new instance with pluggable runnable and stale check.
-   * 
-   * @param name the job name, surfaced in UI; never {@code null}
-   * @param computeTask the actual runnable to compute a result; never {@code null}
-   * @param stalenessCheck a predicate to check if this job is stale; never {@code null}
-   */
-  public FuturisticJob(String name, Callable<? extends T> computeTask,
-      Predicate<? super FuturisticJob<T>> stalenessCheck) {
-    super(name);
-    Preconditions.checkNotNull(computeTask);
-    Preconditions.checkNotNull(stalenessCheck);
-    this.computeTask = computeTask;
-    this.stalenessCheck = stalenessCheck;
-  }
-
-  /**
-   * Compute and return the result. Overriders can call {@link #checkCancelled(IProgressMonitor)} at
+   * Compute and return the result. Subclasses can call {@link #checkCancelled(IProgressMonitor)} at
    * checkpoints to see if the job has been cancelled.
    * <p>
    * This default implementation executes the <code>computeTask</code>.
    */
-  protected T compute(IProgressMonitor monitor) throws Exception {
-    Preconditions.checkNotNull(computeTask, "subclasses must override or provide a computeTask");
-    return computeTask.call();
-  }
+  protected abstract T compute(IProgressMonitor monitor) throws Exception;
 
+  /** Returns the computation result via the future. */
   public ListenableFuture<T> getFuture() {
     return future;
   }
@@ -131,7 +84,7 @@ public class FuturisticJob<T> extends Job {
       }
       return Status.OK_STATUS;
     } catch (OperationCanceledException ex) {
-      future.cancel(false);
+      future.cancel(true);
       return Status.CANCEL_STATUS;
     } catch (Exception ex) { // throwable?
       checkCancelled(monitor);
@@ -165,7 +118,7 @@ public class FuturisticJob<T> extends Job {
 
   @Override
   protected void canceling() {
-    future.cancel(false);
+    future.cancel(true);
     super.canceling();
   }
 
@@ -178,17 +131,16 @@ public class FuturisticJob<T> extends Job {
    */
   protected void checkCancelled(IProgressMonitor monitor) throws OperationCanceledException {
     if (monitor.isCanceled() || future.isCancelled() || isStale()) {
-      future.cancel(false);
+      future.cancel(true);
       throw new OperationCanceledException();
     }
   }
 
   /**
    * Return true if this job is stale: the provided starting parameters have been changed since the
-   * job was started.
+   * job was started. This method will be called from this job's thread.
    */
   protected boolean isStale() {
-    return stalenessCheck.apply(this);
+    return false;
   }
-
 }
