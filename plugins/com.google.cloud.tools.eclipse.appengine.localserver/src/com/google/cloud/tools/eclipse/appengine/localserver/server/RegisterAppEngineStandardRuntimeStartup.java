2d407f382a3f8486a56534580398dfc5ea75566d
Brian de Alwis <bsd@mt.ca>
2017-10-04 14:38:22 -0400

    Review nits

--- plugins/com.google.cloud.tools.eclipse.appengine.localserver/src/com/google/cloud/tools/eclipse/appengine/localserver/server/RegisterAppEngineStandardRuntimeStartup.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.localserver/src/com/google/cloud/tools/eclipse/appengine/localserver/server/RegisterAppEngineStandardRuntimeStartup.java
@@ -1,19 +1,33 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.google.cloud.tools.eclipse.appengine.localserver.server;
 
+import com.google.cloud.tools.eclipse.appengine.facets.AppEngineStandardFacet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.wst.server.core.IRuntime;
-import org.eclipse.wst.server.core.IRuntimeType;
-import org.eclipse.wst.server.core.IRuntimeWorkingCopy;
 import org.eclipse.wst.server.core.ServerCore;
 import org.eclipse.wst.server.core.internal.IStartup;
 
 /**
  * Registers the default instance of our App Engine Standard runtime if not found. Uses the internal
- * WTP {@link IStartup} to minimize activation to use of WTP-related functionality.
+ * WTP {@link IStartup} to avoid unnecessary activation until use of WTP-related functionality.
  */
 public class RegisterAppEngineStandardRuntimeStartup implements IStartup {
   private static final Logger logger =
@@ -29,19 +43,10 @@ public class RegisterAppEngineStandardRuntimeStartup implements IStartup {
   public void startup() {
     IRuntime runtime = ServerCore.findRuntime(RUNTIME_ID);
     if (runtime != null) {
-      logger.info("Runtime found: " + RUNTIME_ID);
-      return;
-    }
-    IRuntimeType runtimeType = ServerCore.findRuntimeType(AppToolsRuntime.RUNTIME_TYPE_ID);
-    if (runtimeType == null) {
-      logger.log(Level.SEVERE, "Cannot find server runtime type: ",
-          AppToolsRuntime.RUNTIME_TYPE_ID);
       return;
     }
     try {
-      IRuntimeWorkingCopy inprogress =
-          runtimeType.createRuntime(RUNTIME_ID, new NullProgressMonitor());
-      inprogress.save(true, new NullProgressMonitor());
+      AppEngineStandardFacet.createAppEngineServerRuntime(new NullProgressMonitor());
     } catch (CoreException ex) {
       logger.log(Level.SEVERE, "Unable to create default runtime instance", ex);
     }
