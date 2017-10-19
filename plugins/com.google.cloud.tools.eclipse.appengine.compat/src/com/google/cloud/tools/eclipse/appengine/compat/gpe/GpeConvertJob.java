a74910c5a0b9395a23d10b6acd5ba2a2eec7384c
Brian de Alwis <bsd@mt.ca>
2017-10-19 12:47:59 -0400

    Share code with AppEngineStandardProjectConvertJob

--- plugins/com.google.cloud.tools.eclipse.appengine.compat/src/com/google/cloud/tools/eclipse/appengine/compat/gpe/GpeConvertJob.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.compat/src/com/google/cloud/tools/eclipse/appengine/compat/gpe/GpeConvertJob.java
@@ -16,44 +16,33 @@
 
 package com.google.cloud.tools.eclipse.appengine.compat.gpe;
 
-import com.google.cloud.tools.eclipse.appengine.facets.AppEngineStandardFacet;
-import com.google.cloud.tools.eclipse.util.Messages;
+import com.google.cloud.tools.eclipse.appengine.facets.convert.AppEngineStandardProjectConvertJob;
 import com.google.cloud.tools.eclipse.util.status.StatusUtil;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.core.runtime.SubMonitor;
-import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.wst.common.project.facet.core.IFacetedProject;
 
-public class GpeConvertJob extends Job {
-  private final IFacetedProject facetedProject;
+public class GpeConvertJob extends AppEngineStandardProjectConvertJob {
 
   public GpeConvertJob(IFacetedProject facetedProject) {
-    super("Google Plugin for Eclipse Project Conversion Job");
-    this.facetedProject = facetedProject;
+    super("Google Plugin for Eclipse Project Conversion Job", facetedProject);
   }
 
   @Override
-  protected IStatus run(IProgressMonitor monitor) {
-    SubMonitor subMonitor = SubMonitor.convert(monitor, 100);
+  protected void convert(MultiStatus status, IProgressMonitor monitor) {
+    SubMonitor progress = SubMonitor.convert(monitor, 100);
 
+    // Updating project before installing App Engine facet to avoid
+    // https://github.com/GoogleCloudPlatform/google-cloud-eclipse/issues/1155.
     try {
-      // Updating project before installing App Engine facet to avoid
-      // https://github.com/GoogleCloudPlatform/google-cloud-eclipse/issues/1155.
-      GpeMigrator.removeObsoleteGpeRemnants(facetedProject, subMonitor.newChild(20));
-
-      if (!monitor.isCanceled()) {
-        AppEngineStandardFacet.installAppEngineFacet(facetedProject,
-            true /* install Java and Web facets too (safe even if already installed) */,
-            subMonitor.newChild(80));
-      }
-
-      return monitor.isCanceled() ? Status.CANCEL_STATUS : Status.OK_STATUS;
+      GpeMigrator.removeObsoleteGpeRemnants(facetedProject, progress.newChild(20));
+      super.convert(status, progress.newChild(20));
     } catch (CoreException ex) {
-      String project = facetedProject.getProject().getName();
-      return StatusUtil.error(this, Messages.getString("project.conversion.error", project), ex);
+      status.add(StatusUtil.error(this, "Unable to remove GPE remains", ex));
     }
   }
+
+
 }
