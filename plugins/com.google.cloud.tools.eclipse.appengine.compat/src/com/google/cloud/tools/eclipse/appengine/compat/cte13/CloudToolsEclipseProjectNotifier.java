fe9dbe59548d7803a5faf8a55a7dc0972d06a65b
Brian de Alwis <bsd@mt.ca>
2017-11-16 16:34:44 -0500

    Use StatusUtil.filter() to rewrite OK MultiStatus into Status.OK_STATUS

--- plugins/com.google.cloud.tools.eclipse.appengine.compat/src/com/google/cloud/tools/eclipse/appengine/compat/cte13/CloudToolsEclipseProjectNotifier.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.compat/src/com/google/cloud/tools/eclipse/appengine/compat/cte13/CloudToolsEclipseProjectNotifier.java
@@ -145,6 +145,7 @@ public class CloudToolsEclipseProjectNotifier implements IStartup {
       IStatus result = CloudToolsEclipseProjectUpdater.updateProject(project, progress.newChild(1));
       status.merge(result);
     }
-    return status;
+    // rewrite if OK as otherwise Progress View shows the updating projects message
+    return StatusUtil.filter(status);
   }
 }
