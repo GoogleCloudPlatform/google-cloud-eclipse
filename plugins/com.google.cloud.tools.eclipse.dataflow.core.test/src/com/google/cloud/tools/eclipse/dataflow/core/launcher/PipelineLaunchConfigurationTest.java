4edf1f993987f208ae4e68a4936bef20bf954baf
Elliotte Harold <elharo@google.com>
2017-12-11 07:08:14 -0500

    suppress some  warnings

--- plugins/com.google.cloud.tools.eclipse.dataflow.core.test/src/com/google/cloud/tools/eclipse/dataflow/core/launcher/PipelineLaunchConfigurationTest.java
+++ plugins/com.google.cloud.tools.eclipse.dataflow.core.test/src/com/google/cloud/tools/eclipse/dataflow/core/launcher/PipelineLaunchConfigurationTest.java
@@ -56,6 +56,7 @@ public class PipelineLaunchConfigurationTest {
         pipelineLaunchConfig.getRunner());
   }
 
+  @SuppressWarnings("unchecked")
   @Test
   public void testFromLaunchConfigurationCopiesArgumentsFromLaunchConfiguration()
       throws CoreException {
