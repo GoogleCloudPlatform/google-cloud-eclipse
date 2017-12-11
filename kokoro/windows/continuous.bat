f282fd3a8e1a800adec3e5455fde8d1a39068b3c
Chanseok Oh <chanseok@google.com>
2017-12-11 18:53:16 -0500

    Let Kokoro pull Eclipse from GCS mirror

--- kokoro/windows/continuous.bat
+++ kokoro/windows/continuous.bat
@@ -1,11 +1,13 @@
 @echo on
-cd github/google-cloud-eclipse
+cd github\google-cloud-eclipse
 
 set CLOUDSDK_CORE_DISABLE_USAGE_REPORTING=true
 call gcloud.cmd components update --quiet
 call gcloud.cmd components install app-engine-java --quiet
 
-mvn -B -N io.takari:maven:wrapper -Dmaven=3.5.0
-mvnw.cmd -B --fail-at-end -Ptravis -Declipse.target=oxygen verify
+mvn -B --settings kokoro\windows\m2-settings.xml ^
+    -N io.takari:maven:wrapper -Dmaven=3.5.0
+mvnw.cmd -B --settings kokoro\windows\m2-settings.xml ^
+         --fail-at-end -Ptravis -Declipse.target=oxygen verify
 
 exit /b %ERRORLEVEL%
