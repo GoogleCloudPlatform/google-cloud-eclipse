7ddd94253c98e00e1fd91af3794c2910a5c55bcc
Chanseok Oh <chanseok@google.com>
2017-12-11 23:27:50 -0500

    Turn on echo again after gcloud.cmd

--- kokoro/windows/continuous.bat
+++ kokoro/windows/continuous.bat
@@ -3,7 +3,9 @@ cd github\google-cloud-eclipse
 
 set CLOUDSDK_CORE_DISABLE_USAGE_REPORTING=true
 call gcloud.cmd components update --quiet
+@echo on
 call gcloud.cmd components install app-engine-java --quiet
+@echo on
 
 mvn -B --settings kokoro\windows\m2-settings.xml ^
     -N io.takari:maven:wrapper -Dmaven=3.5.0
