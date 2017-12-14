245b7e70201ce3bd20de1fdafb716230d7c8a7fc
Chanseok Oh <chanseok@google.com>
2017-12-13 23:00:18 -0500

    test

--- kokoro/windows/continuous.bat
+++ kokoro/windows/continuous.bat
@@ -1,6 +1,8 @@
 @echo on
 cd github\google-cloud-eclipse
 
+rem Pre-download all dependency JARs that test projects of the integration test
+rem requires to avoid https://github.com/GoogleCloudPlatform/google-cloud-eclipse/issues/2284
 pushd plugins\com.google.cloud.tools.eclipse.integration.appengine\test-projects
 for %%i in (*.zip) do jar xf %%i
 for /r . %%i in (pom.xml) do @if exist %%i mvn -B -f "%%i" package
