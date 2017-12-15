2dc98b720e14c41f1c8959247fa1a2a26f581340
Chanseok Oh <chanseok@google.com>
2017-12-14 19:34:06 -0500

    test

--- kokoro/windows/continuous.bat
+++ kokoro/windows/continuous.bat
@@ -4,8 +4,7 @@ cd github
 call gsutil -q cp "gs://ct4e-m2-repositories/m2-oxygen.tar" .
 echo on
 tar xf m2-oxygen.tar
-set M2_OXYGEN=%CD%\m2-oxygen\repository
-echo %M2_OXYGEN%
+set M2_OXYGEN="%CD%\m2-oxygen\repository"
 dir /b m2-oxygen
 
 cd google-cloud-eclipse
@@ -13,11 +12,15 @@ cd google-cloud-eclipse
 rem Pre-download all dependency JARs that test projects from the integration
 rem test require to avoid the concurrent download issue:
 rem https://github.com/GoogleCloudPlatform/google-cloud-eclipse/issues/2284
+rem (Currently, this workaround is mostly not effective as we dump the M2 local
+rem repo above that already caches the JARs for these projects, but we leave it
+rem here in case we decided not to dump the local repo.)
 pushd plugins\com.google.cloud.tools.eclipse.integration.appengine\test-projects
 mkdir tmp-unzip-area
 cd tmp-unzip-area
 for %%i in (..\*.zip) do jar xf %%i
-for /f %%i in ('dir /b /s pom.xml') do mvn -B -Dmaven.repo.local=%M2_OXYGEN% -f "%%i" package
+for /f %%i in ('dir /b /s pom.xml') do (
+    mvn -B -q -Dmaven.repo.local="%M2_OXYGEN%" -f "%%i" package)
 cd ..
 rmdir /s /q tmp-unzip-area
 popd
@@ -29,10 +32,10 @@ call gcloud.cmd components install app-engine-java --quiet
 @echo on
 
 mvn -B --settings kokoro\windows\m2-settings.xml ^
-    -Dmaven.repo.local=%M2_OXYGEN% ^
+    -Dmaven.repo.local="%M2_OXYGEN%" ^
     -N io.takari:maven:wrapper -Dmaven=3.5.0
 mvnw.cmd -B --settings kokoro\windows\m2-settings.xml ^
-         -Dmaven.repo.local=%M2_OXYGEN% ^
+         -Dmaven.repo.local="%M2_OXYGEN%" ^
          --fail-at-end -Ptravis -Declipse.target=oxygen verify
 
 exit /b %ERRORLEVEL%
