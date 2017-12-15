908de310f9503a10652a49afbc2be6f21a232e8a
Chanseok Oh <chanseok@google.com>
2017-12-14 19:23:20 -0500

    test

--- kokoro/windows/continuous.bat
+++ kokoro/windows/continuous.bat
@@ -2,7 +2,10 @@
 cd github
 
 call gsutil -q cp "gs://ct4e-m2-repositories/m2-oxygen.tar" .
+echo on
 tar xf m2-oxygen.tar
+set M2_OXYGEN=%CD%\m2-oxygen\repository
+echo %M2_OXYGEN%
 dir /b m2-oxygen
 
 cd google-cloud-eclipse
@@ -14,7 +17,7 @@ pushd plugins\com.google.cloud.tools.eclipse.integration.appengine\test-projects
 mkdir tmp-unzip-area
 cd tmp-unzip-area
 for %%i in (..\*.zip) do jar xf %%i
-for /f %%i in ('dir /b /s pom.xml') do mvn -B -Dmaven.repo.local=..\m2-oxygen\repository -f "%%i" package
+for /f %%i in ('dir /b /s pom.xml') do mvn -B -Dmaven.repo.local=%M2_OXYGEN% -f "%%i" package
 cd ..
 rmdir /s /q tmp-unzip-area
 popd
@@ -26,10 +29,10 @@ call gcloud.cmd components install app-engine-java --quiet
 @echo on
 
 mvn -B --settings kokoro\windows\m2-settings.xml ^
-    -Dmaven.repo.local=..\m2-oxygen\repository ^
+    -Dmaven.repo.local=%M2_OXYGEN% ^
     -N io.takari:maven:wrapper -Dmaven=3.5.0
 mvnw.cmd -B --settings kokoro\windows\m2-settings.xml ^
-         -Dmaven.repo.local=..\m2-oxygen\repository ^
+         -Dmaven.repo.local=%M2_OXYGEN% ^
          --fail-at-end -Ptravis -Declipse.target=oxygen verify
 
 exit /b %ERRORLEVEL%
