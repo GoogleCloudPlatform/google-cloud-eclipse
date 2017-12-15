1fae50e0b7399e0328d72d1f0b99272bdc8be02f
Chanseok Oh <chanseok@google.com>
2017-12-15 01:00:36 -0500

    test

--- kokoro/windows/continuous.bat
+++ kokoro/windows/continuous.bat
@@ -1,13 +1,10 @@
 @echo on
 
+rem To speed up build, download and unpack the preloaded M2 repo.
 pushd %USERPROFILE%
-rmdir /s /q .m2
 call gsutil -q cp "gs://ct4e-m2-repositories/m2-oxygen.tar" .
 echo on
-tar xf m2-oxygen.tar
-del m2-oxygen.tar
-rename m2-oxygen .m2
-dir
+tar xf m2-oxygen.tar && del m2-oxygen.tar
 popd
 
 cd github\google-cloud-eclipse
@@ -15,14 +12,14 @@ cd github\google-cloud-eclipse
 rem Pre-download all dependency JARs that test projects from the integration
 rem test require to avoid the concurrent download issue:
 rem https://github.com/GoogleCloudPlatform/google-cloud-eclipse/issues/2284
-rem (Currently, this workaround is mostly not effective as we dump the M2 local
-rem repo above that already caches the JARs for these projects, but we leave it
-rem here in case we decided not to dump the local repo.)
+rem (Currently, this workaround is mostly unnecessary, as we download and unpack
+rem the M2 repo preloaded with all the JARs. However, we leave it here, since
+rem test projects may change in the future.)
 pushd plugins\com.google.cloud.tools.eclipse.integration.appengine\test-projects
 mkdir tmp-unzip-area
 cd tmp-unzip-area
 for %%i in (..\*.zip) do jar xf %%i
-for /f %%i in ('dir /b /s pom.xml') do mvn -B -f "%%i" package
+for /f %%i in ('dir /b /s pom.xml') do mvn -B -q -f "%%i" package
 cd ..
 rmdir /s /q tmp-unzip-area
 popd
@@ -38,4 +35,4 @@ mvn -B --settings kokoro\windows\m2-settings.xml ^
 mvnw.cmd -B --settings kokoro\windows\m2-settings.xml ^
          --fail-at-end -Ptravis -Declipse.target=oxygen verify
 
-exit /b %MAIN_EXIT%
+exit /b %ERRORLEVEL%
