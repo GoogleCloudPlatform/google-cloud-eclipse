2f441ec3a4e4892a113cc258a6410ca97f82e83d
Brian de Alwis <bsd@mt.ca>
2018-01-09 12:06:14 -0500

    Use `mvn -V` to show build env info

--- kokoro/windows/continuous.bat
+++ kokoro/windows/continuous.bat
@@ -27,9 +27,9 @@ call gcloud.cmd components update --quiet
 call gcloud.cmd components install app-engine-java --quiet
 @echo on
 
-mvn -B --settings kokoro\windows\m2-settings.xml ^
+mvn -V -B --settings kokoro\windows\m2-settings.xml ^
     -N io.takari:maven:wrapper -Dmaven=3.5.0
-mvnw.cmd -B --settings kokoro\windows\m2-settings.xml ^
+mvnw.cmd -V -B --settings kokoro\windows\m2-settings.xml ^
          --fail-at-end -Ptravis -Declipse.target=oxygen verify
 
 exit /b %ERRORLEVEL%
