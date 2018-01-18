037b062f925a7406c53baa752c4d86990749ff49
Chanseok Oh <chanseok@google.com>
2018-01-18 13:24:02 -0500

    Restore original visiblity

--- plugins/com.google.cloud.tools.eclipse.sdk/src/com/google/cloud/tools/eclipse/sdk/internal/PreferenceConstants.java
+++ plugins/com.google.cloud.tools.eclipse.sdk/src/com/google/cloud/tools/eclipse/sdk/internal/PreferenceConstants.java
@@ -53,7 +53,7 @@ public final class PreferenceConstants {
     return new ScopedPreferenceStore(InstanceScope.INSTANCE, BUNDLEID);
   }
 
-  public static IEclipsePreferences getPreferenceNode() {
+  static IEclipsePreferences getPreferenceNode() {
     return InstanceScope.INSTANCE.getNode(BUNDLEID);
   }
 }
