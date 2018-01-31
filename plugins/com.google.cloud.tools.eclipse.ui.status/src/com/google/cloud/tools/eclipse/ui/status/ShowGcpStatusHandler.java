1fe9c7524d2f463cacc43086d4faacb0c7e65032
Brian de Alwis <bsd@mt.ca>
2018-01-31 16:49:42 -0500

    Simplify ImageDescriptor generation

--- plugins/com.google.cloud.tools.eclipse.ui.status/src/com/google/cloud/tools/eclipse/ui/status/ShowGcpStatusHandler.java
+++ plugins/com.google.cloud.tools.eclipse.ui.status/src/com/google/cloud/tools/eclipse/ui/status/ShowGcpStatusHandler.java
@@ -3,13 +3,10 @@ package com.google.cloud.tools.eclipse.ui.status;
 import com.google.cloud.tools.eclipse.ui.util.ServiceUtils;
 import com.google.cloud.tools.eclipse.ui.util.WorkbenchUtil;
 import com.google.common.base.Joiner;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -18,6 +15,7 @@ import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.commands.IElementUpdater;
 import org.eclipse.ui.menus.UIElement;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 public class ShowGcpStatusHandler extends AbstractHandler implements IElementUpdater {
   private static final Logger logger = Logger.getLogger(ShowGcpStatusHandler.class.getName());
@@ -26,26 +24,17 @@ public class ShowGcpStatusHandler extends AbstractHandler implements IElementUpd
 
   private static final String BUNDLE_ID = "com.google.cloud.tools.eclipse.ui.status";
 
-  private static ImageDescriptor IMG_OK;
-  private static ImageDescriptor IMG_LOW;
-  private static ImageDescriptor IMG_MEDIUM;
-  private static ImageDescriptor IMG_HIGH;
-  private static ImageDescriptor IMG_ERROR;
-
-  static {
-    try {
-      String prefix = "platform:/plugin/" + BUNDLE_ID + "/icons/";
-      IMG_OK = ImageDescriptor.createFromURL(new URL(prefix + "gcp-ok.png"));
-      IMG_LOW = ImageDescriptor.createFromURL(new URL(prefix + "gcp-low.png"));
-      IMG_MEDIUM = ImageDescriptor.createFromURL(new URL(prefix + "gcp-medium.png"));
-      IMG_HIGH = ImageDescriptor.createFromURL(new URL(prefix + "gcp-high.png"));
-      IMG_ERROR =
-          ImageDescriptor.createFromURL(
-              new URL("platform:/plugin/org.eclipse.jface/icons/full/message_error.png"));
-    } catch (MalformedURLException ex) {
-      logger.log(Level.SEVERE, "Invalid image locations", ex);
-    }
-  }
+  private static final ImageDescriptor IMG_OK =
+      AbstractUIPlugin.imageDescriptorFromPlugin(BUNDLE_ID, "icons/gcp-ok.png");
+  private static final ImageDescriptor IMG_LOW =
+      AbstractUIPlugin.imageDescriptorFromPlugin(BUNDLE_ID, "icons/gcp-low.png");
+  private static final ImageDescriptor IMG_MEDIUM =
+      AbstractUIPlugin.imageDescriptorFromPlugin(BUNDLE_ID, "icons/gcp-medium.png");
+  private static final ImageDescriptor IMG_HIGH =
+      AbstractUIPlugin.imageDescriptorFromPlugin(BUNDLE_ID, "icons/gcp-high.png");
+  private static final ImageDescriptor IMG_ERROR =
+      AbstractUIPlugin.imageDescriptorFromPlugin(
+          "org.eclipse.jface", "icons/full/message_error.png");
 
   @Override
   public Object execute(ExecutionEvent event) throws ExecutionException {
