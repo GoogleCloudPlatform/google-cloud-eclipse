01a61246e79c3b0e4915f1f13a7138961c6edaf4
Chanseok Oh <chanseok@google.com>
2018-02-02 17:40:52 -0500

    wip

--- plugins/com.google.cloud.tools.eclipse.sdk/src/com/google/cloud/tools/eclipse/sdk/internal/CloudSdkInstallJob.java
+++ plugins/com.google.cloud.tools.eclipse.sdk/src/com/google/cloud/tools/eclipse/sdk/internal/CloudSdkInstallJob.java
@@ -16,9 +16,22 @@
 
 package com.google.cloud.tools.eclipse.sdk.internal;
 
+import com.google.cloud.tools.eclipse.sdk.MessageConsoleWriterListener;
 import com.google.cloud.tools.eclipse.sdk.Messages;
 import com.google.cloud.tools.eclipse.util.jobs.MutexRule;
+import com.google.cloud.tools.eclipse.util.status.StatusUtil;
+import com.google.cloud.tools.managedcloudsdk.ManagedCloudSdk;
+import com.google.cloud.tools.managedcloudsdk.ManagedSdkVerificationException;
+import com.google.cloud.tools.managedcloudsdk.ManagedSdkVersionMismatchException;
+import com.google.cloud.tools.managedcloudsdk.UnsupportedOsException;
+import com.google.cloud.tools.managedcloudsdk.command.CommandExecutionException;
+import com.google.cloud.tools.managedcloudsdk.command.CommandExitException;
+import com.google.cloud.tools.managedcloudsdk.components.SdkComponent;
+import com.google.cloud.tools.managedcloudsdk.components.SdkComponentInstaller;
+import com.google.cloud.tools.managedcloudsdk.install.SdkInstaller;
+import com.google.cloud.tools.managedcloudsdk.install.SdkInstallerException;
 import com.google.common.annotations.VisibleForTesting;
+import java.io.IOException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -50,6 +63,31 @@ public class CloudSdkInstallJob extends Job {
   protected IStatus run(IProgressMonitor monitor) {
     // TODO(chanseok): to be implemented: https://github.com/GoogleCloudPlatform/google-cloud-eclipse/issues/2753
     consoleStream.println("to be implemented");
-    return Status.OK_STATUS;
+
+    try {
+      ManagedCloudSdk managedSdk = ManagedCloudSdk.newManagedSdk();
+      if (!managedSdk.isInstalled()) {
+        SdkInstaller installer = managedSdk.newInstaller();
+        installer.install(new MessageConsoleWriterListener(consoleStream));
+      }
+
+      if (!managedSdk.hasComponent(SdkComponent.APP_ENGINE_JAVA)) {
+        SdkComponentInstaller componentInstaller = managedSdk.newComponentInstaller();
+        componentInstaller.installComponent(
+            SdkComponent.APP_ENGINE_JAVA, new MessageConsoleWriterListener(consoleStream));
+      }
+      return Status.OK_STATUS;
+
+    } catch (InterruptedException e) {
+      return Status.CANCEL_STATUS;
+    } catch (IOException | ManagedSdkVerificationException | SdkInstallerException |
+        CommandExecutionException | CommandExitException e) {
+      return StatusUtil.error(this, "Failed to install the Google Cloud SDK.", e);
+
+    } catch (UnsupportedOsException e) {
+      throw new RuntimeException("Cloud Tools for Eclipse supports Windows, Linux, and Mac only.");
+    } catch (ManagedSdkVersionMismatchException e) {
+      throw new RuntimeException("This is never thrown because we always use LATEST.");
+    }
   }
 }
