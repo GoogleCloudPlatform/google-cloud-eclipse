3ba8d7f356259e8d097a8369f667df2842d3fe65
Chanseok Oh <chanseok@google.com>
2018-02-06 13:45:23 -0500

    Update validation status correctly

--- plugins/com.google.cloud.tools.eclipse.sdk.ui/src/com/google/cloud/tools/eclipse/sdk/ui/preferences/CloudSdkPreferenceArea.java
+++ plugins/com.google.cloud.tools.eclipse.sdk.ui/src/com/google/cloud/tools/eclipse/sdk/ui/preferences/CloudSdkPreferenceArea.java
@@ -44,6 +44,7 @@ import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -94,6 +95,17 @@ public class CloudSdkPreferenceArea extends PreferenceArea {
         @Override
         public void widgetSelected(SelectionEvent e) {
           updateControlEnablement();
+        }
+      });
+      useLocalSdk.addSelectionListener(new SelectionAdapter() {
+        @Override
+        public void widgetSelected(SelectionEvent e) {
+          if (!useLocalSdk.getSelection()) {
+            status = Status.OK_STATUS;
+          } else {
+            sdkLocation.doCheckState();
+          }
+          fireValueChanged(VALUE, "", "");
         }
       });
     }
