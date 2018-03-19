4fb4114bd817b80e61c6f98c25ea1972e5a987f6
Brian de Alwis <bsd@mt.ca>
2018-03-19 12:37:16 -0400

    Provide custom MenuMatcher

--- plugins/com.google.cloud.tools.eclipse.integration.appengine/src/com/google/cloud/tools/eclipse/integration/appengine/ProjectContextMenuTest.java
+++ plugins/com.google.cloud.tools.eclipse.integration.appengine/src/com/google/cloud/tools/eclipse/integration/appengine/ProjectContextMenuTest.java
@@ -15,11 +15,9 @@
  */
 
 package com.google.cloud.tools.eclipse.integration.appengine;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
 
 import com.google.cloud.tools.eclipse.appengine.facets.AppEngineStandardFacet;
 import com.google.cloud.tools.eclipse.swtbot.SwtBotProjectActions;
@@ -27,8 +25,6 @@ import com.google.cloud.tools.eclipse.test.util.project.TestProjectCreator;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.jst.common.project.facet.core.JavaFacet;
 import org.eclipse.jst.j2ee.web.project.facet.WebFacetUtils;
-import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotMenu;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.junit.Rule;
 import org.junit.Test;
@@ -41,12 +37,14 @@ public class ProjectContextMenuTest extends BaseProjectTest {
   public void testPlainJavaProject() {
     IProject project = projectCreator.withFacets(JavaFacet.VERSION_1_8).getProject();
     SWTBotTreeItem selected = SwtBotProjectActions.selectProject(bot, project.getName());
-    assertContextMenuPathNotFound(selected, "Debug As", "2 App Engine");
-    assertContextMenuPathNotFound(selected, "Run As", "2 App Engine");
-    assertContextMenuPathNotFound(selected, "Deploy to App Engine Standard...");
-    assertContextMenuPathFound(selected, "Configure", "Convert to App Engine Standard Project");
-    assertContextMenuPathNotFound(
-        selected, "Configure", "Reconfigure for App Engine Java 8 runtime");
+    assertThat(selected.contextMenu("Debug As"), not(MenuMatcher.hasMenuItem(endsWith("App Engine"))));
+    assertThat(selected.contextMenu("Run As"), not(MenuMatcher.hasMenuItem(endsWith("App Engine"))));
+    assertThat(
+        selected.contextMenu(), not(MenuMatcher.hasMenuItem("Deploy to App Engine Standard...")));
+    assertThat(selected.contextMenu("Configure"), MenuMatcher.hasMenuItem("Convert to App Engine Standard Project"));
+    assertThat(
+        selected.contextMenu("Configure"),
+        not(MenuMatcher.hasMenuItem("Reconfigure for App Engine Java 8 runtime")));
   }
 
   @Test
@@ -54,12 +52,18 @@ public class ProjectContextMenuTest extends BaseProjectTest {
     IProject project =
         projectCreator.withFacets(JavaFacet.VERSION_1_7, WebFacetUtils.WEB_25).getProject();
     SWTBotTreeItem selected = SwtBotProjectActions.selectProject(bot, project.getName());
-    assertContextMenuPathNotFound(selected, "Debug As", "2 App Engine");
-    assertContextMenuPathNotFound(selected, "Run As", "2 App Engine");
-    assertContextMenuPathNotFound(selected, "Deploy to App Engine Standard...");
-    assertContextMenuPathFound(selected, "Configure", "Convert to App Engine Standard Project");
-    assertContextMenuPathNotFound(
-        selected, "Configure", "Reconfigure for App Engine Java 8 runtime");
+    assertThat(
+        selected.contextMenu("Debug As"), not(MenuMatcher.hasMenuItem(endsWith("App Engine"))));
+    assertThat(
+        selected.contextMenu("Run As"), not(MenuMatcher.hasMenuItem(endsWith("App Engine"))));
+    assertThat(
+        selected.contextMenu(), not(MenuMatcher.hasMenuItem("Deploy to App Engine Standard...")));
+    assertThat(
+        selected.contextMenu("Configure"),
+        MenuMatcher.hasMenuItem("Convert to App Engine Standard Project"));
+    assertThat(
+        selected.contextMenu("Configure"),
+        not(MenuMatcher.hasMenuItem("Reconfigure for App Engine Java 8 runtime")));
   }
 
   @Test
@@ -67,12 +71,18 @@ public class ProjectContextMenuTest extends BaseProjectTest {
     IProject project =
         projectCreator.withFacets(JavaFacet.VERSION_1_8, WebFacetUtils.WEB_31).getProject();
     SWTBotTreeItem selected = SwtBotProjectActions.selectProject(bot, project.getName());
-    assertContextMenuPathNotFound(selected, "Debug As", "2 App Engine");
-    assertContextMenuPathNotFound(selected, "Run As", "2 App Engine");
-    assertContextMenuPathNotFound(selected, "Deploy to App Engine Standard...");
-    assertContextMenuPathFound(selected, "Configure", "Convert to App Engine Standard Project");
-    assertContextMenuPathNotFound(
-        selected, "Configure", "Reconfigure for App Engine Java 8 runtime");
+    assertThat(
+        selected.contextMenu("Debug As"), not(MenuMatcher.hasMenuItem(endsWith("App Engine"))));
+    assertThat(
+        selected.contextMenu("Run As"), not(MenuMatcher.hasMenuItem(endsWith("App Engine"))));
+    assertThat(
+        selected.contextMenu(), not(MenuMatcher.hasMenuItem("Deploy to App Engine Standard...")));
+    assertThat(
+        selected.contextMenu("Configure"),
+        MenuMatcher.hasMenuItem("Convert to App Engine Standard Project"));
+    assertThat(
+        selected.contextMenu("Configure"),
+        not(MenuMatcher.hasMenuItem("Reconfigure for App Engine Java 8 runtime")));
   }
 
   @Test
@@ -82,11 +92,15 @@ public class ProjectContextMenuTest extends BaseProjectTest {
             .withFacets(AppEngineStandardFacet.JRE7, JavaFacet.VERSION_1_7, WebFacetUtils.WEB_25)
             .getProject();
     SWTBotTreeItem selected = SwtBotProjectActions.selectProject(bot, project.getName());
-    assertContextMenuPathFound(selected, "Debug As", "2 App Engine");
-    assertContextMenuPathFound(selected, "Run As", "2 App Engine");
-    assertContextMenuPathFound(selected, "Deploy to App Engine Standard...");
-    assertContextMenuPathNotFound(selected, "Configure", "Convert to App Engine Standard Project");
-    assertContextMenuPathFound(selected, "Configure", "Reconfigure for App Engine Java 8 runtime");
+    assertThat(selected.contextMenu("Debug As"), MenuMatcher.hasMenuItem(endsWith("App Engine")));
+    assertThat(selected.contextMenu("Run As"), MenuMatcher.hasMenuItem(endsWith("App Engine")));
+    assertThat(selected.contextMenu(), MenuMatcher.hasMenuItem("Deploy to App Engine Standard..."));
+    assertThat(
+        selected.contextMenu("Configure"),
+        not(MenuMatcher.hasMenuItem("Convert to App Engine Standard Project")));
+    assertThat(
+        selected.contextMenu("Configure"),
+        MenuMatcher.hasMenuItem("Reconfigure for App Engine Java 8 runtime"));
   }
 
   @Test
@@ -99,43 +113,14 @@ public class ProjectContextMenuTest extends BaseProjectTest {
                 WebFacetUtils.WEB_31)
             .getProject();
     SWTBotTreeItem selected = SwtBotProjectActions.selectProject(bot, project.getName());
-    assertContextMenuPathFound(selected, "Debug As", "2 App Engine");
-    assertContextMenuPathFound(selected, "Run As", "2 App Engine");
-    assertContextMenuPathFound(selected, "Deploy to App Engine Standard...");
-    assertContextMenuPathNotFound(selected, "Configure", "Convert to App Engine Standard Project");
-    assertContextMenuPathNotFound(
-        selected, "Configure", "Reconfigure for App Engine Java 8 runtime");
-  }
-
-  /** Verify that the menu items on the path are all present and enabled. */
-  private void assertContextMenuPathFound(SWTBotTreeItem projectItem, String... contextMenuPath) {
-    assertTrue(contextMenuPath.length > 0);
-    SWTBotMenu menu = projectItem.contextMenu(contextMenuPath[0]);
-    for (int i = 1; i < contextMenuPath.length; i++) {
-      menu = menu.menu(contextMenuPath[i]);
-    }
-    assertTrue("should be enabled: " + menu.getText(), menu.isEnabled());
-  }
-
-  /** Verify that the last item on the path was not present. */
-  private void assertContextMenuPathNotFound(
-      SWTBotTreeItem projectItem, String... contextMenuPath) {
-    assertTrue(contextMenuPath.length > 0);
-    if (contextMenuPath.length == 1) {
-      assertFalse(projectItem.contextMenu().menuItems().contains(contextMenuPath[0]));
-      return;
-    }
-    SWTBotMenu menu = null;
-    try {
-      menu = projectItem.contextMenu(contextMenuPath[0]);
-      // intermediate menu items should be present
-      for (int i = 1; i < contextMenuPath.length - 1; i++) {
-        menu = menu.menu(contextMenuPath[i]);
-      }
-    } catch (WidgetNotFoundException ex) {
-      fail("intermediate menus not found");
-    }
-    assertNotNull(menu);
-    assertFalse(menu.menuItems().contains(contextMenuPath[contextMenuPath.length - 1]));
+    assertThat(selected.contextMenu("Debug As"), MenuMatcher.hasMenuItem(endsWith("App Engine")));
+    assertThat(selected.contextMenu("Run As"), MenuMatcher.hasMenuItem(endsWith("App Engine")));
+    assertThat(selected.contextMenu(), MenuMatcher.hasMenuItem("Deploy to App Engine Standard..."));
+    assertThat(
+        selected.contextMenu("Configure"),
+        not(MenuMatcher.hasMenuItem("Convert to App Engine Standard Project")));
+    assertThat(
+        selected.contextMenu("Configure"),
+        not(MenuMatcher.hasMenuItem("Reconfigure for App Engine Java 8 runtime")));
   }
 }
