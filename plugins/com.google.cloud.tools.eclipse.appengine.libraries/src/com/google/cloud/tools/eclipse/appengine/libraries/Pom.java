7ebbd912c5ab8ddcdb2ffeb497c419e9626488e5
Elliotte Harold <elharo@google.com>
2018-03-28 09:14:27 -0400

    check type and scope

--- plugins/com.google.cloud.tools.eclipse.appengine.libraries/src/com/google/cloud/tools/eclipse/appengine/libraries/Pom.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.libraries/src/com/google/cloud/tools/eclipse/appengine/libraries/Pom.java
@@ -93,7 +93,7 @@ class Pom {
       xpath.setNamespaceContext(new Maven4NamespaceContext());
 
       NodeList bomNodes = (NodeList) xpath.evaluate(
-          "//m:dependencyManagement/m:dependencies/m:dependency",
+          "//m:dependencyManagement/m:dependencies/m:dependency[type='pom'][scope='import']",
           document.getDocumentElement(),
           XPathConstants.NODESET);
       
