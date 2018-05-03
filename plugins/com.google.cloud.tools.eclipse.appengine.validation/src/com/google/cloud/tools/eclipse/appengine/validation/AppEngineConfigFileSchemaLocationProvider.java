514c2a45751bf353e96e95e8aae82925a472b04c
Brian de Alwis <bsd@mt.ca>
2018-05-03 15:39:41 -0400

    turn into table lookup

--- plugins/com.google.cloud.tools.eclipse.appengine.validation/src/com/google/cloud/tools/eclipse/appengine/validation/AppEngineConfigFileSchemaLocationProvider.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.validation/src/com/google/cloud/tools/eclipse/appengine/validation/AppEngineConfigFileSchemaLocationProvider.java
@@ -34,6 +34,42 @@ public class AppEngineConfigFileSchemaLocationProvider implements IExternalSchem
   private static final String SCHEMA_LOCATIONS_PREFIX =
       "platform:/plugin/com.google.cloud.tools.eclipse.appengine.validation/xsd/";
 
+  private static final Map<String, Map<String, String>> schemaLocations =
+      ImmutableMap.<String, Map<String, String>>builder()
+          // appengine-web.xml has a known namespace
+          .put("appengine-web.xml",
+              ImmutableMap.<String, String>builder()
+                  .put(NO_NAMESPACE_SCHEMA_LOCATION, SCHEMA_LOCATIONS_PREFIX + "appengine-web.xsd")
+                  .put(SCHEMA_LOCATION,
+                      "http://appengine.google.com/ns/1.0 " + SCHEMA_LOCATIONS_PREFIX
+                          + "appengine-web.xsd")
+                  .build())
+
+          .put("datastore-indexes.xml",
+              Collections.singletonMap(NO_NAMESPACE_SCHEMA_LOCATION,
+                  SCHEMA_LOCATIONS_PREFIX + "datastore-indexes.xsd"))
+          .put("datastore-indexes-auto.xml",
+              Collections.singletonMap(NO_NAMESPACE_SCHEMA_LOCATION,
+                  SCHEMA_LOCATIONS_PREFIX + "datastore-indexes.xsd"))
+
+          .put("cron.xml",
+              Collections.singletonMap(NO_NAMESPACE_SCHEMA_LOCATION,
+                  SCHEMA_LOCATIONS_PREFIX + "cron.xsd"))
+
+          .put("dispatch.xml",
+              Collections.singletonMap(NO_NAMESPACE_SCHEMA_LOCATION,
+                  SCHEMA_LOCATIONS_PREFIX + "dispatch.xsd"))
+
+          .put("queue.xml",
+              Collections.singletonMap(NO_NAMESPACE_SCHEMA_LOCATION,
+                  SCHEMA_LOCATIONS_PREFIX + "queue.xsd"))
+
+          .put("dos.xml",
+              Collections.singletonMap(NO_NAMESPACE_SCHEMA_LOCATION,
+                  SCHEMA_LOCATIONS_PREFIX + "dos.xsd"))
+
+          .build();
+
   @Override
   public Map<String, String> getExternalSchemaLocation(URI fileURI) {
     Preconditions.checkNotNull(fileURI);
@@ -42,38 +78,6 @@ public class AppEngineConfigFileSchemaLocationProvider implements IExternalSchem
       return Collections.emptyMap();
     }
     String basename = path.lastSegment();
-    switch (basename) {
-      // appengine-web.xml has a known namespace
-      case "appengine-web.xml":
-        return ImmutableMap.<String, String>builder()
-            .put(NO_NAMESPACE_SCHEMA_LOCATION, SCHEMA_LOCATIONS_PREFIX + "appengine-web.xsd")
-            .put(SCHEMA_LOCATION, "http://appengine.google.com/ns/1.0 " + SCHEMA_LOCATIONS_PREFIX
-                + "appengine-web.xsd")
-            .build();
-
-      case "datastore-indexes.xml":
-      case "datastore-indexes-auto.xml":
-        return Collections.singletonMap(NO_NAMESPACE_SCHEMA_LOCATION,
-            SCHEMA_LOCATIONS_PREFIX + "datastore-indexes.xsd");
-
-      case "cron.xml":
-        return Collections.singletonMap(NO_NAMESPACE_SCHEMA_LOCATION,
-            SCHEMA_LOCATIONS_PREFIX + "cron.xsd");
-
-      case "dispatch.xml":
-        return Collections.singletonMap(NO_NAMESPACE_SCHEMA_LOCATION,
-            SCHEMA_LOCATIONS_PREFIX + "dispatch.xsd");
-
-      case "queue.xml":
-        return Collections.singletonMap(NO_NAMESPACE_SCHEMA_LOCATION,
-            SCHEMA_LOCATIONS_PREFIX + "queue.xsd");
-
-      case "dos.xml":
-        return Collections.singletonMap(NO_NAMESPACE_SCHEMA_LOCATION,
-            SCHEMA_LOCATIONS_PREFIX + "dos.xsd");
-
-      default:
-        return Collections.emptyMap();
-    }
+    return schemaLocations.getOrDefault(basename, Collections.emptyMap());
   }
 }
