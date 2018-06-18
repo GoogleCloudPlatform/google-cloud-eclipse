18fa6905224789bfa22b7afb326aa92bdb6853e4
Chanseok Oh <chanseok@google.com>
2018-06-18 16:19:40 -0400

    Propagate exception in project creation

--- plugins/com.google.cloud.tools.eclipse.dataflow.ui/src/com/google/cloud/tools/eclipse/dataflow/ui/wizard/NewDataflowProjectWizard.java
+++ plugins/com.google.cloud.tools.eclipse.dataflow.ui/src/com/google/cloud/tools/eclipse/dataflow/ui/wizard/NewDataflowProjectWizard.java
@@ -23,6 +23,7 @@ import com.google.cloud.tools.eclipse.dataflow.ui.page.NewDataflowProjectWizardD
 import com.google.cloud.tools.eclipse.dataflow.ui.page.NewDataflowProjectWizardLandingPage;
 import com.google.cloud.tools.eclipse.usagetracker.AnalyticsEvents;
 import com.google.cloud.tools.eclipse.usagetracker.AnalyticsPingManager;
+import com.google.cloud.tools.eclipse.util.status.StatusUtil;
 import java.lang.reflect.InvocationTargetException;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
@@ -60,7 +61,7 @@ public class NewDataflowProjectWizard extends Wizard implements INewWizard {
     try {
       getContainer().run(true, true, creator);
     } catch (InvocationTargetException | InterruptedException ex) {
-      // TODO: handle
+      StatusUtil.setErrorStatus(this, ex.getCause().getMessage(), ex.getCause());
       DataflowUiPlugin.logError(ex, 
           "Error encountered when trying to create project"); //$NON-NLS-1$
       return false;
