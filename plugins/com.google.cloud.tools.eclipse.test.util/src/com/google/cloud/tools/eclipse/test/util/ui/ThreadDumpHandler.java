f1ed2956e3451f2964324ef51622723ccaafa4bb
Brian de Alwis <bsd@mt.ca>
2018-07-19 11:41:08 -0400

    Add missing copyright

--- plugins/com.google.cloud.tools.eclipse.test.util/src/com/google/cloud/tools/eclipse/test/util/ui/ThreadDumpHandler.java
+++ plugins/com.google.cloud.tools.eclipse.test.util/src/com/google/cloud/tools/eclipse/test/util/ui/ThreadDumpHandler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.google.cloud.tools.eclipse.test.util.ui;
 
@@ -6,6 +21,10 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 
+/**
+ * A helper command that can be invoked from Eclipse's <em>Quick Access</em> to perform a
+ * thread-dump to the running instance's stdout.
+ */
 public class ThreadDumpHandler extends AbstractHandler {
 
   @Override
