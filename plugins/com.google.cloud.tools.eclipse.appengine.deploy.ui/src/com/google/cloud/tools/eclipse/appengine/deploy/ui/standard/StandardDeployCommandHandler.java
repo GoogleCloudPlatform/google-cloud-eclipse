54ba87a43a4602fb9fbac2c4bfd902982506ff91
Brian de Alwis <bsd@mt.ca>
2018-12-19 12:59:53 -0500

    Provide never-warn toggle button in dialog

--- plugins/com.google.cloud.tools.eclipse.appengine.deploy.ui/src/com/google/cloud/tools/eclipse/appengine/deploy/ui/standard/StandardDeployCommandHandler.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.deploy.ui/src/com/google/cloud/tools/eclipse/appengine/deploy/ui/standard/StandardDeployCommandHandler.java
@@ -29,6 +29,7 @@ import com.google.cloud.tools.eclipse.login.IGoogleLoginService;
 import com.google.cloud.tools.eclipse.usagetracker.AnalyticsEvents;
 import com.google.cloud.tools.eclipse.util.jdt.JreDetector;
 import java.nio.file.Path;
+import java.util.LinkedHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.eclipse.core.resources.IProject;
@@ -40,9 +41,11 @@ import org.eclipse.jdt.launching.IVMInstall2;
 import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.MessageDialogWithToggle;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.wst.common.project.facet.core.IFacetedProject;
 import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
+import org.osgi.service.prefs.BackingStoreException;
 
 public class StandardDeployCommandHandler extends DeployCommandHandler {
   private static final Logger logger = Logger.getLogger(StandardDeployCommandHandler.class.getName());
@@ -64,21 +67,38 @@ public class StandardDeployCommandHandler extends DeployCommandHandler {
         return false;
       }
 
+      DeployPreferences deployPreferences = getDeployPreferences(project);
       if (AppEngineStandardFacet.usesObsoleteRuntime(facetedProject)
-          && !getDeployPreferences(project).getAllowObsoleteRuntime()) {
-        String[] buttonLabels =
-            new String[] {Messages.getString("deploy.button"), IDialogConstants.CANCEL_LABEL};
+          && !deployPreferences.getAllowObsoleteRuntime()) {
+        // Explicitly specify button IDs as MessageDialogWithToggle, unlike MessageDialog, assigns
+        // default IDs for custom buttons starting at 256
+        LinkedHashMap<String, Integer> buttonsWithIds = new LinkedHashMap<>();
+        buttonsWithIds.put(Messages.getString("deploy.button"), IDialogConstants.OK_ID);
+        buttonsWithIds.put(IDialogConstants.CANCEL_LABEL, IDialogConstants.CANCEL_ID);
         String message = Messages.getString("obsolete.runtime.proceed", project.getName());
-        MessageDialog dialog =
-            new MessageDialog(
+        MessageDialogWithToggle dialog =
+            new MessageDialogWithToggle(
                 shell,
                 Messages.getString("obsolete.runtime.title"),
                 null,
                 message,
-                MessageDialog.QUESTION,
-                0,
-                buttonLabels);
-        return dialog.open() == 0;
+                MessageDialog.WARNING,
+                buttonsWithIds,
+                0 /* index into buttonsWithIds of default button */,
+                Messages.getString("obsolete.runtime.toggle"),
+                false);
+        // For MessageDialog and MessageDialogWithToggle, open() returns the dialog's return code,
+        // and the return code is set to the ID of the selected button
+        if (dialog.open() == IDialogConstants.OK_ID && dialog.getToggleState()) {
+          try {
+            deployPreferences.setAllowObsoleteRuntime(true);
+            deployPreferences.save();
+          } catch (BackingStoreException ex) {
+            logger.log(
+                Level.WARNING, "Unable to check project use of obsolete App Engine runtime", ex);
+          }
+        }
+        return dialog.getReturnCode() == IDialogConstants.OK_ID;
       }
     } catch(CoreException ex) {
       logger.log(Level.WARNING, "Unable to check project use of obsolete App Engine runtime", ex);
