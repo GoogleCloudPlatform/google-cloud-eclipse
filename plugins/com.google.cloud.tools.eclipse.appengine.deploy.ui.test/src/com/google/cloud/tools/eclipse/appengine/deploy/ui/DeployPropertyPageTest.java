c5eb585d9e145053c2c70f526f1e7231939d3db3
Lakshminarayana Nekkanti <narayana.nekkanti@gmail.com>
2019-05-14 17:18:44 +0530

    Update testcase

--- plugins/com.google.cloud.tools.eclipse.appengine.deploy.ui.test/src/com/google/cloud/tools/eclipse/appengine/deploy/ui/DeployPropertyPageTest.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.deploy.ui.test/src/com/google/cloud/tools/eclipse/appengine/deploy/ui/DeployPropertyPageTest.java
@@ -22,9 +22,10 @@ import static org.junit.Assert.fail;
 
 import com.google.cloud.tools.eclipse.googleapis.IGoogleApiFactory;
 import com.google.cloud.tools.eclipse.login.IGoogleLoginService;
+import com.google.cloud.tools.eclipse.test.util.ui.CompositeUtil;
 import com.google.cloud.tools.eclipse.test.util.ui.ShellTestResource;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.swt.custom.StackLayout;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Layout;
@@ -57,14 +58,17 @@ public abstract class DeployPropertyPageTest<P extends DeployPreferencesPanel> {
       if (control instanceof Composite) {
         Composite maybeDeployPageComposite = (Composite) control;
         Layout layout = maybeDeployPageComposite.getLayout();
-        if (layout instanceof StackLayout) {
-          StackLayout stackLayout = (StackLayout) layout;
-          assertThat(stackLayout.topControl, instanceOf(getPanelClass()));
+        if (layout instanceof GridLayout) {
+          assertThat(getDeployPreferencesPanel(maybeDeployPageComposite), instanceOf(getPanelClass()));
           return;
         }
       }
     }
     fail("Did not find the deploy preferences panel");
+  }
+
+  private DeployPreferencesPanel getDeployPreferencesPanel(Composite composite) {
+    return CompositeUtil.findControl(composite, getPanelClass());
   }
 
   protected abstract IProject getProject();
