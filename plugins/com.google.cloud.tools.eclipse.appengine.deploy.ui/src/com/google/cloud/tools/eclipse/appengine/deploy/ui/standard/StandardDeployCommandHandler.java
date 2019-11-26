9e8bef5ec5a865ff4a86f7d32aaaafd718c88890
Elliotte Rusty Harold <elharo@ibiblio.org>
2019-11-26 11:21:50 -0500

    use standard error dialog

--- plugins/com.google.cloud.tools.eclipse.appengine.deploy.ui/src/com/google/cloud/tools/eclipse/appengine/deploy/ui/standard/StandardDeployCommandHandler.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.deploy.ui/src/com/google/cloud/tools/eclipse/appengine/deploy/ui/standard/StandardDeployCommandHandler.java
@@ -65,17 +65,8 @@ public class StandardDeployCommandHandler extends DeployCommandHandler {
       }
 
       if (AppEngineStandardFacet.usesObsoleteRuntime(facetedProject)) {
-        String[] buttons = {Messages.getString("ok.button")};
         String message = Messages.getString("obsolete.runtime.message", project.getName());
-        MessageDialog dialog = new MessageDialog(
-            shell,
-            Messages.getString("obsolete.runtime.title"),
-            null,
-            message,
-            MessageDialog.ERROR,
-            buttons,
-            0 /* index into buttonsWithIds of default button */);
-        dialog.open();
+        MessageDialog.openError(shell, Messages.getString("obsolete.runtime.title"), message);
         return false;
       }
     } catch(CoreException ex) {
