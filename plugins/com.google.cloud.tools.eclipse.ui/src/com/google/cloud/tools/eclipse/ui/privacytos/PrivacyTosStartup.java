4a781ea8ce7a1dd2579dd7d37c6ebcfe4d3da1ab
Diego Marquez <diegomarquezp@google.com>
2022-09-25 22:18:03 +0000

    feat(privacytos): Display clickable HTML links

--- plugins/com.google.cloud.tools.eclipse.ui/src/com/google/cloud/tools/eclipse/ui/privacytos/PrivacyTosStartup.java
+++ plugins/com.google.cloud.tools.eclipse.ui/src/com/google/cloud/tools/eclipse/ui/privacytos/PrivacyTosStartup.java
@@ -16,6 +16,8 @@
 
 package com.google.cloud.tools.eclipse.ui.privacytos;
 
+import com.google.cloud.tools.eclipse.util.MessageDialogWithToggleAndLink;
+import com.google.common.collect.ImmutableList;
 import java.util.logging.Logger;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.core.runtime.preferences.InstanceScope;
@@ -28,16 +30,19 @@ import org.osgi.service.prefs.BackingStoreException;
 import org.osgi.service.prefs.Preferences;
 
 /**
- * 
+ * Displays Privacy and TOS statement at startup
  */
 public class PrivacyTosStartup implements IStartup {
 
   private static final Logger logger = Logger.getLogger(PrivacyTosStartup.class.getName());
   private static final String PRIVACY_TOS_TITLE = "Google Cloud Tools for Eclipse Privacy and TOS";
-  private static final String PRIVACY_TOS_MESSAGE = "Please read our Privacy Statement and TOS before proceeding with the tool <a href=\"test\">test</test>";
+  private static final String PRIVACY_TOS_MESSAGE = "Please read our Privacy Statement and TOS before proceeding with the tool.";
   private static final String PREFERENCE_PERSISTENCE_KEY = "com.google.cloud.tools.eclipse.ui.privacytos.displayprivacytos";
   private static final String PREFERENCE_NODE_KEY = "com.google.cloud.tools.eclipse.ui.privacytos";
-
+  private static final String[] STATEMENT_LINKS = {
+      "<a href=\"https://policies.google.com/privacy\"> Privacy Policy </a>",
+      "<a href=\"https://policies.google.com/terms\"> Terms of Service </a>"
+  };
   @Override
   public void earlyStartup() {
     // TODO Determine persistent way of "do not show again"
@@ -52,7 +57,14 @@ public class PrivacyTosStartup implements IStartup {
           Preferences preferences = preferenceStore.node(PREFERENCE_NODE_KEY);
           String showAgain = preferences.get(PREFERENCE_PERSISTENCE_KEY, MessageDialogWithToggle.PROMPT);
           if (showAgain.equals(MessageDialogWithToggle.PROMPT)) {
-            MessageDialogWithToggle dialog = MessageDialogWithToggle.openOkCancelConfirm(window.getShell(), PRIVACY_TOS_TITLE, PRIVACY_TOS_MESSAGE, "Do not show again", false, null, null);  
+            MessageDialogWithToggle dialog = MessageDialogWithToggleAndLink.openOkCancelConfirmLinks(
+                null,
+                PRIVACY_TOS_TITLE, 
+                PRIVACY_TOS_MESSAGE, 
+                "Do not show again", 
+                false, 
+                ImmutableList.copyOf(STATEMENT_LINKS)
+            );  
             boolean toggleResponse = dialog.getToggleState();
             String newPreference = toggleResponse ? MessageDialogWithToggle.NEVER : MessageDialogWithToggle.PROMPT;
 
